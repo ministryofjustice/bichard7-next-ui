71b725c6ea7107d6c54a55466c796c8deafeb2e9
Alice Lee <alice.lee@madetech.com>
2022-07-01 15:09:09 +0100

    Add more tests for listCases
    Co-authored-by: Emad Karamad <emad.karamad@madetech.com>
    Co-authored-by: Joe Folkard <joe.folkard@madetech.com>

--- test/UseCases/listCases.integration.test.ts
+++ test/UseCases/listCases.integration.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import Database from "../../src/types/Database"
 import getTestConnection from "../testFixtures/getTestConnection"
 import listCases from "../../src/useCases/listCases"
@@ -21,38 +22,14 @@ describe("listCases", () => {
     connection.$pool.end()
   })
 
-  it("should return a list of cases when the force code length is 2", async () => {
-    const existingCases = Array.from(Array(100)).map((_x, i) => {
-        return {
-           ...errorListCase, 
-           org_for_police_filter: "36FP  ",
-           error_id: i,
-           message_id: String(i).padStart(5, 'x')
-        }
-    })
-
-    await insertIntoErrorListTable(existingCases)
-
-    const cases = await listCases(connection, ["036"], 100)
-
-    expect(isError(cases)).toBe(false)
-
-    expect(cases).toHaveLength(100)
-
-    expect(cases[0].error_id).toBe(0)
-    expect(cases[99].error_id).toBe(99)
-    expect(cases[0].message_id).toBe("xxxx0")
-    expect(cases[99].message_id).toBe("xxx99")
-  })
-
   it("should return a list of cases when the force code length is 1", async () => {
     const existingCases = Array.from(Array(100)).map((_x, i) => {
-        return {
-           ...errorListCase, 
-           org_for_police_filter: "36FP  ",
-           error_id: i,
-           message_id: String(i).padStart(5, 'x')
-        }
+      return {
+        ...errorListCase,
+        org_for_police_filter: "36FP  ",
+        error_id: i,
+        message_id: String(i).padStart(5, "x")
+      }
     })
 
     await insertIntoErrorListTable(existingCases)
@@ -69,14 +46,62 @@ describe("listCases", () => {
     expect(cases[99].message_id).toBe("xxx99")
   })
 
-  it("should return a list of cases when the force code length is 4", async () => {
+  it("should return a list of cases when the force code length is 2", async () => {
     const existingCases = Array.from(Array(100)).map((_x, i) => {
-        return {
-           ...errorListCase, 
-           org_for_police_filter: "36FP  ",
-           error_id: i,
-           message_id: String(i).padStart(5, 'x')
-        }
+      return {
+        ...errorListCase,
+        org_for_police_filter: "36FP  ",
+        error_id: i,
+        message_id: String(i).padStart(5, "x")
+      }
+    })
+
+    await insertIntoErrorListTable(existingCases)
+
+    const cases = await listCases(connection, ["036"], 100)
+
+    expect(isError(cases)).toBe(false)
+
+    expect(cases).toHaveLength(100)
+
+    expect(cases[0].error_id).toBe(0)
+    expect(cases[99].error_id).toBe(99)
+    expect(cases[0].message_id).toBe("xxxx0")
+    expect(cases[99].message_id).toBe("xxx99")
+  })
+
+  it("should return a list of cases when the force code length is 3", async () => {
+    const existingCases = Array.from(Array(100)).map((_x, i) => {
+      return {
+        ...errorListCase,
+        org_for_police_filter: "36FP  ",
+        error_id: i,
+        message_id: String(i).padStart(5, "x")
+      }
+    })
+
+    await insertIntoErrorListTable(existingCases)
+
+    const cases = await listCases(connection, ["036F"], 100)
+
+    expect(isError(cases)).toBe(false)
+
+    expect(cases).toHaveLength(100)
+
+    expect(cases[0].error_id).toBe(0)
+    expect(cases[99].error_id).toBe(99)
+    expect(cases[0].message_id).toBe("xxxx0")
+    expect(cases[99].message_id).toBe("xxx99")
+  })
+
+  it("should return a list of cases when the force code length is 4", async () => {
+    const existingCases = Array.from(Array(100)).map((_, i) => {
+      return {
+        ...errorListCase,
+        org_for_police_filter: "36FP  ",
+        error_id: i,
+        message_id: String(i).padStart(5, "x")
+      }
     })
 
     await insertIntoErrorListTable(existingCases)
@@ -91,15 +116,16 @@ describe("listCases", () => {
     expect(cases[0].message_id).toBe("xxxx0")
     expect(cases[99].message_id).toBe("xxx99")
   })
-  
-   it("should return a list of cases when the force code length is 6", async () => {
-    const existingCases = Array.from(Array(100)).map((_x, i) => {
-        return {
-           ...errorListCase, 
-           org_for_police_filter: "36FPA1",
-           error_id: i,
-           message_id: String(i).padStart(5, 'x')
-        }
+
+  it("shouldn't return non-visible cases when the force code length is 6", async () => {
+    const orgsCodes = ["36", "36F", "36FP", "36FPA", "36FPA1", "12LK", "12G", "12GHB", "12GHA", "12GHAB", "12GHAC"]
+    const existingCases = orgsCodes.map((code, i) => {
+      return {
+        ...errorListCase,
+        org_for_police_filter: code.padEnd(6, " "),
+        error_id: i,
+        message_id: String(i).padStart(5, "x")
+      }
     })
 
     await insertIntoErrorListTable(existingCases)
@@ -107,35 +133,81 @@ describe("listCases", () => {
     const cases = await listCases(connection, ["036FPA1"], 100)
     expect(isError(cases)).toBe(false)
 
-    expect(cases).toHaveLength(100)
+    expect(cases).toHaveLength(3)
 
-    expect(cases[0].error_id).toBe(0)
-    expect(cases[99].error_id).toBe(99)
-    expect(cases[0].message_id).toBe("xxxx0")
-    expect(cases[99].message_id).toBe("xxx99")
+    expect(cases.map((c) => c.org_for_police_filter)).toStrictEqual(["36FP  ", "36FPA ", "36FPA1"])
+    expect(cases.map((c) => c.error_id)).toStrictEqual([2, 3, 4])
   })
 
-   it("should only return cases visible to this user", async () => {
+  it("shouldn't return more cases than the specified limit", async () => {
     const existingCases = Array.from(Array(100)).map((_x, i) => {
-        return {
-           ...errorListCase, 
-           org_for_police_filter: i.toString().padStart(2, "0"),
-           error_id: i,
-           message_id: String(i).padStart(5, 'x')
-        }
+      return {
+        ...errorListCase,
+        org_for_police_filter: "36FPA1",
+        error_id: i,
+        message_id: String(i).padStart(5, "x")
+      }
     })
 
     await insertIntoErrorListTable(existingCases)
 
-    const cases = await listCases(connection, ["012"], 100)
+    const cases = await listCases(connection, ["036FPA1"], 10)
     expect(isError(cases)).toBe(false)
 
-    expect(cases).toHaveLength(1)
+    expect(cases).toHaveLength(10)
 
-    expect(cases[0].error_id).toBe(12)
-    expect(cases[0].message_id).toBe("xxx12")
-    expect(cases[0].org_for_police_filter).toBe("12    ")
-    
+    expect(cases[0].error_id).toBe(0)
+    expect(cases[9].error_id).toBe(9)
+    expect(cases[0].message_id).toBe("xxxx0")
+    expect(cases[9].message_id).toBe("xxxx9")
   })
 
+  it("a user with a visible force of length 4 can see cases where the org filter starts with the visible force", async () => {
+    const orgsCodes = ["12GH", "12LK", "12G", "12GHAB", "13BR", "14AT"]
+    const existingCases = orgsCodes.map((code, i) => {
+      return {
+        ...errorListCase,
+        org_for_police_filter: code.padEnd(6, " "),
+        error_id: i,
+        message_id: String(i).padStart(5, "x")
+      }
+    })
+
+    await insertIntoErrorListTable(existingCases)
+
+    const cases = await listCases(connection, ["012GH"], 100)
+    expect(isError(cases)).toBe(false)
+
+    expect(cases).toHaveLength(2)
+
+    expect(cases[0].message_id).toBe("xxxx0")
+    expect(cases[0].org_for_police_filter).toBe("12GH  ")
+
+    expect(cases[1].message_id).toBe("xxxx3")
+    expect(cases[1].org_for_police_filter).toBe("12GHAB")
+  })
+
+  it("a user with a visible force of length 5 can see cases for the 4-long prefix, and the exact match, and 6-long suffixes of the visible force", async () => {
+    const orgsCodes = ["12GH", "12LK", "12G", "12GHB", "12GHA", "12GHAB", "12GHAC", "13BR", "14AT"]
+    const existingCases = orgsCodes.map((code, i) => {
+      return {
+        ...errorListCase,
+        org_for_police_filter: code.padEnd(6, " "),
+        error_id: i,
+        message_id: String(i).padStart(5, "x")
+      }
+    })
+
+    await insertIntoErrorListTable(existingCases)
+
+    const cases = await listCases(connection, ["012GHA"], 100)
+    expect(isError(cases)).toBe(false)
+
+    expect(cases).toHaveLength(4)
+
+    expect(cases[0].org_for_police_filter).toBe("12GH  ")
+    expect(cases[1].org_for_police_filter).toBe("12GHA ")
+    expect(cases[2].org_for_police_filter).toBe("12GHAB")
+    expect(cases[3].org_for_police_filter).toBe("12GHAC")
+  })
 })
