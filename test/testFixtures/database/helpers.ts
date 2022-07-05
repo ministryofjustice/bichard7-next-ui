9110544529771415aae4ef872ac89ff64cfcb9fb
Alice Lee <alice.lee@madetech.com>
2022-07-05 12:21:47 +0100

    Remove pg-promise
    Co-authored-by: csabagyorfi <csaba@madetech.com>
    Co-authored-by: Emad Karamad <emad.karamad@madetech.com>

--- test/testFixtures/database/helpers.ts
+++
@@ -1,8 +0,0 @@
-import pgPromise from "pg-promise"
-
-export const getTableName = (tableName: string) => {
-  const pgp = pgPromise()
-  return new pgp.helpers.TableName({ table: tableName, schema: "br7own" })
-}
-
-export default { getTableName }
