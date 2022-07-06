3da4b2251cf20543e4601a024a1628100b53bd26
Alice Lee <alice.lee@madetech.com>
2022-07-06 13:43:49 +0100

    Fix asset links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,14 +9,31 @@ const GovUkMetadata = () => (
 
     <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 
-    <link rel="shortcut icon" sizes="16x16 32x32 48x48" href={addBasePath("/images/favicon.ico")} type="image/x-icon" />
+    <link
+      rel="shortcut icon"
+      sizes="16x16 32x32 48x48"
+      href={addBasePath("/assets/images/favicon.ico")}
+      type="image/x-icon"
+    />
     <link rel="mask-icon" href={addBasePath("/images/govuk-mask-icon.svg")} color="#0b0c0c" />
-    <link rel="apple-touch-icon" sizes="180x180" href={addBasePath("/images/govuk-apple-touch-icon-180x180.png")} />
-    <link rel="apple-touch-icon" sizes="167x167" href={addBasePath("/images/govuk-apple-touch-icon-167x167.png")} />
-    <link rel="apple-touch-icon" sizes="152x152" href={addBasePath("/images/govuk-apple-touch-icon-152x152.png")} />
-    <link rel="apple-touch-icon" href={addBasePath("/images/govuk-apple-touch-icon.png")} />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href={addBasePath("/assets/images/govuk-apple-touch-icon-180x180.png")}
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="167x167"
+      href={addBasePath("/assets/images/govuk-apple-touch-icon-167x167.png")}
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="152x152"
+      href={addBasePath("/assets/images/govuk-apple-touch-icon-152x152.png")}
+    />
+    <link rel="apple-touch-icon" href={addBasePath("/assets/images/govuk-apple-touch-icon.png")} />
 
-    <meta property="og:image" content={addBasePath("/images/govuk-opengraph-image.png")} />
+    <meta property="og:image" content={addBasePath("/assets/images/govuk-opengraph-image.png")} />
   </>
 )
 
