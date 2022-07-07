77f612919bbfe9b0076ef20db49caf33fba9dae5
Alice Lee <alice.lee@madetech.com>
2022-07-07 12:02:03 +0100

    Use cypress tasks to insert and clear court cases

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/naming-convention */
 import { defineConfig } from "cypress"
+import CourtCase from "entities/CourtCase"
+import deleteFromTable from "./test/testFixtures/database/deleteFromTable"
+import { insertCourtCasesWithOrgCodes } from "./test/testFixtures/database/insertCourtCases"
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:4080/bichard"
+    baseUrl: "http://localhost:4080/bichard",
+    setupNodeEvents(on, _config) {
+      on("task", {
+        insertCourtCasesWithOrgCodes(orgCodes: string[]) {
+          return insertCourtCasesWithOrgCodes(orgCodes)
+        },
+
+        clearCourtCases() {
+          return deleteFromTable(CourtCase)
+        }
+      })
+    }
   }
 })
--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,8 +1,22 @@
 describe("Home", () => {
-  it("should display 0 cases and the user's username when no cases are added", () => {
-    cy.setAuthCookie("Bichard01")
-    cy.visit("/")
-    cy.get("caption").should("have.text", "0 court cases for Bichard01")
+  context("720p resolution", () => {
+    beforeEach(() => {
+      cy.task("clearCourtCases")
+      cy.clearCookies()
+    })
+
+    it("should display 0 cases and the user's username when no cases are added", () => {
+      cy.setAuthCookie("Bichard01")
+      cy.visit("/")
+      cy.get("caption").should("have.text", "0 court cases for Bichard01")
+    })
+
+    it("should display a case for the user's org", () => {
+      cy.setAuthCookie("Bichard01")
+      cy.task("insertCourtCasesWithOrgCodes", ["01"])
+      cy.visit("/")
+      cy.get("caption").should("have.text", "1 court cases for Bichard01")
+    })
   })
 })
 
