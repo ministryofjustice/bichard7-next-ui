6ec2ee3a9d039670bd169bbdbf2213768dfe6610
Alice Lee <alice.lee@madetech.com>
2022-07-08 15:32:18 +0100

    Add test for case a user doesn't have access to

--- cypress/e2e/court-cases/caseDetails.cy.ts
+++ cypress/e2e/court-cases/caseDetails.cy.ts
@@ -2,13 +2,6 @@ import type { TestUser } from "../../../test/testFixtures/database/manageUsers"
 
 describe("Home", () => {
   context("720p resolution", () => {
-    beforeEach(() => {
-      cy.task("clearCourtCases")
-      cy.task("clearUsers")
-      cy.setAuthCookie("Bichard01")
-      cy.viewport(1280, 720)
-    })
-
     const users: TestUser[] = Array.from(Array(5)).map((_value, idx) => {
       return {
         username: `Bichard0${idx}`,
@@ -19,18 +12,31 @@ describe("Home", () => {
       }
     })
 
+    beforeEach(() => {
+      cy.task("clearCourtCases")
+      cy.task("clearUsers")
+      cy.task("insertUsers", users)
+      cy.clearCookies()
+      cy.setAuthCookie("Bichard01")
+      cy.viewport(1280, 720)
+    })
+
     // it("should load case details for the case that this user can see", () => {
 
     // })
 
-    // it("should return 404 for the case that this user can not see", () => {
+    it("should return 404 for a case that this user can not see", () => {
+      cy.task("insertCourtCasesWithOrgCodes", ["02"])
 
-    // })
+      cy.request({
+        failOnStatusCode: false,
+        url: "/court-cases/0"
+      }).then((response) => {
+        expect(response.status).to.eq(404)
+      })
+    })
 
-    it("should return 404 for the case that does not exist", () => {
-      cy.task("insertUsers", users)
-      cy.setAuthCookie("Bichard01")
-
+    it("should return 404 for a case that does not exist", () => {
       cy.request({
         failOnStatusCode: false,
         url: "/court-cases/1"
@@ -39,7 +45,7 @@ describe("Home", () => {
       })
     })
 
-    it.skip("should return 401 if auth token is invalid", () => {
+    it("should return 401 if the auth token provided is for a non-existent user", () => {
       cy.clearCookies()
       cy.setAuthCookie("InvalidUser")
 
