98a70cbc628af768da1639bf4df12d75e0e71829
csabagyorfi <csaba@madetech.com>
2022-07-14 14:18:45 +0100

    Order court cases by column names in both orders
    Co-authored-by: joe-fol <joe.folkard@madetech.com>

--- src/useCases/listCourtCases.ts
+++ src/useCases/listCourtCases.ts
@@ -3,18 +3,17 @@ import { DataSource } from "typeorm"
 import PromiseResult from "../types/PromiseResult"
 import getColumnName from "../lib/getColumnName"
 import courtCasesByVisibleForcesQuery from "./queries/courtCasesByVisibleForcesQuery"
+import QueryParams from "types/QueryParams"
 
-const listCourtCases = async (
-  connection: DataSource,
-  forces: string[],
-  limit: number,
-  orderBy = "errorId"
-): PromiseResult<CourtCase[]> => {
+const listCourtCases = async (connection: DataSource, queryParams: QueryParams): PromiseResult<CourtCase[]> => {
   const courtCaseRepository = connection.getRepository(CourtCase)
-  const query = courtCasesByVisibleForcesQuery(courtCaseRepository, forces)
+  const query = courtCasesByVisibleForcesQuery(courtCaseRepository, queryParams.forces)
     .leftJoinAndSelect("courtCase.triggers", "trigger")
-    .orderBy(`courtCase.${getColumnName(courtCaseRepository, orderBy)}`, "ASC")
-    .limit(limit)
+    .orderBy(
+      `courtCase.${getColumnName(courtCaseRepository, queryParams.orderBy ?? "errorId")}`,
+      queryParams.order ?? "ASC"
+    )
+    .limit(queryParams.limit)
 
   const result = await query.getMany().catch((error: Error) => error)
   return result
