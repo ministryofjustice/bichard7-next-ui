f9e337fe62254b9cd732579b87b3e95bfcb377d0
Alice Lee <alice.lee@madetech.com>
2022-08-12 15:06:42 +0100

    Add initial implementation and test for marking triggers complete

---
+++ src/services/markTriggerComplete.ts
@@ -0,0 +1,31 @@
+import { DataSource } from "typeorm"
+import PromiseResult from "types/PromiseResult"
+import { isError } from "types/Result"
+import Trigger from "./entities/Trigger"
+
+// Returns back whether the trigger was successfully unlocked
+const markTriggerComplete = async (
+  dataSource: DataSource,
+  trigger: Trigger,
+  resolver: string
+): PromiseResult<boolean> => {
+  try {
+    const result = await dataSource
+      .createQueryBuilder()
+      .update(Trigger)
+      .set({
+        resolvedAt: new Date(),
+        resolvedBy: resolver
+      })
+      .where("trigger_id = :triggerId", { triggerId: trigger.triggerId })
+      .execute()
+
+    return result.affected !== undefined && result.affected > 0
+  } catch (err) {
+    return isError(err)
+      ? err
+      : new Error(`Unspecified database error when marking trigger ${trigger.triggerId} as completed by ${resolver}`)
+  }
+}
+
+export default markTriggerComplete
---
+++ test/services/markTriggerComplete.integration.test.ts
@@ -0,0 +1,67 @@
+import { expect } from "@jest/globals"
+import { differenceInMinutes } from "date-fns"
+import "reflect-metadata"
+import markTriggerComplete from "../../src/services/markTriggerComplete"
+import { DataSource } from "typeorm"
+import { isError } from "types/Result"
+import CourtCase from "../../src/services/entities/CourtCase"
+import Trigger from "../../src/services/entities/Trigger"
+import getDataSource from "../../src/services/getDataSource"
+import deleteFromTable from "../testFixtures/database/deleteFromTable"
+import { insertCourtCasesWithOrgCodes } from "../testFixtures/database/insertCourtCases"
+import { insertTriggers, TestTrigger } from "../testFixtures/database/manageTriggers"
+
+jest.setTimeout(100000)
+
+describe("listCourtCases", () => {
+  let dataSource: DataSource
+
+  beforeAll(async () => {
+    dataSource = await getDataSource()
+  })
+
+  beforeEach(async () => {
+    await deleteFromTable(CourtCase)
+  })
+
+  afterAll(async () => {
+    await dataSource.destroy()
+  })
+
+  describe("Mark trigger as complete", () => {
+    it("Should set the resolvedAt and resolvedBy columns when marking as complete", async () => {
+      const resolverUsername = "triggerResolver01"
+
+      await insertCourtCasesWithOrgCodes(["36"])
+      const trigger: TestTrigger = {
+        triggerId: 0,
+        triggerCode: "TRPR0001",
+        status: 0,
+        createdAt: new Date("2022-07-12T10:22:34.000Z")
+      }
+      await insertTriggers(0, [trigger])
+      let retrievedTrigger = await dataSource
+        .getRepository(Trigger)
+        .findOne({ where: { triggerId: trigger.triggerId } })
+      expect(retrievedTrigger).not.toBeNull()
+      const insertedTrigger = retrievedTrigger as Trigger
+
+      const result = await markTriggerComplete(dataSource, insertedTrigger, resolverUsername)
+
+      expect(isError(result)).toBeFalsy()
+      expect(result as boolean).toBeTruthy()
+
+      retrievedTrigger = await dataSource.getRepository(Trigger).findOne({ where: { triggerId: trigger.triggerId } })
+      expect(retrievedTrigger).not.toBeNull()
+      const updatedTrigger = retrievedTrigger as Trigger
+
+      expect(updatedTrigger.resolvedAt).toBeDefined()
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const minsSinceResolved = differenceInMinutes(new Date(), updatedTrigger.resolvedAt!)
+      expect(minsSinceResolved).toBeLessThanOrEqual(5)
+
+      expect(updatedTrigger.resolvedBy).toBeDefined()
+      expect(updatedTrigger.resolvedBy).toStrictEqual(resolverUsername)
+    })
+  })
+})
