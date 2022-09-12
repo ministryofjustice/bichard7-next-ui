7fc04552085fcf2203e0c4fa18706260fea1ad5e
Tom Vaughan <tomvaughan77@gmail.com>
2022-09-12 16:48:03 +0100

    Fix unit tests and titles for tables

--- src/components/HearingOutcome/HearingOutcome.stories.tsx
+++ src/components/HearingOutcome/HearingOutcome.stories.tsx
@@ -58,14 +58,23 @@ export const AHO: ComponentStory<typeof HearingOutcome> = () => {
 AHO.play = ({ canvasElement }) => {
   const canvas = within(canvasElement)
 
-  // const blah = canvas.getAllBy
+  const hearingTable = canvas.getByRole("table", { name: "Hearing" })
+  expect(hearingTable).toBeInTheDocument()
+  expect(within(hearingTable).getByText("Court location")).toBeInTheDocument()
+  expect(within(hearingTable).getByText("01234567")).toBeInTheDocument()
 
-  expect(canvas.getByText("Court location")).toBeInTheDocument()
-  expect(canvas.getByText("01234567")).toBeInTheDocument()
+  const caseTable = canvas.getByRole("table", { name: "Case" })
+  expect(caseTable).toBeInTheDocument()
+  expect(within(caseTable).getByText("PTIURN")).toBeInTheDocument()
+  expect(within(caseTable).getByText("123456")).toBeInTheDocument()
 
-  expect(canvas.getByText("PTIURN")).toBeInTheDocument()
-  expect(canvas.getByText("123456")).toBeInTheDocument()
+  const defendantTable = canvas.getByRole("table", { name: "Defendant" })
+  expect(defendantTable).toBeInTheDocument()
+  expect(within(defendantTable).getByText("ASN")).toBeInTheDocument()
+  expect(within(defendantTable).getByText("original_value")).toBeInTheDocument()
 
-  expect(canvas.getByText("ASN")).toBeInTheDocument()
-  expect(canvas.getByText("original_value")).toBeInTheDocument()
+  const offenceTable = canvas.getByRole("table", { name: "Offences" })
+  expect(offenceTable).toBeInTheDocument()
+  expect(within(offenceTable).getByText("1")).toBeInTheDocument()
+  expect(within(offenceTable).getByText("Crime")).toBeInTheDocument()
 }
