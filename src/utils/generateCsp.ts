1178d87d37958c1bcd6cf191e453447f477ca761
Tom Vaughan <tomvaughan77@gmail.com>
2022-09-23 11:45:20 +0100

    Simplify csp generation and add missed bits from existing nginx config

--- src/utils/generateCsp.ts
+++ src/utils/generateCsp.ts
@@ -1,74 +1,9 @@
-// utils/generate-csp.ts
-type Directive =
-  | "child-src"
-  | "connect-src"
-  | "default-src"
-  | "font-src"
-  | "frame-src"
-  | "img-src"
-  | "manifest-src"
-  | "media-src"
-  | "object-src"
-  | "prefetch-src"
-  | "script-src"
-  | "script-src-elem"
-  | "script-src-attr"
-  | "style-src"
-  | "style-src-elem"
-  | "style-src-attr"
-  | "worker-src"
-  | "base-uri"
-  | "plugin-types"
-  | "sandbox"
-  | "form-action"
-  | "frame-ancestors"
-  | "navigate-to"
-  | "report-uri"
-  | "report-to"
-  | "block-all-mixed-content"
-  | "referrer"
-  | "require-sri-for"
-  | "require-trusted-types-for"
-  | "trusted-types"
-  | "upgrade-insecure-requests"
-type Value = string
-interface Options {
-  devOnly?: boolean
-}
-
-interface generateCSPProps {
+interface props {
   nonce?: string
 }
 
-const generateCSP = ({ nonce }: generateCSPProps = {}) => {
-  const policy: Partial<Record<Directive, Value[]>> = {}
-
-  // adder function for our policy object
-  const add = (directive: Directive, value: Value, options: Options = {}) => {
-    if (options.devOnly && process.env.NODE_ENV !== "development") return
-    const curr = policy[directive]
-    policy[directive] = curr ? [...curr, value] : [value]
-  }
-
-  // default-src
-  add("default-src", `'self'`)
-
-  // script-src-elem
-  add("script-src-elem", `'self'`)
-  add("script-src-elem", `'nonce-${nonce}'`)
-
-  // style-src
-  add("style-src", `'self'`)
-  add("style-src", `'nonce-${nonce}'`)
-
-  add("img-src", `'self'`)
-  //   add("img-src", `data:`)
-  //   add("img-src", `w3.org/svg/2000`)
-
-  // return the object in a formatted value (this won't work on IE11 without a polyfill!)
-  return Object.entries(policy)
-    .map(([key, value]) => `${key} ${value.join(" ")}`)
-    .join("; ")
+const generateCSP = ({ nonce }: props = {}) => {
+  return `default-src 'none'; script-src-elem 'self' 'nonce-${nonce}'; style-src 'self' 'nonce-${nonce}'; img-src 'self'; frame-src 'self'; frame-ancestors 'self'; form-action 'self'; object-src 'none'`
 }
 
 export default generateCSP
