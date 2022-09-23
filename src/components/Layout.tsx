0ff7536931200e665287ca52cc5bfb69ba122381
Tom Vaughan <tomvaughan77@gmail.com>
2022-09-23 14:27:41 +0100

    Forked relevant bits of govuk-react library and fixed style-src errors for now

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,14 +2,24 @@ import { Page, Footer, TopNav } from "govuk-react"
 import { addBasePath } from "next/dist/shared/lib/router/router"
 import { ReactNode } from "react"
 import User from "../services/entities/User"
+import IconTitle from "./govuk-react-forked/IconTitle"
+import Crown from "./govuk-react-forked/CrownIcon"
 
 interface Props {
   children: ReactNode
   user: User
 }
 
+const Company = (
+  <IconTitle icon={<Crown width="36" height="32" fill="currentColor" title="GOV.UK" />}>{"GOV.UK"}</IconTitle>
+)
+
 const Layout = ({ children, user }: Props) => {
-  const header = <TopNav serviceTitle={"Bichard7"}>{[user.forenames, user.surname].join(" ")}</TopNav>
+  const header = (
+    <TopNav serviceTitle={"Bichard7"} company={Company}>
+      {[user.forenames, user.surname].join(" ")}
+    </TopNav>
+  )
   return (
     <>
       <Page header={header}>{children}</Page>
