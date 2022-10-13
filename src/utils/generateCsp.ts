84756c98243574f379967bc6d5545e55a8c2e119
Alice Lee <alice.lee@madetech.com>
2022-10-13 11:33:20 +0100

    Fix CSP by adding unsafe-eval and nonce to allowed scripts
    Co-authored-by: Donny Hyon <donny.hyon@madetech.com>
    Co-authored-by: Tom Vaughan <tom.vaughan@madetech.com>

--- src/utils/generateCsp.ts
+++ src/utils/generateCsp.ts
@@ -1,6 +1,6 @@
 const generateCsp = (nonce: string) => {
   console.log(nonce)
-  return `default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self'; frame-src 'self'; form-action 'self'; object-src 'none'; base-uri 'none'`
+  return `default-src 'self'; script-src 'self' 'nonce-${nonce}' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self'; frame-src 'self'; form-action 'self'; object-src 'none'; base-uri 'none'`
 }
 
 export default generateCsp
