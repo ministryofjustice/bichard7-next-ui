e395cc7336df3571e0b4866d8a830e18e87ce06f
Alice Lee <alice.lee@madetech.com>
2022-10-13 11:37:15 +0100

    Remove unneeded console.log
    Co-authored-by: Donny Hyon <donny.hyon@madetech.com>
    Co-authored-by: Tom Vaughan <tom.vaughan@madetech.com>

--- src/utils/generateCsp.ts
+++ src/utils/generateCsp.ts
@@ -1,5 +1,4 @@
 const generateCsp = (nonce: string) => {
-  console.log(nonce)
   return `default-src 'self'; script-src 'self' 'nonce-${nonce}' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self'; frame-src 'self'; form-action 'self'; object-src 'none'; base-uri 'none'`
 }
 
