096286e919dac12a3d64bc3887ab2585e950e96c
Donny <donny.hyon@madetech.com>
2022-10-17 14:54:21 +0100

    Got styles working

--- src/components/Header.stories.tsx
+++ src/components/Header.stories.tsx
@@ -6,8 +6,25 @@ export default {
   component: Header
 } as ComponentMeta<typeof Header>
 
-export const HeaderStory: ComponentStory<typeof Header> = () => <Header serviceName={"test"} />
+export const HeaderStoryEmpty: ComponentStory<typeof Header> = () => <Header serviceName={""} />
+HeaderStoryEmpty.story = {
+  parameters: {
+    nextRouter: {
+      basePath: "/bichard"
+    }
+  }
+}
 
+HeaderStoryEmpty.parameters = {
+  design: [
+    {
+      name: "Design",
+      type: "figma",
+      url: "https://www.figma.com/file/gy3HppiITvQdHAOD2rpO42/05_-B7_22-Completed-initial-components-(for-devs)?node-id=43%3A10"
+    }
+  ]
+}
+export const HeaderStory: ComponentStory<typeof Header> = () => <Header serviceName={"Bichard7"} />
 HeaderStory.story = {
   parameters: {
     nextRouter: {
