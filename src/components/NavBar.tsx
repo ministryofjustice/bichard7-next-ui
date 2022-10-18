7e9ced411c22197f57068b60db314963cd8eb5a5
Donny <donny.hyon@madetech.com>
2022-10-18 11:24:34 +0100

    Broke out NavItem component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,4 @@
-interface Props {
-  NavItemName: string
-  NavItemLink: string
-}
-
-const NavItem: React.FC<Props> = ({ NavItemName, NavItemLink }: Props) => (
-  <li className="moj-primary-navigation__item">
-    <a className="moj-primary-navigation__link" aria-current="page" href={NavItemLink}>
-      {NavItemName}
-    </a>
-  </li>
-)
+import NavItem from "./NavItem"
 
 const NavBar: React.FC = () => (
   <div className="moj-primary-navigation">
@@ -17,6 +6,7 @@ const NavBar: React.FC = () => (
       <div className="moj-primary-navigation__nav">
         <nav className="moj-primary-navigation" aria-label="Primary navigation">
           <ul className="moj-primary-navigation__list">
+            {/* TODO: Add help link */}
             <NavItem NavItemName={"Help"} NavItemLink={"https://www.google.co.uk"} />
           </ul>
         </nav>
