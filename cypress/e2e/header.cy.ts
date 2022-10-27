8bbcbca9e10df9cc29efa512768213e2bbf19121
Alice Lee <monotypical@users.noreply.github.com>
2022-10-27 12:34:54 +0100

    Add link to reports screen from nav bar (#90)

--- cypress/e2e/header.cy.ts
+++ cypress/e2e/header.cy.ts
@@ -1,21 +1,5 @@
-import User from "services/entities/User"
-
 describe("Home", () => {
-  const hashedPassword =
-    "$argon2id$v=19$m=256,t=20,p=2$TTFCN3BRcldZVUtGejQ3WE45TGFqPT0$WOE+jDILDnVIAt1dytb+h65uegrMomp2xb0Q6TxbkLA"
-
   context("720p resolution", () => {
-    const users: Partial<User>[] = Array.from(Array(5)).map((_value, idx) => {
-      return {
-        username: `Bichard0${idx}`,
-        visibleForces: [`0${idx}`],
-        forenames: "Bichard Test User",
-        surname: `0${idx}`,
-        email: `bichard0${idx}@example.com`,
-        password: hashedPassword
-      }
-    })
-
     beforeEach(() => {
       cy.task("clearCourtCases")
       cy.task("clearUsers")
@@ -23,13 +7,25 @@ describe("Home", () => {
     })
 
     context("top-nav", () => {
-      it("should goto help page", () => {
-        cy.task("insertUsers", users)
+      it("should link to other pages", () => {
+        cy.task("insertUsers", [
+          {
+            username: `Bichard01`,
+            visibleForces: [`01`],
+            forenames: "Bichard Test User",
+            surname: `01`,
+            email: `bichard01@example.com`,
+            password:
+              "$argon2id$v=19$m=256,t=20,p=2$TTFCN3BRcldZVUtGejQ3WE45TGFqPT0$WOE+jDILDnVIAt1dytb+h65uegrMomp2xb0Q6TxbkLA"
+          }
+        ])
         cy.task("insertIntoUserGroup", { emailAddress: "bichard01@example.com", groupName: "B7NewUI_grp" })
         cy.login("bichard01@example.com", "password")
         cy.visit("/bichard")
 
+        cy.contains("nav a", "Case List").should("have.attr", "href", "/bichard/")
         cy.contains("nav a", "Help").should("have.attr", "href", "/help/")
+        cy.contains("nav a", "Reports").should("have.attr", "href", "/bichard-ui/ReturnToReportIndex")
       })
     })
   })
