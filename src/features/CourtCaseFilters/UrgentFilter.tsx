4ea032d4544556b1c5f3918943339e4b95d2439a
Alice Lee <alice.lee@madetech.com>
2022-11-02 09:52:23 +0000

    Add checkbox for filtering urgent cases only

---
+++ src/features/CourtCaseFilters/UrgentFilter.tsx
@@ -0,0 +1,27 @@
+import { Checkbox } from "govuk-react"
+import { useRouter } from "next/router"
+
+interface Props {
+  checked: boolean
+}
+
+const UrgentFilter = (props: Props) => {
+  const router = useRouter()
+  const getTargetUrl = (checked: boolean) => {
+    const params = new URLSearchParams({ ...router.query, ...(!checked && { isUrgent: "true" }) })
+    if (checked) {
+      params.delete("isUrgent")
+    }
+    return `?${params}`
+  }
+
+  return (
+    <>
+      <Checkbox defaultChecked={props.checked} onClick={() => router.push(getTargetUrl(props.checked))}>
+        {"Urgent cases only"}
+      </Checkbox>
+    </>
+  )
+}
+
+export default UrgentFilter
