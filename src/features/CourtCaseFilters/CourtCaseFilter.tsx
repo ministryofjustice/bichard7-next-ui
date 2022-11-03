a467ffbff16ad0a1e144679bcfa54450ec66de8f
csabagyorfi <csaba@madetech.com>
2022-11-03 15:09:08 +0000

    Fix misaligned fieldsets in court case filter component

--- src/features/CourtCaseFilters/CourtCaseFilter.tsx
+++ src/features/CourtCaseFilters/CourtCaseFilter.tsx
@@ -88,58 +88,60 @@ const CourtCaseFilter: React.FC<Props> = ({ courtCaseTypes, keywords, urgency }:
               <HintText>{"Defendent name, Court name, Reason, PTIURN"}</HintText>
               <input className="govuk-input" id="keywords" name="keywords" type="text"></input>
             </div>
-          </div>
-          <div className="govuk-form-group">
-            <fieldset className="govuk-fieldset">
-              <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">{"Case type"}</legend>
-              <div className="govuk-checkboxes govuk-checkboxes--small" data-module="govuk-checkboxes">
-                <div className="govuk-checkboxes__item">
-                  <input
-                    className="govuk-checkboxes__input"
-                    id="exceptions-type"
-                    name="type"
-                    type="checkbox"
-                    value="Exceptions"
-                    defaultChecked={courtCaseTypes.includes("Exceptions")}
-                  ></input>
-                  <label className="govuk-label govuk-checkboxes__label" htmlFor="exceptions-type">
-                    {"Exceptions"}
-                  </label>
+            <div className="govuk-form-group">
+              <fieldset className="govuk-fieldset">
+                <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">{"Case type"}</legend>
+                <div className="govuk-checkboxes govuk-checkboxes--small" data-module="govuk-checkboxes">
+                  <div className="govuk-checkboxes__item">
+                    <input
+                      className="govuk-checkboxes__input"
+                      id="exceptions-type"
+                      name="type"
+                      type="checkbox"
+                      value="Exceptions"
+                      defaultChecked={courtCaseTypes.includes("Exceptions")}
+                    ></input>
+                    <label className="govuk-label govuk-checkboxes__label" htmlFor="exceptions-type">
+                      {"Exceptions"}
+                    </label>
+                  </div>
+                  <div className="govuk-checkboxes__item">
+                    <input
+                      className="govuk-checkboxes__input"
+                      id="triggers-type"
+                      name="type"
+                      type="checkbox"
+                      value="Triggers"
+                      defaultChecked={courtCaseTypes.includes("Triggers")}
+                    ></input>
+                    <label className="govuk-label govuk-checkboxes__label" htmlFor="triggers-type">
+                      {"Triggers"}
+                    </label>
+                  </div>
                 </div>
-                <div className="govuk-checkboxes__item">
-                  <input
-                    className="govuk-checkboxes__input"
-                    id="triggers-type"
-                    name="type"
-                    type="checkbox"
-                    value="Triggers"
-                    defaultChecked={courtCaseTypes.includes("Triggers")}
-                  ></input>
-                  <label className="govuk-label govuk-checkboxes__label" htmlFor="triggers-type">
-                    {"Triggers"}
-                  </label>
-                </div>
-              </div>
-            </fieldset>
-          </div>
-          <fieldset className="govuk-fieldset">
-            <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">{"Urgency"}</legend>
-            <div className="govuk-checkboxes govuk-checkboxes--small" data-module="govuk-checkboxes">
-              <div className="govuk-checkboxes__item">
-                <input
-                  className="govuk-checkboxes__input"
-                  id="is-urgent-filter"
-                  name="urgency"
-                  type="checkbox"
-                  value="Urgent"
-                  defaultChecked={urgency}
-                ></input>
-                <label className="govuk-label govuk-checkboxes__label" htmlFor="is-urgent-filter">
-                  {"Urgent"}
-                </label>
-              </div>
+              </fieldset>
             </div>
-          </fieldset>
+            <div className="govuk-form-group">
+              <fieldset className="govuk-fieldset">
+                <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">{"Urgency"}</legend>
+                <div className="govuk-checkboxes govuk-checkboxes--small" data-module="govuk-checkboxes">
+                  <div className="govuk-checkboxes__item">
+                    <input
+                      className="govuk-checkboxes__input"
+                      id="is-urgent-filter"
+                      name="urgency"
+                      type="checkbox"
+                      value="Urgent"
+                      defaultChecked={urgency}
+                    ></input>
+                    <label className="govuk-label govuk-checkboxes__label" htmlFor="is-urgent-filter">
+                      {"Urgent"}
+                    </label>
+                  </div>
+                </div>
+              </fieldset>
+            </div>
+          </div>
         </div>
       </div>
     </form>
