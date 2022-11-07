4c87f73a5cf991431a4909a8d666e499226d246c
csabagyorfi <csaba@madetech.com>
2022-11-07 15:53:22 +0000

    Fix favicon path in cypress test
    Co-authored-by: joe-fol <joe.folkard@madetech.com>
    Co-authohed-by: Donny Hyon <donny.hyon@madetech.com>

--- cypress/e2e/assets.cy.ts
+++ cypress/e2e/assets.cy.ts
@@ -28,20 +28,18 @@ describe("GOV.UK Assets", () => {
       .should("have.attr", "image", "[object Object]")
   })
 
-  it("should provide favicon icon that loads correctly", () => {
+  it.only("should provide favicon icon that loads correctly", () => {
     cy.visit("/bichard")
     cy.get("link[rel='shortcut icon']")
       .should("have.attr", "href")
       .then((iconHref) => {
-        // cy.request automatically uses Cypress' defined baseUrl, which
-        // includes /users already - so we need to strip it out here to get it
-        // to work
-        const iconUrl = (iconHref as unknown as string).replace("/bichard", "")
+        const iconUrl = iconHref as unknown as string
         cy.request({
           url: iconUrl,
           failOnStatusCode: false
         }).then((resp) => {
           expect(resp.status).not.to.equal(404)
+          expect(resp.status).to.equal(200)
           expect(resp.body).not.to.equal(undefined)
         })
       })
