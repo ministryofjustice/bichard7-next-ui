1ed0ea62f14e3abbdf946ac87d77916bf4cba838
csabagyorfi <csaba@madetech.com>
2022-11-15 14:07:00 +0000

    Refactor: update NamedDateRangeOptions return type
    Co-authored-by: Alice Lee <alice.lee@madetech.com>
    Co-authored-by: Donny Hyon <donny.hyon@madetech.com>

--- src/utils/validators/validateDateRanges.ts
+++ src/utils/validators/validateDateRanges.ts
@@ -2,13 +2,15 @@ import { CourtDateRange, NamedCourtDateRanges } from "types/CaseListQueryParams"
 import KeyValuePair from "types/KeyValuePair"
 
 const validCourtDateRanges = ["Today"]
+const NamedDateRangeOptions: KeyValuePair<string, () => CourtDateRange> = {
+  Today: () => {
+    return { from: new Date(), to: new Date() }
+  }
+}
 
 export const validateDateRanges = (dateRange: string | string[] | undefined): CourtDateRange | undefined => {
-  const NamedDateRangeOptions: KeyValuePair<string, CourtDateRange> = {
-    Today: { from: new Date(), to: new Date() }
-  }
   const validatedDateRangeNames = [dateRange]
     .flat()
     .filter((range) => validCourtDateRanges.includes(String(range))) as NamedCourtDateRanges[]
-  return validatedDateRangeNames.map((range) => range && NamedDateRangeOptions[range])[0]
+  return validatedDateRangeNames.map((range) => range && NamedDateRangeOptions[range]())[0]
 }
