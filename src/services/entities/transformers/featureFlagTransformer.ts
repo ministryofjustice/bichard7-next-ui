e01566a45d322da930ce487799cdeffcb53190df
Alice Lee <alice.lee@madetech.com>
2023-01-11 15:13:55 +0000

    Handle unescaped json values for feature_flags column
    Co-authored-by: csabagyorfi <csaba@madetech.com>

--- src/services/entities/transformers/featureFlagTransformer.ts
+++ src/services/entities/transformers/featureFlagTransformer.ts
@@ -2,7 +2,7 @@ import { ValueTransformer } from "typeorm"
 
 const featureFlagTransformer: ValueTransformer = {
   to: (value) => JSON.stringify(value),
-  from: (value) => (value && value !== null && Object.keys(value)?.length > 0 ? JSON.parse(value) : {})
+  from: (value) => (value && value !== null && Object.keys(value)?.length > 0 ? JSON.parse(JSON.stringify(value)) : {})
 }
 
 export default featureFlagTransformer
