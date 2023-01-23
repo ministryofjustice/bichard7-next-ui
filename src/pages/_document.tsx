132d3de754bae90605d6529daf70a39381de1d2b
Alice Lee <monotypical@users.noreply.github.com>
2023-01-23 15:19:19 +0000

    Don't show unstyled content during page loads (#155)
    * Add next plugin to eslint
    
    * Include styles from styled-components for SSR
    
    See https://styled-components.com/docs/advanced#server-side-rendering for more details
    
    * Remove unused import
    
    * Include react-jss styles in server-side rendered pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from "next/document"
 import React from "react"
+import { createGenerateId, JssProvider, SheetsRegistry } from "react-jss"
+import { ServerStyleSheet } from "styled-components"
 import generateCsp from "utils/generateCsp"
 import generateNonce from "utils/generateNonce"
 import { basePath } from "../../next.config"
@@ -51,16 +53,51 @@ class GovUkDocument extends Document<DocumentProps> {
     const nonce = generateNonce()
     ctx.res?.setHeader("Content-Security-Policy", generateCsp(nonce))
 
-    const initialProps = await Document.getInitialProps(ctx)
-    const additionalProps = { nonce }
-    return {
-      ...initialProps,
-      ...additionalProps
+    // styled-components
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    // react-jss
+    const registry = new SheetsRegistry()
+    const generateId = createGenerateId()
+
+    // include styles from both styled-components and react-jss
+    try {
+      ctx.renderPage = () => {
+        return originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            (
+              <JssProvider registry={registry} generateId={generateId}>
+                {sheet.collectStyles(<App {...props} />)}
+              </JssProvider>
+            )
+        })
+      }
+
+      const initialProps = await Document.getInitialProps(ctx)
+      const additionalProps = {
+        nonce,
+        styles: [
+          initialProps.styles,
+          sheet.getStyleElement(),
+          <style key="react-jss" id="server-side-styles-react-jss">
+            {registry.toString()}
+          </style>
+        ]
+      }
+
+      return {
+        ...initialProps,
+        ...additionalProps
+      }
+    } finally {
+      sheet.seal()
     }
   }
 
   render() {
     const { nonce } = this.props
+
     return (
       <Html className="govuk-template" lang="en">
         <Head>
