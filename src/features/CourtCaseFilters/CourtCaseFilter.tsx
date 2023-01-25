561bdb9ad154a95a9699b822130eb91e377a5803
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-01-25 13:26:34 +0000

    Move ExpandingFilters to a new component

--- src/features/CourtCaseFilters/CourtCaseFilter.tsx
+++ src/features/CourtCaseFilters/CourtCaseFilter.tsx
@@ -1,20 +1,18 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import CaseStateFilterOptions from "components/CaseStateFilter/CaseStateFilterOptions"
 import CourtCaseTypeOptions from "components/CourtDateFilter/CourtCaseTypeOptions"
 import UrgencyFilterOptions from "components/CourtDateFilter/UrgencyFilterOptions"
 import If from "components/If"
 import LockedFilterOptions from "components/LockedFilter/LockedFilterOptions"
 import { LabelText } from "govuk-react"
-import { ReactNode, useState, useReducer } from "react"
-import { createUseStyles } from "react-jss"
+import { useReducer } from "react"
 import { CaseState, Reason } from "types/CaseListQueryParams"
 import type { Filter, FilterAction } from "types/CourtCaseFilter"
 import { anyFilterChips } from "utils/filterChips"
 import CourtDateFilterOptions from "../../components/CourtDateFilter/CourtDateFilterOptions"
 import FilterChipSection from "./FilterChipSection"
 import { caseStateLabels } from "utils/caseStateFilters"
+import { createUseStyles } from "react-jss"
+import ExpandingFilters from "./ExpandingFilters"
 
 interface Props {
   defendantName: string | null
@@ -101,80 +99,12 @@ const reducer = (state: Filter, action: FilterAction): Filter => {
   }
   return newState
 }
+
 const useStyles = createUseStyles({
-  legendColour: {
-    color: "#1D70B8"
-  },
-  legendContainer: {
-    marginTop: "8px"
-  },
-  iconButton: {
-    border: "3px solid transparent",
-    backgroundColor: "transparent",
-    "&:active": {
-      backgroundColor: "#b0b4b6"
-    }
-  },
-  container: {
-    marginLeft: "-10px",
-    width: "fit-content",
-    paddingRight: "10px",
-    display: "flex",
-    backgroundColor: "transparent",
-    "&:hover": {
-      backgroundColor: "#b0b4b6"
-    },
-    "&:active": {
-      backgroundColor: "#b0b4b6"
-    }
-  },
   "govuk-form-group": {
     marginBottom: "0"
   }
 })
-
-const UpArrow: React.FC = () => (
-  <svg width={18} height={10} viewBox="0 0 18 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M0.999926 9.28432L8.74976 1.56866L16.4996 9.28432" stroke="#0B0C0C" strokeWidth={2} />
-  </svg>
-)
-
-const DownArrow: React.FC = () => (
-  <svg width={18} height={11} viewBox="0 0 18 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M16.9994 1.26702L9.26685 9L1.49977 1.30171" stroke="#0B0C0C" strokeWidth={2} />
-  </svg>
-)
-
-const ExpandingFilters: React.FC<{ filterName: string; children: ReactNode }> = ({
-  filterName,
-  children
-}: {
-  filterName: string
-  children: ReactNode
-}) => {
-  const [caseTypeIsVisible, setCaseTypeVisible] = useState(true)
-  const classes = useStyles()
-  return (
-    <fieldset className="govuk-fieldset">
-      <div
-        className={classes.container}
-        onClick={() => {
-          setCaseTypeVisible(!caseTypeIsVisible)
-        }}
-      >
-        <button type="button" className={classes.iconButton} aria-label={`${filterName} filter options`}>
-          {caseTypeIsVisible ? <UpArrow /> : <DownArrow />}
-        </button>
-        <div className={classes.legendContainer}>
-          <legend className="govuk-fieldset__legend govuk-fieldset__legend--s">
-            <div className={classes.legendColour}>{filterName}</div>
-          </legend>
-        </div>
-      </div>
-      <If condition={caseTypeIsVisible}>{children}</If>
-    </fieldset>
-  )
-}
 
 const CourtCaseFilter: React.FC<Props> = ({
   courtCaseTypes,
