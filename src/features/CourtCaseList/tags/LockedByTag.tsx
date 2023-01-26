312ee885575074a6f5d6eeecad5c72f21857b5ff
Alice Lee <alice.lee@madetech.com>
2023-01-26 16:06:53 +0000

    Tweak padding in "Locked By" tag

--- src/features/CourtCaseList/tags/LockedByTag.tsx
+++ src/features/CourtCaseList/tags/LockedByTag.tsx
@@ -5,19 +5,21 @@ import { createUseStyles } from "react-jss"
 
 const useStyles = createUseStyles({
   LockedByTag: {
-    display: "flex",
+    display: "inline-flex",
     flexDirection: "row",
-    flexWrap: "nowrap"
+    flexWrap: "nowrap",
+    alignItems: "center",
+    gap: 5
   },
   LockedIcon: {
     // Change colour from black to #144e81 (GDS blue-text) with CSS filters
     filter: "invert(12%) sepia(70%) saturate(4629%) hue-rotate(197deg) brightness(97%) contrast(84%)",
-    flexGrow: 0,
-    verticalAlign: "middle",
-    alignSelf: "center"
   },
   LockedByText: {
-    textDecoration: "underline"
+    textDecoration: "underline",
+    marginTop: 4,
+    marginBottom: 2,
+    fontWeight: "normal"
   }
 })
 
@@ -26,14 +28,16 @@ const LockedByTag: React.FC<{ lockedBy?: string | null }> = (props: { lockedBy?:
   return (
     <If condition={!!props.lockedBy}>
       <Tag backgroundColor="#e9f1f8" color="#114e81" className={`locked-by-tag ${classes.LockedByTag}`}>
-        <Image
-          src={"/bichard/assets/images/lock.svg"}
-          width={20}
-          height={20}
-          className={classes.LockedIcon}
-          alt="Lock icon"
-        />
-        <span className={classes.LockedByText}>{props.lockedBy}</span>
+        <div className={classes.LockedByTag}>
+          <Image
+            src={"/bichard/assets/images/lock.svg"}
+            width={18}
+            height={18}
+            className={classes.LockedIcon}
+            alt="Lock icon"
+          />
+          <span className={classes.LockedByText}>{props.lockedBy}</span>
+        </div>
       </Tag>
     </If>
   )
