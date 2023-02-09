0c76f3681da585e0735994241b7021e702d1fa5e
Donny <donny.hyon@madetech.com>
2023-02-09 15:00:59 +0000

    Failing test WIP

--- src/utils/deleteQueryParam/deleteQueryParamsByName.test.ts
+++ src/utils/deleteQueryParam/deleteQueryParamsByName.test.ts
@@ -1,24 +1,25 @@
 import deleteQueryParamsByName from "./deleteQueryParamsByName"
 
 describe("deleteQueryParams", () => {
+  beforeEach(() => {
+    const query = new URLSearchParams()
+    query.append("defendant", "Name")
+    query.append("type", "exeptions")
+    query.append("type", "triggers")
+  })
   it("can delete a named parameter from search queries", () => {
-    const query = { defendant: "Name", type: ["exceptions", "triggers"] }
-
     expect(deleteQueryParamsByName(["defendant"], query)).toStrictEqual({ type: ["exceptions", "triggers"] })
   })
 
   it("should not modify query when deleting a parameter that does not exist", () => {
-    const query = { defendant: "Name", type: ["exceptions", "triggers"] }
     expect(deleteQueryParamsByName(["invalid key"], query)).toStrictEqual(query)
   })
 
   it("should delete a named parameter with multiple values", () => {
-    const query = { defendant: "Name", type: ["exceptions", "triggers"] }
     expect(deleteQueryParamsByName(["type"], query)).toStrictEqual({ defendant: "Name" })
   })
 
   it("should delete multiple named parameters", () => {
-    const query = { defendant: "Name", type: ["exceptions", "triggers"] }
     expect(deleteQueryParamsByName(["type", "defendant"], query)).toStrictEqual({})
   })
 })
