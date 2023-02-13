d4c2a1a0906a538869ca2bcf9e540dc4aee83435
Donny <donny.hyon@madetech.com>
2023-02-13 14:49:26 +0000

    removed unecessary loop in validateCustomDateRange function

--- src/utils/validators/validateCustomDateRange.ts
+++ src/utils/validators/validateCustomDateRange.ts
@@ -1,21 +1,9 @@
 import { CourtDateRange } from "types/CaseListQueryParams"
-import { validateQueryParams } from "./validateQueryParams"
 
 export const validateCustomDateRange = (customDate: {
   from: string | string[] | undefined
   to: string | string[] | undefined
 }): CourtDateRange | undefined => {
-  let hasInvalidParam = false
-  Object.values(customDate).forEach((param) => {
-    if (!validateQueryParams(param)) {
-      hasInvalidParam = true
-    }
-  })
-
-  if (hasInvalidParam) {
-    return undefined
-  }
-
   const fromTimeStamp = Date.parse(`${customDate.from}`)
   const toTimeStamp = Date.parse(`${customDate.to}`)
 
