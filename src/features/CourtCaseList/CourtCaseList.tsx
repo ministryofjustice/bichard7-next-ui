a522035ee237fc38b4164e6377e086b1a97ab647
joe-fol <joe.folkard@madetech.com>
2023-02-14 10:24:55 +0000

    Added svg for sorting columns

--- src/features/CourtCaseList/CourtCaseList.tsx
+++ src/features/CourtCaseList/CourtCaseList.tsx
@@ -1,20 +1,20 @@
 import DateTime from "components/DateTime"
 import If from "components/If"
+import { GridRow, Link, Paragraph, Table } from "govuk-react"
 import Image from "next/image"
 import { useRouter } from "next/router"
+import { encode } from "querystring"
+import { createUseStyles } from "react-jss"
 import CourtCase from "services/entities/CourtCase"
-import { Table, Link, GridRow, Paragraph } from "govuk-react"
+import User from "services/entities/User"
 import type { QueryOrder } from "types/CaseListQueryParams"
+import { deleteQueryParamsByName } from "utils/deleteQueryParam"
+import getTriggerWithDescription from "utils/formatReasons/getTriggerWithDescription"
+import groupErrorsFromReport from "utils/formatReasons/groupErrorsFromReport"
+import { displayedDateFormat } from "utils/formattedDate"
 import LockedByTag from "./tags/LockedByTag"
 import NotesTag from "./tags/NotesTag"
 import UrgentTag from "./tags/UrgentTag"
-import groupErrorsFromReport from "utils/formatReasons/groupErrorsFromReport"
-import getTriggerWithDescription from "utils/formatReasons/getTriggerWithDescription"
-import { createUseStyles } from "react-jss"
-import User from "services/entities/User"
-import { deleteQueryParamsByName } from "utils/deleteQueryParam"
-import { encode } from "querystring"
-import { displayedDateFormat } from "utils/formattedDate"
 
 const useStyles = createUseStyles({
   caseDetailsRow: {
@@ -31,6 +31,29 @@ interface Props {
   order?: QueryOrder
   currentUser: User
 }
+
+const UpArrow: React.FC = () => (
+  <svg width={15} height={25} viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 14L7.5 5L13 14H2Z" fill="#1D70B8" />
+  </svg>
+)
+
+const DownArrow: React.FC = () => (
+  <svg width={15} height={25} viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M13 11L7.5 20L2 11L13 11Z" fill="#1D70B8" />
+  </svg>
+)
+
+const Unordered: React.FC = () => (
+  <svg width={15} height={25} viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 11L7.5 3L11 11H4Z" fill="#1D70B8" />
+    <path d="M11 13L7.5 21L4 13L11 13Z" fill="#1D70B8" />
+  </svg>
+)
+
+console.log(DownArrow)
+console.log(UpArrow)
+console.log(Unordered)
 
 const CourtCaseList: React.FC<Props> = ({ courtCases, order = "asc", currentUser }: Props) => {
   const classes = useStyles()
