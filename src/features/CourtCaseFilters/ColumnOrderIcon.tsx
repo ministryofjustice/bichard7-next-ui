492fab2adad36040926e7bf25b73477b5d8d9c02
Donny <donny.hyon@madetech.com>
2023-02-15 15:17:31 +0000

    added JSS

--- src/features/CourtCaseFilters/ColumnOrderIcon.tsx
+++ src/features/CourtCaseFilters/ColumnOrderIcon.tsx
@@ -2,6 +2,21 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import If from "components/If"
 import { ReactNode } from "react"
+import { createUseStyles } from "react-jss"
+
+const useStyles = createUseStyles({
+  container: {
+    width: "fit-content",
+    display: "flex"
+  },
+  content: {
+    display: "inline-block"
+  },
+  icon: {
+    display: "inline-block",
+    paddingLeft: "2px"
+  }
+})
 
 interface Props {
   orderBy: string | string[] | undefined
@@ -10,11 +25,15 @@ interface Props {
   children: ReactNode
 }
 
-const UpArrow: React.FC = () => (
-  <svg width={15} height={25} viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2 14L7.5 5L13 14H2Z" fill="#1D70B8" />
-  </svg>
-)
+const UpArrow: React.FC = () => {
+  return (
+    <div>
+      <svg width={15} height={25} viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2 14L7.5 5L13 14H2Z" fill="#1D70B8" />
+      </svg>
+    </div>
+  )
+}
 
 const DownArrow: React.FC = () => (
   <svg width={15} height={25} viewBox="0 0 15 25" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -30,21 +49,34 @@ const Unordered: React.FC = () => (
 )
 
 const ColumnOrderIcon: React.FC<Props> = ({ orderBy, currentOrder, columnName, children }) => {
+  const classes = useStyles()
   return (
-    <>
+    <div>
       <If condition={orderBy === undefined || orderBy !== columnName}>
-        {children}
-        <Unordered />
+        <div className={classes.container}>
+          <div className={classes.content}>{children}</div>
+          <div className={classes.icon}>
+            <Unordered />
+          </div>
+        </div>
       </If>
       <If condition={currentOrder === "asc" && orderBy === columnName}>
-        {children}
-        <UpArrow />
+        <div className={classes.container}>
+          <div className={classes.content}>{children}</div>
+          <div className={classes.icon}>
+            <UpArrow />
+          </div>
+        </div>
       </If>
       <If condition={currentOrder === "desc" && orderBy === columnName}>
-        {children}
-        <DownArrow />
+        <div className={classes.container}>
+          <div className={classes.content}>{children}</div>
+          <div className={classes.icon}>
+            <DownArrow />
+          </div>
+        </div>
       </If>
-    </>
+    </div>
   )
 }
 
