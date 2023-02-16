af5310445878cb50210500353595d1837da519d4
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-02-16 15:19:38 +0000

    Use moj-badge for the resolved tag

--- src/features/CourtCaseList/tags/ResolvedTag.tsx
+++ src/features/CourtCaseList/tags/ResolvedTag.tsx
@@ -1,19 +1,12 @@
 import If from "components/If"
-import { Tag } from "govuk-react"
-import { textGrey } from "utils/colours"
 import { useCustomStyles } from "../../../../styles/customStyles"
 
 const ResolvedTag: React.FC<{ isResolved: boolean }> = (props: { isResolved: boolean }) => {
   const classes = useCustomStyles()
+
   return (
     <If condition={props.isResolved}>
-      <Tag
-        backgroundColor="white"
-        color={textGrey}
-        className={`${classes["border-grey"]} ${classes["margin-top-bottom"]}`}
-      >
-        {"Resolved"}
-      </Tag>
+      <span className={`moj-badge moj-badge--grey ${classes["margin-top-bottom"]}`}>{"Resolved"}</span>
     </If>
   )
 }
