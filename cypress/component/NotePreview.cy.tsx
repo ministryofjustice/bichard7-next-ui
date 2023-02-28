d9d9957eec857cc703956cfbdf41ee44137bcc15
Donny <donny.hyon@madetech.com>
2023-02-28 14:46:41 +0000

    added more assertions to component tests

--- cypress/component/NotePreview.cy.tsx
+++ cypress/component/NotePreview.cy.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable cypress/no-async-tests */
 import { HideButton, PreviewButton } from "../../src/features/CourtCaseList/NotePreviewButton"
-describe("chevron icons check", () => {
+describe("NotePreview.cy.tx", () => {
   it("shows the default down chevron with the label 'preview'", () => {
     cy.mount(<PreviewButton />)
     cy.get(".govuk-accordion-nav__chevron--down").should("exist")
+    cy.get(".govuk-accordion-nav__chevron").should("exist")
   })
 
   it("shows the default up chevron with the label 'hide'", () => {
     cy.mount(<HideButton />)
     cy.get(".govuk-accordion-nav__chevron").should("exist")
+    cy.get(".govuk-accordion-nav__chevron--down").should("not", "exist")
   })
 })
