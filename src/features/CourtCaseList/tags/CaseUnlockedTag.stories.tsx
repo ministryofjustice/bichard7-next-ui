0bdfba31c4c54869a775c8abf6193ea9d2fe8cfa
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-03-06 16:46:13 +0000

    Remove redundant await from storybook

--- src/features/CourtCaseList/tags/CaseUnlockedTag.stories.tsx
+++ src/features/CourtCaseList/tags/CaseUnlockedTag.stories.tsx
@@ -9,13 +9,13 @@ export default {
 } as ComponentMeta<typeof CaseUnlockedTag>
 
 export const CaseNotUnlocked: ComponentStory<typeof CaseUnlockedTag> = () => <CaseUnlockedTag isCaseUnlocked={false} />
-CaseNotUnlocked.play = async ({ canvasElement }) => {
+CaseNotUnlocked.play = ({ canvasElement }) => {
   const canvas = within(canvasElement)
-  await expect(canvas.queryByText("Case unlocked")).toBeNull()
+  expect(canvas.queryByText("Case unlocked")).toBeNull()
 }
 
 export const CaseUnlocked: ComponentStory<typeof CaseUnlockedTag> = () => <CaseUnlockedTag isCaseUnlocked={true} />
-CaseUnlocked.play = async ({ canvasElement }) => {
+CaseUnlocked.play = ({ canvasElement }) => {
   const canvas = within(canvasElement)
-  await expect(canvas.getByText("Case unlocked")).toBeInTheDocument()
+  expect(canvas.getByText("Case unlocked")).toBeInTheDocument()
 }
