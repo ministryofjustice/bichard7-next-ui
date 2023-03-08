8ac1c308e17af9ee4805f61bac2ffec46d4260e8
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-03-08 11:20:05 +0000

    Add yellow colour variable

--- src/utils/colours.ts
+++ src/utils/colours.ts
@@ -3,5 +3,6 @@ const gdsLightGrey = "#f3f2f1"
 const tagBlue = "#e9f1f8"
 const textBlue = "#114e81"
 const navy = "#1D70B8"
+const yellow = "#FFDD00"
 
-export { darkGrey, tagBlue, textBlue, gdsLightGrey, navy }
+export { darkGrey, tagBlue, textBlue, gdsLightGrey, navy, yellow }
