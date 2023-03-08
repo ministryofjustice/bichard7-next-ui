4ea4cb101c3269a83eca43328e2d3503c91732d5
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-03-08 11:16:20 +0000

    Group colours together

--- src/utils/colours.ts
+++ src/utils/colours.ts
@@ -1,6 +1,6 @@
 const darkGrey = "#62696D"
+const gdsLightGrey = "#f3f2f1"
 const tagBlue = "#e9f1f8"
 const textBlue = "#114e81"
-const gdsLightGrey = "#f3f2f1"
 
 export { darkGrey, tagBlue, textBlue, gdsLightGrey }
