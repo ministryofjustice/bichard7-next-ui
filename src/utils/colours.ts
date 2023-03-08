642b80058fef4c40b470ecdb407da9ba46254ea1
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-03-08 11:17:15 +0000

    Add navy colour

--- src/utils/colours.ts
+++ src/utils/colours.ts
@@ -2,5 +2,6 @@ const darkGrey = "#62696D"
 const gdsLightGrey = "#f3f2f1"
 const tagBlue = "#e9f1f8"
 const textBlue = "#114e81"
+const navy = "#1D70B8"
 
-export { darkGrey, tagBlue, textBlue, gdsLightGrey }
+export { darkGrey, tagBlue, textBlue, gdsLightGrey, navy }
