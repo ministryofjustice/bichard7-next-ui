9be098f81a8d05fbc7a003ad93e0f1bbf907070c
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-04-12 13:55:54 +0100

    Fix case details cy test

--- cypress/e2e/court-cases/caseDetails.cy.ts
+++ cypress/e2e/court-cases/caseDetails.cy.ts
@@ -105,7 +105,8 @@ describe("Case details", () => {
       cy.get("table").eq(-1).find("tr").eq(1).find("td").eq(4).should("include.text", "09/07/2022")
 
       // Notes
-      cy.get("H3").contains("Notes")
+      cy.contains("Notes").click()
+      cy.get("H4").contains("Notes")
       cy.get("p").contains("Case has no notes.")
 
       // Urgency
