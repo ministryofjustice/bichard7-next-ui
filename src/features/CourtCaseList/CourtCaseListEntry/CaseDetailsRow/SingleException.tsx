90d7b89f76a1ea6e914ee142a147a8d2ebc4a4e7
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-04-21 13:01:48 +0100

    Rename Exception to SingleException

--- src/features/CourtCaseList/CourtCaseListEntry/CaseDetailsRow/SingleException.tsx
+++ src/features/CourtCaseList/CourtCaseListEntry/CaseDetailsRow/SingleException.tsx
@@ -1,9 +1,9 @@
-interface ExceptionProps {
+interface SingleExceptionProps {
   exception: string
   exceptionCounter: number
 }
 
-export const Exception = ({ exception, exceptionCounter }: ExceptionProps) => (
+export const SingleException = ({ exception, exceptionCounter }: SingleExceptionProps) => (
   <span>
     {exception}
     <b>&nbsp;{exceptionCounter > 1 ? `(${exceptionCounter})` : ""}</b>
