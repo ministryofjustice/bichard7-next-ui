b5446a37be3793893ed2eb54090e1608d402d151
Emad Karamad <emad.karamad@madetech.com>
2023-04-24 09:42:55 +0100

    Update groupErrorsFromReport to also return field names

--- src/utils/formatReasons/groupErrorsFromReport.test.ts
+++ src/utils/formatReasons/groupErrorsFromReport.test.ts
@@ -2,12 +2,28 @@ import groupErrorsFromReport from "./groupErrorsFromReport"
 
 describe("groupErrorsFromReport", () => {
   it("can map all exceptions from the error report", () => {
-    const errorReport = "HO100322||ds:OrganisationUnitCode, HO100323||ds:NextHearingDate HO100310||ds:NextHearingDate"
-    expect(groupErrorsFromReport(errorReport)).toStrictEqual({ HO100322: 1, HO100323: 1, HO100310: 1 })
+    const errorReport = "HO100322||ds:OrganisationUnitCode, HO100323||ds:NextHearingDate, HO100310||ds:NextHearingDate"
+    expect(groupErrorsFromReport(errorReport)).toStrictEqual({
+      HO100322: { count: 1, fields: [{ name: "OrganisationUnitCode", displayName: "Organisation unit code" }] },
+      HO100323: { count: 1, fields: [{ name: "NextHearingDate", displayName: "Next hearing date" }] },
+      HO100310: { count: 1, fields: [{ name: "NextHearingDate", displayName: "Next hearing date" }] }
+    })
   })
 
   it("can group multiple occurrences of a code", () => {
-    const errorReport = "HO100322||ds:OrganisationUnitCode, HO100322||ds:NextHearingDate HO100310||ds:NextHearingDate"
-    expect(groupErrorsFromReport(errorReport)).toStrictEqual({ HO100322: 2, HO100310: 1 })
+    const errorReport = "HO100322||ds:OrganisationUnitCode, HO100322||ds:NextHearingDate, HO100310||ds:NextHearingDate"
+    expect(groupErrorsFromReport(errorReport)).toStrictEqual({
+      HO100322: {
+        count: 2,
+        fields: [
+          { name: "OrganisationUnitCode", displayName: "Organisation unit code" },
+          {
+            name: "NextHearingDate",
+            displayName: "Next hearing date"
+          }
+        ]
+      },
+      HO100310: { count: 1, fields: [{ name: "NextHearingDate", displayName: "Next hearing date" }] }
+    })
   })
 })
--- src/utils/formatReasons/groupErrorsFromReport.ts
+++ src/utils/formatReasons/groupErrorsFromReport.ts
@@ -1,11 +1,31 @@
 import type { KeyValuePair } from "types/KeyValuePair"
 
-const groupErrorsFromReport = (errorReport: string): KeyValuePair<string, number> => {
-  const errorsCodes: KeyValuePair<string, number> = {}
-  errorReport.match(/(HO)[0-9]{1,9}/g)?.forEach((code) => {
-    errorsCodes[code] = errorsCodes[code] ? errorsCodes[code] + 1 : (errorsCodes[code] = 1)
-  })
-  return errorsCodes
+type ErrorResult = { count: number; fields: { name: string; displayName: string }[] }
+
+const formatFieldName = (name: string) => {
+  const words = name.match(/([A-Z]+[a-z]+)/g)
+  if (words) {
+    const firstWord = words[0]
+    return firstWord + words.join(" ").slice(firstWord.length).toLowerCase()
+  }
+
+  return name
+}
+
+const groupErrorsFromReport = (errorReport: string): KeyValuePair<string, ErrorResult> => {
+  return errorReport.split(",").reduce((errors, report) => {
+    const code = report.split("||")[0].trim()
+    const field = report.split("||")[1]?.split(":")[1]?.trim()
+    errors[code] = errors[code] ?? { count: 0, fields: [] }
+    errors[code].count += 1
+    if (field) {
+      errors[code].fields.push({
+        name: field,
+        displayName: formatFieldName(field)
+      })
+    }
+    return errors
+  }, {} as KeyValuePair<string, ErrorResult>)
 }
 
 export default groupErrorsFromReport
