6e4d9f9471ff411bba4cae41798e7c15a0d1786c
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-04-25 16:50:40 +0100

    Add gdsGrey colour variable

--- src/utils/colours.ts
+++ src/utils/colours.ts
@@ -1,5 +1,6 @@
 const darkGrey = "#62696D"
 const lightGrey = "#FAFAFA"
+const gdsGrey = "#bfc1c3"
 const gdsLightGrey = "#f3f2f1"
 
 const tagBlue = "#e9f1f8"
@@ -8,4 +9,4 @@ const blue = "#1D70B8"
 
 const yellow = "#FFDD00"
 
-export { darkGrey, lightGrey, gdsLightGrey, blue, tagBlue, textBlue, yellow }
+export { darkGrey, lightGrey, gdsGrey, gdsLightGrey, blue, tagBlue, textBlue, yellow }
