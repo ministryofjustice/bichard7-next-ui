f9eb4de10b60dd8436d61ced4f336ee1094b16de
Emad Karamad <emad.karamad@madetech.com>
2023-04-26 09:23:04 +0100

    Add NavigationLocations type

--- src/types/NavigationLocations.ts
+++ src/types/NavigationLocations.ts
@@ -1,3 +1,7 @@
-type NavigationLocations = "Case Details > Hearing" | "Case Details > Offences" | "Case Details > Case information"
+type NavigationLocations =
+  | "Case Details > Hearing"
+  | "Case Details > Offences"
+  | "Case Details > Case information"
+  | "Case Details > Defendant"
 
 export default NavigationLocations
