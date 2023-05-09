e7370717b18f1112ae894e18ace8dbd2c3d1b79d
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-05-09 15:46:03 +0100

    Refactor BailInformationAccordion to make it more readable

--- src/components/FilterOptions/ReasonFilterOptions/BailInformationAccordion.tsx
+++ src/components/FilterOptions/ReasonFilterOptions/BailInformationAccordion.tsx
@@ -4,7 +4,24 @@ import { useState } from "react"
 import ConditionalRender from "components/ConditionalRender"
 
 const BailInformationAccordion = () => {
-  const [showBailInfo, setShowBailInfo] = useState(false)
+  const [showBailInformation, setShowBailInformation] = useState(false)
+
+  const informationIcon = (
+    <svg
+      fill={blue}
+      role="presentation"
+      focusable="true"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 25 25"
+      height="20"
+      width="20"
+    >
+      <path
+        d="M13.7,18.5h-2.4v-2.4h2.4V18.5z M12.5,13.7c-0.7,0-1.2-0.5-1.2-1.2V7.7c0-0.7,0.5-1.2,1.2-1.2s1.2,0.5,1.2,1.2v4.8
+C13.7,13.2,13.2,13.7,12.5,13.7z M12.5,0.5c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S19.1,0.5,12.5,0.5z"
+      />
+    </svg>
+  )
 
   return (
     <>
@@ -13,25 +30,12 @@ const BailInformationAccordion = () => {
         href="/"
         onClick={(event) => {
           event.preventDefault()
-          setShowBailInfo(!showBailInfo)
+          setShowBailInformation(!showBailInformation)
         }}
       >
-        <svg
-          fill={blue}
-          role="presentation"
-          focusable="true"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 25 25"
-          height="20"
-          width="20"
-        >
-          <path
-            d="M13.7,18.5h-2.4v-2.4h2.4V18.5z M12.5,13.7c-0.7,0-1.2-0.5-1.2-1.2V7.7c0-0.7,0.5-1.2,1.2-1.2s1.2,0.5,1.2,1.2v4.8
-    C13.7,13.2,13.2,13.7,12.5,13.7z M12.5,0.5c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S19.1,0.5,12.5,0.5z"
-          />
-        </svg>
+        {informationIcon}
       </a>
-      <ConditionalRender isRendered={showBailInfo}>
+      <ConditionalRender isRendered={showBailInformation}>
         <Preview className={"govuk-!-margin-top-2 govuk-!-margin-bottom-6"}>{"Lorem Ipslum"}</Preview>
       </ConditionalRender>
     </>
