d7a16bb7319372fb33e6498512ce64ab801d0d17
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-05-09 14:23:27 +0100

    Add an svg for an important icon

--- src/components/FilterOptions/ReasonFilterOptions.tsx
+++ src/components/FilterOptions/ReasonFilterOptions.tsx
@@ -1,6 +1,7 @@
 import type { Dispatch } from "react"
 import { Reason } from "types/CaseListQueryParams"
 import type { FilterAction } from "types/CourtCaseFilter"
+import { blue } from "utils/colours"
 import { reasonOptions } from "utils/reasonOptions"
 
 interface Props {
@@ -27,7 +28,23 @@ const ReasonFilterOptions: React.FC<Props> = ({ reasons, dispatch }: Props) => {
               }}
             ></input>
             <label className="govuk-label govuk-checkboxes__label" htmlFor={`${reason.toLowerCase()}-type`}>
-              {reason}
+              {reason}{" "}
+              {reason === "Bails" && (
+                <svg
+                  fill={blue}
+                  role="presentation"
+                  focusable="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 25 25"
+                  height="20"
+                  width="20"
+                >
+                  <path
+                    d="M13.7,18.5h-2.4v-2.4h2.4V18.5z M12.5,13.7c-0.7,0-1.2-0.5-1.2-1.2V7.7c0-0.7,0.5-1.2,1.2-1.2s1.2,0.5,1.2,1.2v4.8
+	C13.7,13.2,13.2,13.7,12.5,13.7z M12.5,0.5c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S19.1,0.5,12.5,0.5z"
+                  />
+                </svg>
+              )}
             </label>
           </div>
         ))}
