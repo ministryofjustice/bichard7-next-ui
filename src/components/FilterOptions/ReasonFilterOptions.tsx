12c52a79440e64873d1eef60323d62a96bc55d05
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-05-09 14:45:47 +0100

    Conditionally display bail info

--- src/components/FilterOptions/ReasonFilterOptions.tsx
+++ src/components/FilterOptions/ReasonFilterOptions.tsx
@@ -1,8 +1,11 @@
+import { Preview } from "components/Preview"
 import type { Dispatch } from "react"
 import { Reason } from "types/CaseListQueryParams"
 import type { FilterAction } from "types/CourtCaseFilter"
 import { blue } from "utils/colours"
+import { useState } from "react"
 import { reasonOptions } from "utils/reasonOptions"
+import ConditionalRender from "components/ConditionalRender"
 
 interface Props {
   reasons?: Reason[]
@@ -10,6 +13,8 @@ interface Props {
 }
 
 const ReasonFilterOptions: React.FC<Props> = ({ reasons, dispatch }: Props) => {
+  const [showBailInfo, setShowBailInfo] = useState(false)
+
   return (
     <fieldset className="govuk-fieldset">
       <div className="govuk-checkboxes govuk-checkboxes--small" data-module="govuk-checkboxes">
@@ -31,29 +36,34 @@ const ReasonFilterOptions: React.FC<Props> = ({ reasons, dispatch }: Props) => {
               {reason}{" "}
             </label>
             {reason === "Bails" && (
-              <a
-                className="govuk-link"
-                href="/"
-                onClick={(event) => {
-                  event.preventDefault()
-                  console.log("clicked!")
-                }}
-              >
-                <svg
-                  fill={blue}
-                  role="presentation"
-                  focusable="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 25 25"
-                  height="20"
-                  width="20"
+              <>
+                <a
+                  className="govuk-link"
+                  href="/"
+                  onClick={(event) => {
+                    event.preventDefault()
+                    setShowBailInfo(!showBailInfo)
+                  }}
                 >
-                  <path
-                    d="M13.7,18.5h-2.4v-2.4h2.4V18.5z M12.5,13.7c-0.7,0-1.2-0.5-1.2-1.2V7.7c0-0.7,0.5-1.2,1.2-1.2s1.2,0.5,1.2,1.2v4.8
+                  <svg
+                    fill={blue}
+                    role="presentation"
+                    focusable="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 25 25"
+                    height="20"
+                    width="20"
+                  >
+                    <path
+                      d="M13.7,18.5h-2.4v-2.4h2.4V18.5z M12.5,13.7c-0.7,0-1.2-0.5-1.2-1.2V7.7c0-0.7,0.5-1.2,1.2-1.2s1.2,0.5,1.2,1.2v4.8
     C13.7,13.2,13.2,13.7,12.5,13.7z M12.5,0.5c-6.6,0-12,5.4-12,12s5.4,12,12,12s12-5.4,12-12S19.1,0.5,12.5,0.5z"
-                  />
-                </svg>
-              </a>
+                    />
+                  </svg>
+                </a>
+                <ConditionalRender isRendered={showBailInfo}>
+                  <Preview>{"Lorem Ipslum"}</Preview>
+                </ConditionalRender>
+              </>
             )}
           </div>
         ))}
