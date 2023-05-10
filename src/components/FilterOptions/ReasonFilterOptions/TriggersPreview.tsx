0ccf2e30eedb5e725a5caa74d0701c90e6671af9
agnieszkazbikowska-madetech <agnieszka.zbikowska@madetech.com>
2023-05-10 14:56:47 +0100

    Move BailCodeLine to a new component

--- src/components/FilterOptions/ReasonFilterOptions/TriggersPreview.tsx
+++ src/components/FilterOptions/ReasonFilterOptions/TriggersPreview.tsx
@@ -1,12 +1,21 @@
 import { Preview } from "components/Preview"
 import { BailCodes } from "utils/bailCodes"
 
+interface BailCodeLineProps {
+  bailCode: string
+  bailName: string
+}
+
+const BailCodeLine = ({ bailCode, bailName }: BailCodeLineProps) => {
+  return <li>{`${bailCode} - ${bailName}`}</li>
+}
+
 const TriggersPreview = () => {
   return (
     <Preview className={"govuk-!-margin-top-2 govuk-!-margin-bottom-6"}>
       <p>{"Included triggers:"}</p>
       {Object.entries(BailCodes).map(([bailCode, bailName]) => (
-        <li key={bailCode}>{`${bailCode} - ${bailName}`}</li>
+        <BailCodeLine key={bailCode} bailCode={bailCode} bailName={bailName} />
       ))}
     </Preview>
   )
