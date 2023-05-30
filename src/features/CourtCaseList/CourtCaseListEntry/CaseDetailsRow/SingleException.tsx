65927d191c3cddacff0e22a679d4969ead67d965
Richard Cane <richard.cane@madetech.com>
2023-05-30 19:03:03 +0100

    Remove nbsp and use space instead, add class names

--- src/features/CourtCaseList/CourtCaseListEntry/CaseDetailsRow/SingleException.tsx
+++ src/features/CourtCaseList/CourtCaseListEntry/CaseDetailsRow/SingleException.tsx
@@ -4,9 +4,10 @@ interface SingleExceptionProps {
 }
 
 export const SingleException = ({ exception, exceptionCounter }: SingleExceptionProps) => (
-  <span>
-    {exception}
-    <b>&nbsp;{exceptionCounter > 1 ? `(${exceptionCounter})` : ""}</b>
-    <br />
+  <span className="single-exception">
+    <span className="code">{exception}</span>
+    <span className="counter">
+      <b> {exceptionCounter > 1 ? `(${exceptionCounter})` : ""}</b>
+    </span>
   </span>
 )
