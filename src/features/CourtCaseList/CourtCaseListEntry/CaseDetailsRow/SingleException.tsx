a8282285cea4bc8a4426403e37754bda385355c7
Richard Cane <richard.cane@madetech.com>
2023-05-30 19:06:21 +0100

    Remove unnecessary wrapping

--- src/features/CourtCaseList/CourtCaseListEntry/CaseDetailsRow/SingleException.tsx
+++ src/features/CourtCaseList/CourtCaseListEntry/CaseDetailsRow/SingleException.tsx
@@ -5,9 +5,6 @@ interface SingleExceptionProps {
 
 export const SingleException = ({ exception, exceptionCounter }: SingleExceptionProps) => (
   <span className="single-exception">
-    <span className="code">{exception}</span>
-    <span className="counter">
-      <b> {exceptionCounter > 1 ? `(${exceptionCounter})` : ""}</b>
-    </span>
+    {exception} <b>{exceptionCounter > 1 ? `(${exceptionCounter})` : ""}</b>
   </span>
 )
