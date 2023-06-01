41267a9d56b4edad4582e7d815790a52202c0619
Richard Cane <richard.cane@madetech.com>
2023-06-01 15:16:59 +0100

    Mark as complete button disabled if all triggers resolved

--- cypress/e2e/court-cases/court-case-details/Triggers.cy.ts
+++ cypress/e2e/court-cases/court-case-details/Triggers.cy.ts
@@ -3,7 +3,7 @@ import hashedPassword from "../../../fixtures/hashedPassword"
 
 const caseURL = "/bichard/court-cases/0"
 
-describe("Trigger panel", () => {
+describe("Triggers", () => {
   before(() => {
     cy.task("clearCourtCases")
     cy.task("clearUsers")
@@ -27,46 +27,69 @@ describe("Trigger panel", () => {
         orgForPoliceFilter: "01"
       }
     ])
+  })
+
+  beforeEach(() => {
     cy.login("bichard01@example.com", "password")
   })
 
-  it("should display a message and no button when there are no triggers on the case", () => {
-    cy.visit(caseURL)
-    cy.get(".moj-tab-panel-triggers").should("contain.text", "There are no triggers for this case.")
-    cy.get("#mark-triggers-complete-button").should("not.exist")
+  describe("Trigger status", () => {
+    it("should display a message and no button when there are no triggers on the case", () => {
+      cy.visit(caseURL)
+      cy.get(".moj-tab-panel-triggers").should("contain.text", "There are no triggers for this case.")
+      cy.get("#mark-triggers-complete-button").should("not.exist")
+    })
+
+    it("should show a complete badge against each resolved trigger", () => {
+      const triggers: TestTrigger[] = [
+        {
+          triggerId: 0,
+          triggerCode: "TRPR0010",
+          status: "Resolved",
+          createdAt: new Date("2022-07-09T10:22:34.000Z"),
+          resolvedAt: new Date("2022-07-09T12:22:34.000Z"),
+          resolvedBy: "Bichard01"
+        },
+        {
+          triggerId: 1,
+          triggerCode: "TRPR0015",
+          status: "Resolved",
+          createdAt: new Date("2022-07-09T10:22:34.000Z"),
+          resolvedAt: new Date("2022-07-09T12:22:34.000Z"),
+          resolvedBy: "Bichard01"
+        }
+      ]
+      cy.task("insertTriggers", { caseId: 0, triggers })
+
+      cy.visit(caseURL)
+
+      cy.get(".moj-tab-panel-triggers").should("be.visible")
+      cy.get(".moj-tab-panel-exceptions").should("not.be.visible")
+
+      cy.get(".moj-trigger-row").each((trigger) => {
+        cy.wrap(trigger).get(".moj-trigger-header-row").contains("Complete").should("exist")
+      })
+    })
   })
 
-  it("should show a complete badge against each resolved trigger", () => {
-    cy.task("insertCourtCasesWithFields", [{ orgForPoliceFilter: "01" }])
-    const triggers: TestTrigger[] = [
-      {
-        triggerId: 0,
-        triggerCode: "TRPR0010",
-        status: "Resolved",
-        createdAt: new Date("2022-07-09T10:22:34.000Z"),
-        resolvedAt: new Date("2022-07-09T12:22:34.000Z"),
-        resolvedBy: "Bichard01"
-      },
-      {
-        triggerId: 1,
-        triggerCode: "TRPR0015",
-        status: "Resolved",
-        createdAt: new Date("2022-07-09T10:22:34.000Z"),
-        resolvedAt: new Date("2022-07-09T12:22:34.000Z"),
-        resolvedBy: "Bichard01"
-      }
-    ]
-    cy.task("insertTriggers", { caseId: 0, triggers })
+  describe("Mark as complete button", () => {
+    it("should be disabled if all triggers are resolved", () => {
+      const triggers: TestTrigger[] = [
+        {
+          triggerId: 0,
+          triggerCode: "TRPR0010",
+          status: "Resolved",
+          createdAt: new Date("2022-07-09T10:22:34.000Z"),
+          resolvedAt: new Date("2022-07-09T12:22:34.000Z"),
+          resolvedBy: "Bichard01"
+        }
+      ]
+      cy.task("insertTriggers", { caseId: 0, triggers })
 
-    cy.login("bichard01@example.com", "password")
+      cy.visit(caseURL)
+      cy.get(".moj-tab-panel-triggers").should("be.visible")
 
-    cy.visit("/bichard/court-cases/0")
-
-    cy.get(".moj-tab-panel-triggers").should("be.visible")
-    cy.get(".moj-tab-panel-exceptions").should("not.be.visible")
-
-    cy.get(".moj-trigger-row").each((trigger) => {
-      cy.wrap(trigger).get(".moj-trigger-header-row").contains("Complete").should("exist")
+      cy.get("#mark-triggers-complete-button").should("be.visible").should("have.attr", "disabled")
     })
   })
 })
