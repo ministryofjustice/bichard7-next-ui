bbecc6908a60832c3eec7fd6598555f16c60d35d
joe-fol <joe.folkard@madetech.com>
2023-06-05 12:29:18 +0100

    Updates radio button order to match designs

--- src/types/NotesViewOption.ts
+++ src/types/NotesViewOption.ts
@@ -1,4 +1,4 @@
-const NOTES_VIEW_OPTIONS = ["View user notes", "View system notes", "View all notes"] as const
+const NOTES_VIEW_OPTIONS = ["View all notes", "View user notes", "View system notes"] as const
 type NotesViewOption = (typeof NOTES_VIEW_OPTIONS)[number]
 
 export { NOTES_VIEW_OPTIONS }
