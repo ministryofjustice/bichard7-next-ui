1a0b72ea2cfc97d7b3e9f5fd4012ef8b7f0245f1
Alice Lee <alice.lee@madetech.com>
2023-06-06 09:31:24 +0100

    Don't generate resolved cases with no exceptions or triggers

--- test/helpers/createDummyCase.ts
+++ test/helpers/createDummyCase.ts
@@ -40,7 +40,7 @@ export default async (
   const triggers = createDummyTriggers(dataSource, caseId, caseDate, isResolved)
   const hasTriggers = triggers.length > 0
   const notes = createDummyNotes(dataSource, caseId, triggers, isResolved)
-  const { errorReport, errorReason, exceptionCount } = createDummyExceptions()
+  const { errorReport, errorReason, exceptionCount } = createDummyExceptions(isResolved, hasTriggers)
   const hasExceptions = exceptionCount > 0
   const courtCase = await dataSource.getRepository(CourtCase).save({
     errorId: caseId,
