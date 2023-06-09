e2a53548acbba9907d0f245064a5c732bad5c0ec
Emad Karamad <emad.karamad@madetech.com>
2023-06-09 11:11:40 +0100

    Update validation error on add note page

--- src/features/AddNoteForm/AddNoteForm.tsx
+++ src/features/AddNoteForm/AddNoteForm.tsx
@@ -5,19 +5,20 @@ import { FormEvent, FormEventHandler, useState } from "react"
 
 interface Props {
   lockedByAnotherUser: boolean
-  error?: string
 }
 
-const AddNoteForm: React.FC<Props> = ({ lockedByAnotherUser, error }: Props) => {
+const AddNoteForm: React.FC<Props> = ({ lockedByAnotherUser }: Props) => {
   const [noteRemainingLength, setNoteRemainingLength] = useState(MAX_NOTE_LENGTH)
-  const [showError, setShowError] = useState(false)
+  const [isFormValid, setIsFormValid] = useState(true)
+  const showError = !isFormValid && noteRemainingLength === MAX_NOTE_LENGTH
+
   const handleOnNoteChange: FormEventHandler<HTMLTextAreaElement> = (event) => {
     setNoteRemainingLength(MAX_NOTE_LENGTH - event.currentTarget.value.length)
   }
 
   const validateForm = (event: FormEvent<HTMLFormElement>) => {
     if (noteRemainingLength === MAX_NOTE_LENGTH) {
-      setShowError(true)
+      setIsFormValid(false)
       event.preventDefault()
       return false
     }
@@ -28,15 +29,15 @@ const AddNoteForm: React.FC<Props> = ({ lockedByAnotherUser, error }: Props) => 
       <ConditionalRender isRendered={lockedByAnotherUser}>{"Case is locked by another user."}</ConditionalRender>
       <ConditionalRender isRendered={!lockedByAnotherUser}>
         <form method="POST" action="#" onSubmit={validateForm}>
-          <FormGroup error={showError && noteRemainingLength === MAX_NOTE_LENGTH}>
+          <FormGroup>
             <Heading as="h3" size="MEDIUM">
               {"Add a new note"}
             </Heading>
             <TextArea
               input={{ name: "noteText", rows: 5, maxLength: MAX_NOTE_LENGTH, onInput: handleOnNoteChange }}
               meta={{
-                error,
-                touched: !!error
+                error: "Input message into text box",
+                touched: showError
               }}
             >
               {}
