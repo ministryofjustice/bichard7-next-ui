2967d680b1575dcd090703c2e84e727f27dbc1bc
Ian King <ian.antking@gmail.com>
2023-06-16 11:56:55 +0100

    add e2e tests

---
+++ cypress/e2e/court-cases/court-case-list/court-case-list.cy.ts
@@ -0,0 +1,71 @@
+import User from "services/entities/User"
+import hashedPassword from "../../../fixtures/hashedPassword"
+
+describe("Court case details", () => {
+  const users: Partial<User>[] = Array.from(Array(5)).map((_value, idx) => {
+    return {
+      username: `Bichard0${idx}`,
+      visibleForces: [`0${idx}`],
+      forenames: "Bichard Test User",
+      surname: `0${idx}`,
+      email: `bichard0${idx}@example.com`,
+      password: hashedPassword
+    }
+  })
+
+  before(() => {
+    cy.task("clearCourtCases")
+    cy.task("clearUsers")
+    cy.task("insertUsers", { users, userGroups: ["B7NewUI_grp"] })
+    cy.task("insertIntoUserGroup", { emailAddress: "bichard01@example.com", groupName: "B7TriggerHandler_grp" })
+    cy.task("insertIntoUserGroup", { emailAddress: "bichard02@example.com", groupName: "B7Supervisor_grp" })
+    cy.clearCookies()
+  })
+
+  beforeEach(() => {
+    cy.task("clearCourtCases")
+  })
+
+  it("should lock a case when a user views a case details page", () => {
+    cy.task("insertCourtCasesWithFields", [
+      {
+        errorLockedByUsername: null,
+        triggerLockedByUsername: null,
+        orgForPoliceFilter: "02",
+        errorCount: 1,
+        triggerCount: 1
+      }
+    ])
+
+    cy.login("bichard02@example.com", "password")
+    cy.visit("/bichard")
+
+    cy.findByText("NAME Defendant").click()
+    cy.findByText("Case locked by another user").should("not.exist")
+    cy.findByText("Trigger locked by: Bichard02").should("exist")
+    cy.findByText("Error locked by: Bichard02").should("exist")
+  })
+
+  it("should not lock a court case when its already locked", () => {
+    const existingUserLock = "Another name"
+    cy.task("insertCourtCasesWithFields", [
+      {
+        errorLockedByUsername: existingUserLock,
+        triggerLockedByUsername: existingUserLock,
+        orgForPoliceFilter: "01",
+        errorCount: 1,
+        triggerCount: 1
+      }
+    ])
+
+    cy.login("bichard01@example.com", "password")
+    cy.visit("/bichard")
+    cy.findByText("NAME Defendant").click()
+
+    cy.findByText("Case locked by another user").should("exist")
+    cy.findByText("Trigger locked by: Another name").should("exist")
+    cy.findByText("Error locked by: Another name").should("exist")
+  })
+})
+
+export {}
