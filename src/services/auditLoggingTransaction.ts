4bf2da8854145e6c1cd3436bf04f7496631e4257
Emad Karamad <emad.karamad@madetech.com>
2023-06-19 18:27:19 +0100

    Add tests for auditLoggingTransaction

--- src/services/auditLoggingTransaction.ts
+++ src/services/auditLoggingTransaction.ts
@@ -1,10 +1,11 @@
 import AuditLogEvent from "@moj-bichard7-developers/bichard7-next-core/build/src/types/AuditLogEvent"
 import { DataSource, EntityManager } from "typeorm"
 import PromiseResult from "types/PromiseResult"
+import fetch from "node-fetch"
 
 export async function auditLoggingTransaction(
   dataSource: DataSource | EntityManager,
-  _messageId: string,
+  messageId: string,
   transaction: (events: AuditLogEvent[], entityManager: EntityManager) => Promise<void>
 ): PromiseResult<void> {
   const events: AuditLogEvent[] = []
@@ -12,8 +13,14 @@ export async function auditLoggingTransaction(
   return dataSource.transaction("SERIALIZABLE", async (entityManager) => {
     const result = await transaction(events, entityManager)
 
-    // call api to store events
-    // POST api_path/me [events]
+    const response = await fetch(`http://localhost:3010/messages/${messageId}/events`, {
+      method: "POST",
+      body: JSON.stringify(events)
+    })
+
+    if (!response.ok) {
+      throw Error(`Failed to create audit logs: ${await response.text()}`)
+    }
 
     return result
   })
