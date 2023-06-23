742f09cb0e6ece1e78d8689cdc0f3f22fd583881
Richard Cane <richard.cane@madetech.com>
2023-06-23 17:17:49 +0100

    Add tests for reason visibility due to role

--- cypress/e2e/filter/reasons.roles.cy.ts
+++ cypress/e2e/filter/reasons.roles.cy.ts
@@ -1,21 +1,23 @@
 import hashedPassword from "../../fixtures/hashedPassword"
 import { Groups } from "types/GroupName"
 
-function setUpWithGroups(groups: Groups[]) {
+function newUserLoginWithGroups(groups: Groups[]) {
+  const user = groups.map((g) => g.toLowerCase()).join("") || "nogroups"
+  const email = `${user}@example.com`
   cy.task("insertUsers", {
     users: [
       {
-        username: "Bichard01",
+        username: user,
         visibleForces: ["01"],
         forenames: "Bichard Test User",
         surname: "01",
-        email: "bichard01@example.com",
+        email: email,
         password: hashedPassword
       }
     ],
     userGroups: [Groups.NewUI, ...groups]
   })
-  cy.login("bichard01@example.com", "password")
+  cy.login(email, "password")
 }
 
 function navigateAndShowFilters() {
@@ -25,25 +27,24 @@ function navigateAndShowFilters() {
 
 describe("Reasons filters", () => {
   before(() => {
-    cy.task("clearCourtCases")
-    cy.task("clearTriggers")
     cy.task("clearUsers")
-
-    cy.task("insertCourtCasesWithFields", [
-      {
-        errorLockedByUsername: null,
-        triggerLockedByUsername: null,
-        orgForPoliceFilter: "01"
-      }
-    ])
   })
 
   afterEach(() => {
     cy.task("clearUsers")
   })
 
+  it("should display all options if no user group is specified", () => {
+    newUserLoginWithGroups([])
+    navigateAndShowFilters()
+
+    cy.get("#filter-panel .reasons .bails").should("exist")
+    cy.get("#filter-panel .reasons .triggers").should("exist")
+    cy.get("#filter-panel .reasons .exceptions").should("exist")
+  })
+
   it("should display all options for supervisors", () => {
-    setUpWithGroups([Groups.Supervisor])
+    newUserLoginWithGroups([Groups.Supervisor])
     navigateAndShowFilters()
 
     cy.get("#filter-panel .reasons .bails").should("exist")
@@ -52,7 +53,7 @@ describe("Reasons filters", () => {
   })
 
   it("should display all options for general handlers", () => {
-    setUpWithGroups([Groups.GeneralHandler])
+    newUserLoginWithGroups([Groups.GeneralHandler])
     navigateAndShowFilters()
 
     cy.get("#filter-panel .reasons .bails").should("exist")
@@ -61,23 +62,29 @@ describe("Reasons filters", () => {
   })
 
   it("should display 'Triggers' and 'Bails' for trigger handlers", () => {
-    setUpWithGroups([Groups.TriggerHandler])
+    newUserLoginWithGroups([Groups.TriggerHandler])
     navigateAndShowFilters()
 
     cy.get("#filter-panel .reasons .bails").should("exist")
     cy.get("#filter-panel .reasons .triggers").should("exist")
+  })
+
+  it("should not display 'Exceptions' for trigger handlers", () => {
+    newUserLoginWithGroups([Groups.TriggerHandler])
+    navigateAndShowFilters()
+
     cy.get("#filter-panel .reasons .exceptions").should("not.exist")
   })
 
   it("should not render the reasons component for exception handlers", () => {
-    setUpWithGroups([Groups.ExceptionHandler])
+    newUserLoginWithGroups([Groups.ExceptionHandler])
     navigateAndShowFilters()
 
     cy.get("#filter-panel .reasons").should("not.exist")
   })
 
-  it("should render the reasons component if a user has conflicting groups", () => {
-    setUpWithGroups([Groups.Supervisor, Groups.ExceptionHandler])
+  it("should render the correct reasons if a user has conflicting groups", () => {
+    newUserLoginWithGroups([Groups.Supervisor, Groups.ExceptionHandler])
     navigateAndShowFilters()
 
     cy.get("#filter-panel .reasons .bails").should("exist")
