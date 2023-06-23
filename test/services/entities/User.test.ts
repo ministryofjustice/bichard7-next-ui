1a7d05f4a883ea7b5397a1c6bc51346e313e4df4
Richard Cane <richard.cane@madetech.com>
2023-06-23 17:30:09 +0100

    Fix failing user tests

--- test/services/entities/User.test.ts
+++ test/services/entities/User.test.ts
@@ -1,6 +1,6 @@
 import { expect } from "@jest/globals"
 import User from "services/entities/User"
-import GroupName from "types/GroupName"
+import GroupName, { Groups } from "types/GroupName"
 
 const createUser = (...groups: GroupName[]) => {
   const user = new User()
@@ -11,42 +11,48 @@ const createUser = (...groups: GroupName[]) => {
 
 describe("User", () => {
   test("canLockExceptions should return true and canLockTriggers should return false when user is in Exception Handler group", () => {
-    const user = createUser("ExceptionHandler")
+    const user = createUser(Groups.ExceptionHandler)
 
     expect(user.canLockExceptions).toBe(true)
     expect(user.canLockTriggers).toBe(false)
   })
 
   test("canLockExceptions should return false and canLockTriggers should return true when user is in Trigger Handler group", () => {
-    const user = createUser("TriggerHandler")
+    const user = createUser(Groups.TriggerHandler)
 
     expect(user.canLockExceptions).toBe(false)
     expect(user.canLockTriggers).toBe(true)
   })
 
   test("canLockExceptions and canLockTriggers should return true when user is in General Handler group", () => {
-    const user = createUser("GeneralHandler")
+    const user = createUser(Groups.GeneralHandler)
 
     expect(user.canLockExceptions).toBe(true)
     expect(user.canLockTriggers).toBe(true)
   })
 
   test("canLockExceptions and canLockTriggers should return true when user is in Allocator group", () => {
-    const user = createUser("Allocator")
+    const user = createUser(Groups.Allocator)
 
     expect(user.canLockExceptions).toBe(true)
     expect(user.canLockTriggers).toBe(true)
   })
 
   test("canLockExceptions and canLockTriggers should return true when user is in Supervisor group", () => {
-    const user = createUser("Supervisor")
+    const user = createUser(Groups.Supervisor)
 
     expect(user.canLockExceptions).toBe(true)
     expect(user.canLockTriggers).toBe(true)
   })
 
   test("canLockExceptions and canLockTriggers should return false when user is in any other groups", () => {
-    const user = createUser("Audit", "AuditLoggingManager", "NewUI", "SuperUserManager", "UserManager")
+    const user = createUser(
+      Groups.Audit,
+      Groups.AuditLoggingManager,
+      Groups.NewUI,
+      Groups.SuperUserManager,
+      Groups.UserManager
+    )
 
     expect(user.canLockExceptions).toBe(false)
     expect(user.canLockTriggers).toBe(false)
