f4edee6c51ae51ccbd94854f7813ed2e42555df6
Alice Lee <monotypical@users.noreply.github.com>
2023-06-26 16:18:18 +0100

    Add "Leave and lock", "Leave and unlock" buttons (#304)
    * Moved case details header tests to their own file
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Fix existing tests
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Extract the court case details header into it's own component
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Show return to case list button when case isn't locked
    Added lock util functions for a serialized court case to use
    Add primary text colour constant from GDS
    Capitalised colour constants
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Aligned case details navigation buttons to the right
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Add basic "Leave and unlock" button
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Made the Leave and Unlock set parameters
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Make "leave and unlock" button actually unlock case
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Don't save unlockTrigger or unlockException param
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Move reallocate button to court case details header
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Tidy up header tests
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Remove redundant bits of the case details page
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    * Updated test to use new return to case list buttons
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>
    
    ---------
    
    Co-authored-by: Seneca Saunders <bianco14@hotmail.com>

---
+++ src/features/CourtCaseDetails/Header.tsx
@@ -0,0 +1,91 @@
+import ConditionalRender from "components/ConditionalRender"
+import LinkButton from "components/LinkButton"
+import UrgentBadge from "features/CourtCaseList/tags/UrgentBadge"
+import { Button, Heading } from "govuk-react"
+import { useRouter } from "next/router"
+import { createUseStyles } from "react-jss"
+import CourtCase from "services/entities/CourtCase"
+import User from "services/entities/User"
+import styled from "styled-components"
+import { isLockedByCurrentUser } from "utils/caseLocks"
+import { gdsLightGrey, textPrimary } from "utils/colours"
+
+interface Props {
+  courtCase: CourtCase
+  user: User
+  canReallocate: boolean
+}
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 24px;
+  gap: 12px;
+`
+
+const useStyles = createUseStyles({
+  button: {
+    marginBottom: 0
+  }
+})
+
+const Header: React.FC<Props> = ({ courtCase, user, canReallocate }: Props) => {
+  const { basePath } = useRouter()
+  const classes = useStyles()
+  const leaveAndUnlockParams = new URLSearchParams({ unlockTrigger: courtCase.errorId?.toString() })
+  const leaveAndUnlockUrl = `${basePath}?${leaveAndUnlockParams.toString()}`
+
+  const hasCaseLock = isLockedByCurrentUser(courtCase, user.username)
+  return (
+    <>
+      <Heading as="h1" size="LARGE" className="govuk-!-font-weight-regular">
+        {"Case details"}
+      </Heading>
+      <Heading as="h2" size="MEDIUM" className="govuk-!-font-weight-regular">
+        {courtCase.defendantName}
+        <UrgentBadge
+          isUrgent={courtCase.isUrgent}
+          className="govuk-!-static-margin-left-5 govuk-!-font-weight-regular"
+        />
+      </Heading>
+      <ButtonContainer>
+        <ConditionalRender isRendered={canReallocate}>
+          <LinkButton
+            href="reallocate"
+            className="b7-reallocate-button"
+            buttonColour={gdsLightGrey}
+            buttonTextColour={textPrimary}
+          >
+            {"Reallocate Case"}
+          </LinkButton>
+        </ConditionalRender>
+        <ConditionalRender isRendered={hasCaseLock}>
+          <a href={basePath}>
+            <Button id="leave-and-lock" className={classes.button}>
+              {"Leave and lock"}
+            </Button>
+          </a>
+          <form method="post" action={leaveAndUnlockUrl}>
+            <Button id="leave-and-unlock" className={classes.button} type="submit">
+              {"Leave and unlock"}
+            </Button>
+          </form>
+        </ConditionalRender>
+        <ConditionalRender isRendered={!hasCaseLock}>
+          <a href={basePath}>
+            <Button
+              id="return-to-case-list"
+              className={classes.button}
+              buttonColour={gdsLightGrey}
+              buttonTextColour={textPrimary}
+            >
+              {"Return to case list"}
+            </Button>
+          </a>
+        </ConditionalRender>
+      </ButtonContainer>
+    </>
+  )
+}
+
+export default Header
