b07b658f94a4e1ab67ec3fccde528d17e70d11a4
Ian King <ian.antking@gmail.com>
2023-06-28 15:40:06 +0100

    align summary box values and update test

--- cypress/component/CourtCaseDetailsSummary.cy.tsx
+++ cypress/component/CourtCaseDetailsSummary.cy.tsx
@@ -15,13 +15,31 @@ describe("Court Case Details Summary Box", () => {
       />
     )
 
-    cy.contains("ASN asn-value")
-    cy.contains("Court code (LJA) courtCode-value")
-    cy.contains("Court name courtName-value")
-    cy.contains("Court case reference courtReference-value")
-    cy.contains("PNCID pnci-value")
-    cy.contains("PTIURN ptirn-value")
-    cy.contains("DOB 03/10/2004")
-    cy.contains("Hearing date 25/04/2001")
+    cy.contains("ASN")
+    cy.contains("asn-value")
+
+    cy.contains("Court code (LJA)")
+    cy.contains("ourtCode-value")
+
+    cy.contains("Court name")
+    cy.contains("courtName-value")
+
+    cy.contains("Court name")
+    cy.contains("courtName-value")
+
+    cy.contains("Court case reference")
+    cy.contains("courtReference-value")
+
+    cy.contains("PNCID")
+    cy.contains("pnci-value")
+
+    cy.contains("PTIURN")
+    cy.contains("ptirn-value")
+
+    cy.contains("DOB")
+    cy.contains("03/10/2004")
+
+    cy.contains("Hearing date")
+    cy.contains("25/04/2001")
   })
 })
