d6237df30a03fa27bab76b47f3419703d0aa8c86
Alice Lee <alice.lee@madetech.com>
2023-07-03 14:37:59 +0100

    Gracefully handle missing user groups from permission checks

--- src/utils/userPermissions.ts
+++ src/utils/userPermissions.ts
@@ -2,27 +2,33 @@ import type User from "services/entities/User"
 import { UserGroup } from "types/UserGroup"
 
 const hasAccessToTriggers = (user: User) => {
-  return user.groups.some(
-    (group) =>
-      group === UserGroup.TriggerHandler ||
-      group === UserGroup.GeneralHandler ||
-      group === UserGroup.Allocator ||
-      group === UserGroup.Supervisor
+  return (
+    user.groups &&
+    user.groups.some(
+      (group) =>
+        group === UserGroup.TriggerHandler ||
+        group === UserGroup.GeneralHandler ||
+        group === UserGroup.Allocator ||
+        group === UserGroup.Supervisor
+    )
   )
 }
 
 const hasAccessToExceptions = (user: User) => {
-  return user.groups.some(
-    (group) =>
-      group === UserGroup.ExceptionHandler ||
-      group === UserGroup.GeneralHandler ||
-      group === UserGroup.Allocator ||
-      group === UserGroup.Supervisor
+  return (
+    user.groups &&
+    user.groups.some(
+      (group) =>
+        group === UserGroup.ExceptionHandler ||
+        group === UserGroup.GeneralHandler ||
+        group === UserGroup.Allocator ||
+        group === UserGroup.Supervisor
+    )
   )
 }
 
 const isSupervisor = (user: User) => {
-  return user.groups.some((group) => group === UserGroup.Supervisor)
+  return user.groups && user.groups.some((group) => group === UserGroup.Supervisor)
 }
 
 export { hasAccessToExceptions, hasAccessToTriggers, isSupervisor }
