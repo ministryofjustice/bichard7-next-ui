9fcba5377221dcfe607905fb182abc1bd7a20668
Richard Cane <richard.cane@madetech.com>
2023-07-12 13:24:28 +0100

    Pin @typescript-eslint/eslint-plugin to @5 Pin @typescript-eslint/parser to @5 Update npm dependencies

--- .ncurc.js
+++ .ncurc.js
@@ -1,6 +1,18 @@
+/*
+  Pinned packages:
+
+  - styled-components@5 
+    - conflict with GDS as of June 2023
+ 
+  - @typescript-eslint/eslint-plugin@5
+  - @typescript-eslint/parser@5
+    - eslint-config-airbnb-typescript@17 requires these two packages @5
+*/
+const pinned = ["styled-components", "@typescript-eslint/eslint-plugin", "@typescript-eslint/parser"]
+
 module.exports = {
   target: (dependencyName) => {
-    if (["styled-components"].includes(dependencyName)) {
+    if (pinned.includes(dependencyName)) {
       const res = "minor"
       console.log(` ${dependencyName} is pinned to ${res} upgrades only (.ncurc.js)`)
       return res
