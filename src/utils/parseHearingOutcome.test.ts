4f3e18abde0f154b5dc4187ed5f1f9bf48c0aef7
csabagyorfi <csaba@madetech.com>
2023-08-08 17:28:48 +0100

    Check if message is an AnnotatedPNCUpdateDatasetXml or AHO when parsing XML

---
+++ src/utils/parseHearingOutcome.test.ts
@@ -0,0 +1,47 @@
+import fs from "fs"
+import parseHearingOutcome from "./parseHearingOutcome"
+import { parseAhoXml } from "@moj-bichard7-developers/bichard7-next-core/dist/parse/parseAhoXml"
+import parseAnnotatedPNCUpdateDatasetXml from "@moj-bichard7-developers/bichard7-next-core/dist/parse/parseAnnotatedPNCUpdateDatasetXml/parseAnnotatedPNCUpdateDatasetXml"
+import dummyAho from "../../test/test-data/AnnotatedHO1.json"
+
+jest.mock("@moj-bichard7-developers/bichard7-next-core/dist/parse/parseAhoXml")
+jest.mock(
+  "@moj-bichard7-developers/bichard7-next-core/dist/parse/parseAnnotatedPNCUpdateDatasetXml/parseAnnotatedPNCUpdateDatasetXml"
+)
+
+beforeEach(() => {
+  ;(parseAhoXml as jest.Mock).mockImplementation(
+    jest.requireActual("@moj-bichard7-developers/bichard7-next-core/dist/parse/parseAhoXml").parseAhoXml
+  )
+  ;(parseAnnotatedPNCUpdateDatasetXml as jest.Mock).mockImplementation(
+    jest.requireActual(
+      "@moj-bichard7-developers/bichard7-next-core/dist/parse/parseAnnotatedPNCUpdateDatasetXml/parseAnnotatedPNCUpdateDatasetXml"
+    ).default
+  )
+})
+
+afterEach(async () => {
+  jest.resetAllMocks()
+  jest.clearAllMocks()
+})
+
+describe("parseHearingOutcome", () => {
+  it("Should return the error when hearing outcome XML is invalid", () => {
+    expect(parseHearingOutcome("not an XML")).toEqual(new Error("Could not parse AHO XML"))
+  })
+
+  it("Should call parseAnnotatedPNCUpdateDatasetXml when the XML is a isPncUpdateDataset", () => {
+    const dummyPNCUpdateDataset = fs.readFileSync("test/test-data/AnnotatedPNCUpdateDataset.xml").toString()
+    parseHearingOutcome(dummyPNCUpdateDataset)
+
+    expect(parseAnnotatedPNCUpdateDatasetXml).toHaveBeenCalledTimes(1)
+    expect(parseAnnotatedPNCUpdateDatasetXml).toHaveBeenCalledWith(dummyPNCUpdateDataset)
+  })
+
+  it("Should call parseAhoXml when the XML is not a isPncUpdateDataset", () => {
+    parseHearingOutcome(dummyAho.hearingOutcomeXml)
+
+    expect(parseAhoXml).toHaveBeenCalledTimes(1)
+    expect(parseAhoXml).toHaveBeenCalledWith(dummyAho.hearingOutcomeXml)
+  })
+})
