99895a76e478082ec913ec15cacbfbf649d0cf28
Richard Cane <richard.cane@madetech.com>
2023-08-11 16:17:14 +0100

    Skip next 13.4.13

--- .ncurc.js
+++ .ncurc.js
@@ -1,22 +1,39 @@
-/*
-  Pinned packages:
+const semver = new RegExp(
+  /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/
+) // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
 
-  - styled-components@5 
-    - conflict with GDS as of June 2023
- 
-  - @typescript-eslint/eslint-plugin@5
-  - @typescript-eslint/parser@5
-    - eslint-config-airbnb-typescript@17 requires these two packages @5
+/*
+  Pinned:
+  - chalk
+    - v5 is a breaking change
+
+  Ignored:
+  - bichard7-next-data-x.x.x
+    - ncu updates all of them to the latest version, very unhelpful
+  - p-limit
+  - esbuild
+    - ignored at v0.18.16 because v0.18.17 doesn't run the postinstall script properly.
 */
 const pinned = ["styled-components"]
+const ignored = []
+const skipped = [{ package: "next", version: "13.4.13" }]
 
 module.exports = {
-  target: (dependencyName) => {
-    if (pinned.includes(dependencyName)) {
+  target: (package) => {
+    if (pinned.some((pin) => pin === package)) {
       const res = "minor"
-      console.log(` ${dependencyName} is pinned to ${res} upgrades only (.ncurc.js)`)
+      console.log(` ${package} is pinned to ${res} upgrades only (.ncurc.js)`)
       return res
     }
     return "latest"
+  },
+
+  filterResults: (package, { upgradedVersion }) => {
+    if (ignored.some((ignore) => ignore.package === package)) {
+      return
+    } else if (skipped.some((skip) => skip.package === package && skip.version === upgradedVersion)) {
+      return
+    }
+    return true
   }
 }
