ca7ce9e3766e8e7074d3badc72a9418ba15b4140
csabagyorfi <csaba@madetech.com>
2023-08-14 15:29:23 +0100

    Rename featureFlagTransformer to jsonTransformer
    It doesn't do anything that is specific to the feature flag but makes it reusable for other json columns

--- src/services/entities/SurveyFeedback.ts
+++ src/services/entities/SurveyFeedback.ts
@@ -2,14 +2,14 @@ import { Column, Entity, PrimaryColumn } from "typeorm"
 import BaseEntity from "./BaseEntity"
 import type { SurveyFeedbackResponse } from "types/SurveyFeedback"
 import dateTransformer from "./transformers/dateTransformer"
-import featureFlagTransformer from "./transformers/featureFlagTransformer"
+import jsonTransformer from "./transformers/jsonTransformer"
 
 @Entity({ name: "survey_feedback" })
 export default class SurveyFeedback extends BaseEntity {
   @PrimaryColumn()
   id!: number
 
-  @Column({ transformer: featureFlagTransformer, type: "jsonb" })
+  @Column({ transformer: jsonTransformer, type: "jsonb" })
   response!: SurveyFeedbackResponse
 
   @Column({ name: "feedback_type" })
