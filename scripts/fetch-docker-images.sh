72da25476094ada9cd627e52a8ffab189690587e
Richard Cane <richard.cane@madetech.com>
2023-08-15 22:34:31 +0100

    Add fetch image scripting for cypress e2e

---
+++ scripts/fetch-docker-images.sh
@@ -0,0 +1,8 @@
+IMAGES=($@)
+
+for image in "${IMAGES[@]}"; do
+    if [[ "$CI" == "true" || "$(docker images -q $image 2> /dev/null)" == "" ]]; then
+        echo "Fetching $image..."
+        scripts/fetch-docker-image.sh $image
+    fi
+done
