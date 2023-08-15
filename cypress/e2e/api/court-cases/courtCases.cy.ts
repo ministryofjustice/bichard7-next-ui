51763b50d861141603c0ff363d7256a7c1bdff79
csabagyorfi <csaba@madetech.com>
2023-08-15 21:54:06 +0100

    WIP: Test API endpoint to lock/unlock a case

--- cypress/e2e/api/court-cases/courtCases.cy.ts
+++ cypress/e2e/api/court-cases/courtCases.cy.ts
@@ -8,15 +8,15 @@ describe("Court cases API endpoint", () => {
     cy.task("insertUsers", {
       users: [
         {
-          username: "Bichard00",
-          visibleForces: [`00`],
+          username: "Bichard01",
+          visibleForces: ["01"],
           forenames: "Bichard Test User",
-          surname: "00",
-          email: "bichard00@example.com",
+          surname: "Bichard user",
+          email: "bichard01@example.com",
           password: hashedPassword
         } as Partial<User>
       ],
-      userGroups: ["B7NewUI_grp"]
+      userGroups: ["B7NewUI_grp", "B7GeneralHandler_grp"]
     })
     cy.clearCookies()
   })
@@ -33,14 +33,16 @@ describe("Court cases API endpoint", () => {
     })
   })
 
-
   it("returns 200 if the user is authenticated", () => {
-    cy.login("bichard00@example.com", "password")
+    cy.task("insertCourtCasesWithFields", [
+      { orgForPoliceFilter: "01", errorLockedByUsername: "Bichard01", triggerLockedByUsername: "Bichard01" }
+    ])
+    cy.login("bichard01@example.com", "password")
     cy.request({
       method: "PATCH",
-      url: "bichard/api/court-cases/1",
-      headers: { Referer: "/users/users" },
-      failOnStatusCode: false
+      body: { errorLockedByUsername: "SomeUser" },
+      url: "bichard/api/court-cases/0",
+      headers: { Referer: "/users/users" }
     }).then((response) => {
       expect(response.status).to.eq(200)
       expect(response.body).to.have.property("success", true)
--- src/pages/api/court-cases/[courtCaseId]/index.tsx
+++ src/pages/api/court-cases/[courtCaseId]/index.tsx
@@ -1,11 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import withApiAuthentication from "../../../../middleware/withAuthentication/withApiAuthentication"
 import type User from "services/entities/User"
+import unlockCourtCase from "../../../../services/unlockCourtCase"
+import getDataSource from "../../../../services/getDataSource"
+import UnlockReason from "../../../../types/UnlockReason"
+import { isError } from "../../../../types/Result"
 
-export default withApiAuthentication((req: NextApiRequest, res: NextApiResponse, currentUser: User) => {
+export default withApiAuthentication(async (req: NextApiRequest, res: NextApiResponse, currentUser: User) => {
+  const dataSource = await getDataSource()
+
   switch (req.method) {
     case "PATCH":
-      res.status(200).json({ success: true, username: currentUser.email })
-      break
+      const courtCaseId = req.query.courtCaseId as unknown as number
+      const unlockResult = await unlockCourtCase(dataSource, courtCaseId, currentUser, UnlockReason.TriggerAndException).catch(error => error)
+
+      if (!isError(unlockResult)) {
+        return res.status(200).json({ success: true, result: unlockResult})
+      } else {
+        return res.status(500).json({ success: false,  error: unlockResult.message})
+      }
   }
 })
