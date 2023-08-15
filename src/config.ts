e4ec35c3583384b3d589cff60be68658f1640c07
Richard Cane <richard.cane@madetech.com>
2023-08-15 10:07:42 +0100

    Update ports for core services

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { TriggerCode } from "@moj-bichard7-developers/bichard7-next-core/dist/types/TriggerCode"
 
 export const MAX_NOTE_LENGTH = 1000
-export const AUDIT_LOG_API_URL = process.env.AUDIT_LOG_API_URL ?? "http://localhost:3010"
+export const AUDIT_LOG_API_URL = process.env.AUDIT_LOG_API_URL ?? "http://localhost:7010"
 export const AUDIT_LOG_API_KEY = process.env.AUDIT_LOG_API_KEY ?? "dummy_api_key"
 export const AUDIT_LOG_EVENT_SOURCE = "Bichard New UI"
 export const REALLOCATE_CASE_TRIGGER_CODE = "TRPR0028" as TriggerCode
