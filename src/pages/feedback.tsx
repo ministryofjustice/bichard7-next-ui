57572224a0575351d4d5783f614134cf824d2fd9
Seneca Saunders <seneca.saunders@madetech.com>
2023-08-17 13:41:30 +0100

    add textarea and button to feedback page Co-authored-by: Alice Lee <monotypical@users.noreply.github.com> Co-authored-by: Csaba <csaba@madetech.com>

--- src/pages/feedback.tsx
+++ src/pages/feedback.tsx
@@ -1,13 +1,14 @@
 import Layout from "components/Layout"
 import { MAX_NOTE_LENGTH } from "config"
-import { Fieldset, FormGroup, Heading, HintText, Label, Select, TextArea } from "govuk-react"
+import { BackLink, Button, Heading, HintText, Paragraph, Radio, TextArea } from "govuk-react"
 import { withAuthentication, withMultipleServerSideProps } from "middleware"
 import { GetServerSidePropsContext, GetServerSidePropsResult, NextPage } from "next"
+import { useRouter } from "next/router"
 import { ParsedUrlQuery } from "querystring"
 import { FormEventHandler, useState } from "react"
+import CourtCase from "services/entities/CourtCase"
 import User from "services/entities/User"
 import AuthenticationServerSidePropsContext from "types/AuthenticationServerSidePropsContext"
-import { useCustomStyles } from "../../styles/customStyles"
 
 export const getServerSideProps = withMultipleServerSideProps(
   withAuthentication,
@@ -24,6 +25,7 @@ export const getServerSideProps = withMultipleServerSideProps(
 
 interface Props {
   user: User
+  courtCase: CourtCase
 }
 
 const FeedbackPage: NextPage<Props> = ({ user }: Props) => {
@@ -31,7 +33,7 @@ const FeedbackPage: NextPage<Props> = ({ user }: Props) => {
   const handleOnNoteChange: FormEventHandler<HTMLTextAreaElement> = (event) => {
     setNoteRemainingLength(MAX_NOTE_LENGTH - event.currentTarget.value.length)
   }
-  const classes = useCustomStyles()
+  const { basePath } = useRouter()
   return (
     <>
       <Layout user={user}>
@@ -39,45 +41,69 @@ const FeedbackPage: NextPage<Props> = ({ user }: Props) => {
           <title>{"Report an issue | Bichard7"}</title>
           <meta name="description" content="user feedback| Bichard7" />
         </Heading>
-        <h1>{"How can we help?"}</h1>
+        <BackLink href={`${basePath}`} onClick={function noRefCheck() {}}>
+          {"Back"}
+        </BackLink>
+        <Heading as="h1">{"How can we help?"}</Heading>
         <Heading as="h2" size="MEDIUM">
           {"Report an issue"}
         </Heading>
-        <p>
+
+        <Paragraph>
           {
-            "If you are encountering specific technical issues, you should either check our FAQ page or contact the Bichard7 team for support. to raise a ticket. Any issues raised via this page will not be handled."
+            "If you are encountering specific technical issues, you should either check our [FAQ page](#) or [contact the Bichard7](#) for support to raise a ticket. Any issues raised via this page will not be handled"
           }
-        </p>
+        </Paragraph>
+
         <Heading as="h2" size="MEDIUM">
           {"Share your feedback"}
         </Heading>
-        <p>
-          {"If you would like to tell us about your experience using the new version of Bichard7, please do so below."}
-        </p>
-        <p>
-          {
-            "After submitting, if we have any enquiries we would like to be able to contact you. If you would like your feedback to be anonymous please opt-out below."
-          }
-        </p>
+
         <form method="POST" action={"#"}>
-          <Fieldset>
-            {" "}
-            <FormGroup>
-              <Label>{"Rate your experience of using the the new version of Bichard"}</Label>
-              <Select label={""}>
-                {" "}
-                <option value=""></option>
-              </Select>
-            </FormGroup>
-            <FormGroup>
-              <Label>{"Tell us why you gave this rating"}</Label>
-              <HintText className={classes["no-margin-bottom"]}>{"Input message into the text box"}</HintText>
-              <TextArea input={{ name: "feedback", rows: 5, maxLength: MAX_NOTE_LENGTH, onInput: handleOnNoteChange }}>
-                { }
-              </TextArea>
-              <HintText>{`You have ${noteRemainingLength} characters remaining`}</HintText>
-            </FormGroup>
-          </Fieldset>
+          <Paragraph>
+            {
+              "If you would like to tell us about your experience using the new version of Bichard7, please do so below."
+            }
+          </Paragraph>
+          <Paragraph>
+            {
+              "After submitting, if we have any enquiries we would like to be able to contact you. If you would like your feedback to be anonymous please opt-out below."
+            }
+          </Paragraph>
+
+          <Radio name="anonymous" value={"yes"}>
+            {"Yes, I would like to be contacted about this feedback."}
+          </Radio>
+
+          <Radio name="anonymous" value={"no"}>
+            {"No, I would like to opt-out, which will mean my feedback will be anonymous."}
+          </Radio>
+
+          <Heading as="h3" size="SMALL">
+            {"Rate your experience of using the the new version of Bichard"}
+          </Heading>
+          <Paragraph>{"Select one of the below options."}</Paragraph>
+          <Radio name={"Rate your experience"}>{"Very satisfied"}</Radio>
+          <Radio name={"Rate your experience"}>{"Satisfied"}</Radio>
+          <Radio name={"Rate your experience"}>{"Neither satisfied nor dissatisfied"}</Radio>
+          <Radio name={"Rate your experience"}>{"Dissatisfied"}</Radio>
+          <Radio name={"Rate your experience"}>{"Very dissatisfied"}</Radio>
+
+          <Heading as="h3" size="SMALL">
+            {"Tell us why you gave this rating"}
+          </Heading>
+
+          <TextArea
+            input={{ name: "feedback", rows: 5, maxLength: MAX_NOTE_LENGTH, onInput: handleOnNoteChange }}
+            meta={{
+              error: "Input message into the text box",
+              touched: true
+            }}
+          >
+            { }
+          </TextArea>
+          <HintText>{`You have ${noteRemainingLength} characters remaining`}</HintText>
+          <Button type="submit">{"Send feedback and continue"}</Button>
         </form>
       </Layout>
     </>
