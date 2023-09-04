58a297970cc211e55ba30fecc4a30badbb91ec51
Ian King <ian.antking@gmail.com>
2023-09-04 13:39:35 +0100

    remove unneeded eslint overrrides

--- src/features/CourtCaseFilters/ColumnHeading.tsx
+++ src/features/CourtCaseFilters/ColumnHeading.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import { ReactNode } from "react"
 import { createUseStyles } from "react-jss"
 
