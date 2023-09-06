433046ee2735adb3229ad99696edf82d57b9d876
Seneca Saunders <seneca.saunders@madetech.com>
2023-09-06 10:50:50 +0100

    remove unlikely scenario from test

--- src/utils/pagination/calculateLastPossiblePageNumber.test.ts
+++ src/utils/pagination/calculateLastPossiblePageNumber.test.ts
@@ -9,8 +9,7 @@ describe("calculateLastPossiblePageNumber", () => {
     { totalCases: 5, maxPageItems: 100, expected: 1 },
     { totalCases: 11, maxPageItems: 10, expected: 2 },
     { totalCases: 26, maxPageItems: 10, expected: 3 },
-    { totalCases: 0, maxPageItems: 10, expected: 1 },
-    { totalCases: 0, maxPageItems: 0, expected: 1 }
+    { totalCases: 0, maxPageItems: 10, expected: 1 }
   ])(
     "calculate the last possible page number when total cases are $totalCases and max page items are $maxPageItems",
     ({ totalCases, maxPageItems, expected }) => {
