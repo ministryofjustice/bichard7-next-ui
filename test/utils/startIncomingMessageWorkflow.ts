7af4d318c1c9999605391865a9f72dd366df10a4
Ammaar Limbada <ammaar.limbada@madetech.com>
2023-09-22 17:57:13 +0100

    WIP

--- test/utils/startIncomingMessageWorkflow.ts
+++ test/utils/startIncomingMessageWorkflow.ts
@@ -10,7 +10,7 @@ const PHASE1_BUCKET_NAME = "phase1"
 const s3Config = createS3Config()
 
 type WorkflowSearchParams = {
-  freeText: string
+  freeText?: string
   query: {
     workflowType?: string
     status?: string
@@ -60,13 +60,20 @@ export const startIncomingMessageWorkflow = async () => {
 
   await putFileToS3(inputMessage, s3Path, PHASE1_BUCKET_NAME!, s3Config)
 
+  // TODO:
+  // Search for all incoming_message_handler  workflows: http://localhost:5002/api/workflow/search?query=%22workflowType=incoming_message_handler%20AND%20status=COMPLETED%22
+  // Filter the JSON in the test and pull out the one with our externalId
+  // Grab the output from that: "output": "{s3TaskDataPath=6a91ec91-c9ca-42a7-a489-d32ecb3e1ed1.json}",
+  // Search for bichard_process workflow using the id in the output: http://localhost:5002/api/workflow/search?freeText=%226a91ec91-c9ca-42a7-a489-d32ec[…]ery=%22workflowType=bichard_process%20AND%20status=RUNNING%22
+
   // search for the workflow
   const workflows = await waitForWorkflows({
-    freeText: externalId,
     query: {
-      workflowType: "bichard_process",
-      status: "RUNNING"
+      workflowType: "incoming_message_handler",
+      status: "COMPLETED"
     }
   })
   expect(workflows).toHaveLength(1)
+
+  // const incomingMessageHandlerWorkflow = workflows[1]
 }
