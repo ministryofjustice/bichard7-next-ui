a58165b573567149b7f4bd9194817787126a4bcc
Joe Folkard <65510707+joe-fol@users.noreply.github.com>
2023-09-26 11:28:28 +0100

    Column heading icon's are now clickable (#398)
    * Moved ColumnOrderIcons inside Link tags
    
    * Added styles to icon
    
    * Added styles due to moving icon in link
    
    * Added test sorting by court name order icon
    
    * Added icon click to vary sorting
    
    * Removed .only
    
    * Added click variation to PTIURN sorting
    
    * Added sorting variation for urgency
    
    * Fixed linting
    
    * Fixed linting
    
    * Added remaining test to click icon
    
    * Fixed linting
    
    * Added correct selector for icon
    
    * Removed .only
    
    * Added styling for onclick to fit content
    
    * Added a assertion for .downArrow
    
    * Removed .only

--- src/features/CourtCaseList/CourtCaseListTableHeader.tsx
+++ src/features/CourtCaseList/CourtCaseListTableHeader.tsx
@@ -3,8 +3,8 @@ import { Link, Table } from "govuk-react"
 import { useRouter } from "next/router"
 import type { QueryOrder } from "types/CaseListQueryParams"
 
-import { useCustomStyles } from "../../../styles/customStyles"
 import ColumnHeading from "features/CourtCaseFilters/ColumnHeading"
+import { useCustomStyles } from "../../../styles/customStyles"
 
 interface CourtCaseListTableHeaderProps {
   order: QueryOrder
@@ -20,60 +20,53 @@ export const CourtCaseListTableHeader = ({ order }: CourtCaseListTableHeaderProp
     <Table.Row>
       <Table.Cell></Table.Cell>
       <Table.CellHeader className={classes["table-column-header-cell"]} setWidth={"178px"}>
-        <ColumnOrderIcons columnName={"defendantName"} currentOrder={query.order} orderBy={query.orderBy}>
-          <Link
-            className={classes["table-column-header-link"]}
-            href={orderByParams("defendantName")}
-            id="defendant-name-sort"
-          >
-            {"Defendant name"}
-          </Link>
-        </ColumnOrderIcons>
+        <Link
+          className={classes["table-column-header-link"]}
+          href={orderByParams("defendantName")}
+          id="defendant-name-sort"
+        >
+          {"Defendant name"}
+          <ColumnOrderIcons columnName={"defendantName"} currentOrder={query.order} orderBy={query.orderBy} />
+        </Link>
       </Table.CellHeader>
       <Table.CellHeader className={classes["table-column-header-cell"]} setWidth={"115px"}>
-        <ColumnOrderIcons columnName={"courtDate"} currentOrder={query.order} orderBy={query.orderBy}>
-          <Link className={classes["table-column-header-link"]} href={orderByParams("courtDate")} id="court-date-sort">
-            {"Court date"}
-          </Link>
-        </ColumnOrderIcons>
+        <Link className={classes["table-column-header-link"]} href={orderByParams("courtDate")} id="court-date-sort">
+          {"Court date"}
+          <ColumnOrderIcons columnName={"courtDate"} currentOrder={query.order} orderBy={query.orderBy} />
+        </Link>
       </Table.CellHeader>
       <Table.CellHeader className={classes["table-column-header-cell"]}>
-        <ColumnOrderIcons columnName={"courtName"} currentOrder={query.order} orderBy={query.orderBy}>
-          <Link className={classes["table-column-header-link"]} href={orderByParams("courtName")} id="court-name-sort">
-            {"Court name"}
-          </Link>
-        </ColumnOrderIcons>
+        <Link className={classes["table-column-header-link"]} href={orderByParams("courtName")} id="court-name-sort">
+          {"Court name"}
+          <ColumnOrderIcons columnName={"courtName"} currentOrder={query.order} orderBy={query.orderBy} />
+        </Link>
       </Table.CellHeader>
       <Table.CellHeader className={classes["table-column-header-cell"]}>
-        <ColumnOrderIcons columnName={"ptiurn"} currentOrder={query.order} orderBy={query.orderBy}>
-          <Link className={classes["table-column-header-link"]} href={orderByParams("ptiurn")} id="ptiurn-sort">
-            {"PTIURN"}
-          </Link>
-        </ColumnOrderIcons>
+        <Link className={classes["table-column-header-link"]} href={orderByParams("ptiurn")} id="ptiurn-sort">
+          {"PTIURN"}
+          <ColumnOrderIcons columnName={"ptiurn"} currentOrder={query.order} orderBy={query.orderBy} />
+        </Link>
       </Table.CellHeader>
       <Table.CellHeader className={classes["table-column-header-cell"]}>
-        <ColumnOrderIcons columnName={"isUrgent"} currentOrder={query.order} orderBy={query.orderBy}>
-          <Link className={classes["table-column-header-link"]} href={orderByParams("isUrgent")} id="is-urgent-sort">
-            {"Urgent"}
-          </Link>
-        </ColumnOrderIcons>
+        <Link className={classes["table-column-header-link"]} href={orderByParams("isUrgent")} id="is-urgent-sort">
+          {"Urgent"}
+          <ColumnOrderIcons columnName={"isUrgent"} currentOrder={query.order} orderBy={query.orderBy} />
+        </Link>
       </Table.CellHeader>
       <Table.CellHeader className={classes["table-column-header-cell"]}>
-        <ColumnOrderIcons columnName={"notes"} currentOrder={query.order} orderBy={query.orderBy}>
-          <Link className={classes["table-column-header-link"]} href={orderByParams("notes")} id="notes-sort">
-            {"Notes"}
-          </Link>
-        </ColumnOrderIcons>
+        <Link className={classes["table-column-header-link"]} href={orderByParams("notes")} id="notes-sort">
+          {"Notes"}
+          <ColumnOrderIcons columnName={"notes"} currentOrder={query.order} orderBy={query.orderBy} />
+        </Link>
       </Table.CellHeader>
       <Table.CellHeader className={classes["table-column-header-cell"]}>
         <ColumnHeading>{"Reason"}</ColumnHeading>
       </Table.CellHeader>
       <Table.CellHeader>
-        <ColumnOrderIcons columnName={"lockedBy"} currentOrder={query.order} orderBy={query.orderBy}>
-          <Link className={classes["table-column-header-link"]} href={orderByParams("lockedBy")} id="locked-by-sort">
-            {"Locked by"}
-          </Link>
-        </ColumnOrderIcons>
+        <Link className={classes["table-column-header-link"]} href={orderByParams("lockedBy")} id="locked-by-sort">
+          {"Locked by"}
+          <ColumnOrderIcons columnName={"lockedBy"} currentOrder={query.order} orderBy={query.orderBy} />
+        </Link>
       </Table.CellHeader>
     </Table.Row>
   )
