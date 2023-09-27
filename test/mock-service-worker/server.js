e6ddde97109a30aa4455e7daeeb9a4d874b7daba
Richard Cane <richard.cane@madetech.com>
2023-09-27 17:59:06 +0100

    Remove MSW

--- test/mock-service-worker/server.js
+++
@@ -1,6 +0,0 @@
-const { setupServer } = require("msw/node")
-const conductor = require("./conductor")
-
-// modules export arrays of handlers
-const handlers = [...conductor]
-module.exports = { server: setupServer(...handlers) }
