c9086d40262e5b874169baaf2bc2023cadae60fb
Richard Cane <richard.cane@madetech.com>
2023-09-27 17:59:16 +0100

    Add conductor service fake for circleci

---
+++ .circleci/fakes/conductor.js
@@ -0,0 +1,12 @@
+require("http")
+  .createServer(async (req, res) => {
+    const body = JSON.stringify([
+      {
+        workflowId: "test workflow ID"
+      }
+    ])
+    res.writeHead(200, { "Content-Type": "application/json", "Content-Length": Buffer.byteLength(body) })
+    res.write(body)
+    res.end()
+  })
+  .listen(5002, () => console.log("Running conductor fake on port 5002"))
