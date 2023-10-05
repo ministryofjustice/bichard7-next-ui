203b588e4afa04b40ae2213aa71c80e4532d527f
Joe Folkard <joe.folkard@madetech.com>
2023-10-05 15:59:03 +0100

    Updated to use feature flags

--- test/services/userPermissions.test.ts
+++ test/services/userPermissions.test.ts
@@ -6,6 +6,7 @@ import { UserGroup } from "types/UserGroup"
 const createUser = (...groups: UserGroup[]) => {
   const user = new User()
   user.groups = [...groups]
+  user.featureFlags = { exceptionsEnabled: true }
 
   return user
 }
@@ -66,32 +67,35 @@ describe("user permissions", () => {
     expect(user.hasAccessTo[Feature.Triggers]).toBe(false)
   })
 
-  test("An user who's groups have more permissions than a trigger handler has the feature flag exceptions_enabled set to false, should still have access to exceptions", () => {
+  test("An user who's groups have more permissions than a trigger handler has the feature flag exceptions_enabled set to false, should not have access to exceptions", () => {
     const user = createUser(
       UserGroup.Audit,
       UserGroup.AuditLoggingManager,
       UserGroup.NewUI,
       UserGroup.SuperUserManager,
-      UserGroup.UserManager
+      UserGroup.UserManager,
+      UserGroup.GeneralHandler
     )
+
     user.featureFlags = { exceptionsEnabled: false }
 
-    expect(user.hasAccessTo[Feature.Exceptions]).toBe(true)
+    expect(user.hasAccessTo[Feature.Exceptions]).toBe(false)
     expect(user.hasAccessTo[Feature.Triggers]).toBe(true)
   })
 
-  test("An user who's groups have more permissions than a trigger handler has the feature flag exceptions_enabled set to true, should not have access to exceptions", () => {
+  test("An user who's groups have more permissions than a trigger handler has the feature flag exceptions_enabled set to true, should have access to exceptions", () => {
     const user = createUser(
       UserGroup.Audit,
       UserGroup.AuditLoggingManager,
       UserGroup.NewUI,
       UserGroup.SuperUserManager,
-      UserGroup.UserManager
+      UserGroup.UserManager,
+      UserGroup.GeneralHandler
     )
 
     user.featureFlags = { exceptionsEnabled: true }
 
-    expect(user.hasAccessTo[Feature.Exceptions]).toBe(false)
+    expect(user.hasAccessTo[Feature.Exceptions]).toBe(true)
     expect(user.hasAccessTo[Feature.Triggers]).toBe(true)
   })
 })
