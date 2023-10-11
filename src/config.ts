54f3dc4da6a52b6f286e97a2296650810e08f7a7
csabagyorfi <csaba@madetech.com>
2023-10-11 17:20:19 +0100

    Add middleware and config to generate and validate anti-CSRF token
    All the logic is lifted from user-service, we should reduce this duplication between the two repos when we move UI and User-service into the core monorepo

--- src/config.ts
+++ src/config.ts
@@ -7,3 +7,10 @@ export const AUDIT_LOG_API_KEY = process.env.AUDIT_LOG_API_KEY ?? "dummy_api_key
 export const AUDIT_LOG_EVENT_SOURCE = "Bichard New UI"
 export const REALLOCATE_CASE_TRIGGER_CODE = "TRPR0028" as TriggerCode
 export const OUT_OF_AREA_TRIGGER_CODE = TriggerCode.TRPR0027
+export const COOKIES_SECURE_OPTION = (process.env.COOKIES_SECURE ?? "true") === "true"
+export const CSRF = {
+  tokenName: "CSRFToken",
+  cookieSecret: process.env.CSRF_COOKIE_SECRET ?? "OliverTwist1",
+  formSecret: process.env.CSRF_FORM_SECRET ?? "OliverTwist2",
+  maximumTokenAgeInSeconds: parseInt(process.env.CSRF_TOKEN_MAX_AGE ?? "600", 10)
+}
