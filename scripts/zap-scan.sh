81bb1a32ab362d1999af500dee538cfd9bb555b3
Emad Karamad <emad.karamad@madetech.com>
2023-10-12 09:31:55 +0100

    Configure zap

--- scripts/zap-scan.sh
+++
@@ -1,9 +0,0 @@
-docker run \
-  --rm \
-  --network="bichard_default" \
-  -v $(pwd)/scripts:/zap/workspace:ro \
-  -t ghcr.io/zaproxy/zaproxy:stable \
-  zap-baseline.py -I -j\
-    -t https://ui/bichard \
-    -z "-configfile /zap/workspace/zap.config"
-
