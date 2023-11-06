18a764783856ffac886d4cdce89ba25d7fc9ee20
Joe Folkard <joe.folkard@madetech.com>
2023-11-06 11:41:05 +0000

    Added test for Ho100309

---
+++ cypress/e2e/court-cases/court-case-details/ExceptionHandlerPrompt.cy.ts
@@ -0,0 +1,50 @@
+import HO100309 from "../../../../test/test-data/HO100309.json"
+import hashedPassword from "../../../fixtures/hashedPassword"
+
+describe("ExceptionHandlerPrompt", () => {
+  before(() => {
+    cy.task("clearCourtCases")
+    cy.task("clearUsers")
+    cy.task("insertUsers", {
+      users: [
+        {
+          username: "Bichard01",
+          visibleForces: ["01"],
+          forenames: "Bichard Test User",
+          surname: "01",
+          email: "bichard01@example.com",
+          password: hashedPassword
+        }
+      ],
+      userGroups: ["B7NewUI_grp", "B7GeneralHandler_grp"]
+    })
+  })
+
+  beforeEach(() => {
+    cy.task("clearCourtCases")
+  })
+
+  context("Result qualifier - HO100309", () => {
+    it("Should display no error prompt if no exception is raised", () => {
+      cy.task("insertCourtCasesWithFields", [{ orgForPoliceFilter: "01" }])
+
+      cy.login("bichard01@example.com", "password")
+      cy.visit("/bichard/court-cases/0")
+
+      cy.get("ul.moj-sub-navigation__list").contains("Offences").click()
+      cy.get(".govuk-link").contains("Attempt to rape a girl aged 13 / 14 / 15 years of age - SOA 2003").click()
+      cy.get(".qualifierCodeTable").contains("A")
+    })
+
+    it("Should display an error prompt when an exception is raised", () => {
+      cy.task("insertCourtCasesWithFields", [{ orgForPoliceFilter: "01", hearingOutcome: HO100309.hearingOutcomeXml }])
+
+      cy.login("bichard01@example.com", "password")
+      cy.visit("/bichard/court-cases/0")
+
+      cy.get("ul.moj-sub-navigation__list").contains("Offences").click()
+      cy.get(".govuk-link").contains("Aid and abet theft").click()
+      cy.get(".qualifierCodeTable").contains("XX")
+    })
+  })
+})
