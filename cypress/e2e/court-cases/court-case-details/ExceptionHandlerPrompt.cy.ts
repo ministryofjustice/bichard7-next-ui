c4a6fd00361570bdab3205788ef175484f495757
BushraAbdullahi <bushra1175@gmail.com>
2023-11-08 14:46:35 +0000

    complete tests

--- cypress/e2e/court-cases/court-case-details/ExceptionHandlerPrompt.cy.ts
+++ cypress/e2e/court-cases/court-case-details/ExceptionHandlerPrompt.cy.ts
@@ -55,6 +55,27 @@ describe("ExceptionHandlerPrompt", () => {
       cy.get("button").contains("Next offence").click()
       cy.get(".error-prompt-message").should("not.exist")
     })
+
+    it("Should not display any error prompts when exceptions are marked as manually resolved", () => {
+      cy.task("insertCourtCasesWithFields", [
+        { orgForPoliceFilter: "01", errorCount: 1, triggerCount: 1, hearingOutcome: HO100309.hearingOutcomeXml }
+      ])
+
+      cy.login("bichard01@example.com", "password")
+      cy.visit("/bichard/court-cases/0")
+
+      cy.get("ul.moj-sub-navigation__list").contains("Offences").click()
+      cy.get(".govuk-link").contains("Aid and abet theft").click()
+      cy.get(".qualifier-code-table").contains("XX")
+      cy.get(".qualifier-code-table .error-prompt-message").contains(ErrorMessages.QualifierCode)
+      cy.get(".offences-table").contains("YY10XYZXX")
+      cy.get(".offences-table .error-prompt-message").contains(ErrorMessages.HO100306_error_prompt)
+      cy.get("#exceptions-tab").contains("Exceptions").click()
+      cy.get("button").contains("Mark as manually resolved").click()
+      cy.get("button").contains("Resolve").click()
+      cy.get(".qualifier-code-table .error-prompt-message").should("not.exist")
+      cy.get(".offences-table .error-prompt-message").should("not.exist")
+    })
   })
 
   context("Offence code error prompts for HO100251 and HO100306", () => {
@@ -69,21 +90,22 @@ describe("ExceptionHandlerPrompt", () => {
       cy.get(".offences-table").contains("SX03001A")
     })
 
-    it("Should display an error prompt when a HO100306 is raised", () => {
+    it("Should display an error prompt when a HO100306 is raised only on the relevant offence", () => {
       cy.task("insertCourtCasesWithFields", [
         { orgForPoliceFilter: "01", errorCount: 1, triggerCount: 1, hearingOutcome: HO100306.hearingOutcomeXml }
       ])
 
       cy.login("bichard01@example.com", "password")
       cy.visit("/bichard/court-cases/0")
+
       cy.get("ul.moj-sub-navigation__list").contains("Offences").click()
       cy.get(".govuk-link").contains("Aid and abet theft").click()
       cy.get(".offences-table").contains("YY10XYZA")
-      cy.get(".error-prompt-message").contains(ErrorMessages.HO100306_error_prompt)
+      cy.get(".offences-table .error-prompt-message").contains(ErrorMessages.HO100306_error_prompt)
       cy.get("button").contains("Next offence").click()
-      cy.get(".error-prompt-message").should("not.exist")
+      cy.get(".offences-table .error-prompt-message").should("not.exist")
       cy.get("button").contains("Next offence").click()
-      cy.get(".error-prompt-message").should("not.exist")
+      cy.get(".offences-table .error-prompt-message").should("not.exist")
     })
   })
 })
