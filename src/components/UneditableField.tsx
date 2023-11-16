0ec5d04f7be4a10e29cfc9cb2497dccc7d9cd75f
Emad Karamad <emad.karamad@madetech.com>
2023-11-16 15:32:55 +0000

    Refactor UneditableField to be more reusable

--- src/components/UneditableField.tsx
+++ src/components/UneditableField.tsx
@@ -7,9 +7,10 @@ import ErrorIcon from "./ErrorIcon"
 type Props = {
   badge: string
   message: string
-  code?: string
+  value?: string
   colour: string
   label: string
+  children?: React.ReactNode
 }
 
 const useStyles = createUseStyles({
@@ -19,11 +20,11 @@ const useStyles = createUseStyles({
       paddingTop: ".62rem"
     }
   },
-  value: {
+  content: {
     "& .error-prompt-message": {
       color: textSecondary
     },
-    "& .qualifier-code": {
+    "& .invalid-value": {
       paddingBottom: ".94rem"
     },
     "& .badge-wrapper": {
@@ -34,7 +35,8 @@ const useStyles = createUseStyles({
     }
   }
 })
-const UneditableField = ({ badge, message, code, colour, label }: Props) => {
+
+const UneditableField = ({ badge, message, value, colour, label, children }: Props) => {
   const classes = useStyles()
   const labelField = (
     <>
@@ -44,21 +46,25 @@ const UneditableField = ({ badge, message, code, colour, label }: Props) => {
       </div>
     </>
   )
-  const value = (
-    <div className={classes.value}>
-      <div className="qualifier-code">{code}</div>
-      <div className="badge-wrapper">
-        <Badge className="error-prompt-badge" isRendered={true} colour={colour} label={badge} />
-      </div>
-      <div className="error-prompt-message">{message}</div>
+
+  const cellContent = (
+    <div className={classes.content}>
+      {value && <div className="invalid-value">{value}</div>}
+      {badge && (
+        <div className="badge-wrapper">
+          <Badge className="error-prompt-badge" isRendered={true} colour={colour} label={badge} />
+        </div>
+      )}
+      {message && <div className="error-prompt-message">{message}</div>}
     </div>
   )
+
   return (
     <Table.Row>
       <Table.Cell className={classes.label}>
         <b>{labelField}</b>
       </Table.Cell>
-      <Table.Cell>{value}</Table.Cell>
+      <Table.Cell>{children ?? cellContent}</Table.Cell>
     </Table.Row>
   )
 }
