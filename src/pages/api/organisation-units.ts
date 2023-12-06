d6b88551e0f90f431ebb5d27003ec78227e5ca87
csabagyorfi <csaba@madetech.com>
2023-12-06 17:14:19 +0000

    Remove redundant return

--- src/pages/api/organisation-units.ts
+++ src/pages/api/organisation-units.ts
@@ -8,12 +8,10 @@ import searchCourtOrganisationUnits, {
 export default (req: NextApiRequest, res: NextApiResponse<OrganisationUnitApiResponse>) => {
   const searchQueryString = ((req.query.search as string) || "").toLowerCase()
 
-  const filteredItems = searchCourtOrganisationUnits(searchQueryString).map((ou) => {
-    return {
-      fullOrganisationCode: getFullOrganisationCode(ou),
-      fullOrganisationName: getFullOrganisationName(ou)
-    }
-  })
+  const filteredItems = searchCourtOrganisationUnits(searchQueryString).map((ou) => ({
+    fullOrganisationCode: getFullOrganisationCode(ou),
+    fullOrganisationName: getFullOrganisationName(ou)
+  }))
 
   res.status(200).json(filteredItems.slice(0, 20))
 }
