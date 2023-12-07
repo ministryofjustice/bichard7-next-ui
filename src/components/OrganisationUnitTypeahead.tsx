b1042ab80e085e345437d390e0aaa8a12967d875
Csaba <csaba@madetech.com>
2023-12-07 09:24:38 +0000

    Use an API endpoint for organisation unit search (#495)
    * Refactor: Move searchCourtOrganisationUnits into its own service function
    
    - Add unit tests and improve search
    
    * Put organisation search behind an API
    
    * Remove redundant return
    
    * Display next hearing location once
    
    * Login before API e2e tests
    
    nginx redirects back to user service if auth cookie doesn't exist
    
    * Update amendment values on input value change
    
    This will ensure every next hearing location field is editable with a uniq value(when there are multiple offences with next hearing location exceptions)
    
    * Disable no-shadow eslint rule in useCombobox
    
    Cannot use a different variable name

--- src/components/OrganisationUnitTypeahead.tsx
+++ src/components/OrganisationUnitTypeahead.tsx
@@ -1,40 +1,28 @@
-import OrganisationUnits from "@moj-bichard7-developers/bichard7-next-data/data/organisation-unit.json"
-import { OrganisationUnit } from "@moj-bichard7-developers/bichard7-next-data/dist/types/types"
 import { useCombobox } from "downshift"
 import { Input } from "govuk-react"
-import { sortBy } from "lodash"
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import styled from "styled-components"
 import { AmendmentKeys, IndividualAmendmentValues, UpdatedOffenceResult } from "../types/Amendments"
-
-const FilteredOrganisationUnits: OrganisationUnit[] = OrganisationUnits.filter(
-  (organisationUnit) => organisationUnit.topLevelName !== "Police Service" && /\S/.test(organisationUnit.thirdLevelName)
-)
-const FilteredAndSortedOrganisationUnits = sortBy(
-  FilteredOrganisationUnits,
-  (organisationUnit) => organisationUnit.thirdLevelName
-)
-
-const getFullOrganisationCode = (organisationUnit: OrganisationUnit) =>
-  `${organisationUnit.topLevelCode}${organisationUnit.secondLevelCode}${organisationUnit.thirdLevelCode}${organisationUnit.bottomLevelCode}`
-
-const getFullOrganisationName = (organisationUnit: OrganisationUnit) =>
-  [
-    organisationUnit.topLevelName,
-    organisationUnit.secondLevelName,
-    organisationUnit.thirdLevelName,
-    organisationUnit.bottomLevelName
-  ]
-    .filter((part) => !!part)
-    .join("-")
+import OrganisationUnitApiResponse from "../types/OrganisationUnitApiResponse"
+import axios from "axios"
+import { isError } from "../types/Result"
 
 const ListWrapper = styled.div`
+  max-height: 20rem;
+  overflow-y: scroll;
+  background: white;
+  width: 100%;
+
+  ul {
+    margin-top: 0;
+    padding-left: 0;
+  }
+
   li {
     list-style: none;
     padding: 10px;
     border: 1px gray solid;
     position: relative;
-    left: -40px;
   }
 
   span {
@@ -52,53 +40,70 @@ interface Props {
 }
 
 const OrganisationUnitTypeahead: React.FC<Props> = ({ value, amendFn, resultIndex, offenceIndex }: Props) => {
-  const items = FilteredAndSortedOrganisationUnits
-  const [inputItems, setInputItems] = useState(items)
+  const [inputItems, setInputItems] = useState<OrganisationUnitApiResponse>([])
 
-  const { isOpen, getMenuProps, getInputProps, highlightedIndex, getItemProps } = useCombobox({
+  const fetchItems = useCallback(async (searchStringParam?: string) => {
+    const organisationUnitsResponse = await axios
+      .get<OrganisationUnitApiResponse>("/bichard/api/organisation-units", {
+        params: {
+          search: searchStringParam
+        }
+      })
+      .then((response) => response.data)
+      .catch((error) => error as Error)
+
+    if (isError(organisationUnitsResponse)) {
+      return
+    }
+
+    setInputItems(organisationUnitsResponse)
+  }, [])
+
+  const { isOpen, getMenuProps, getInputProps, highlightedIndex, getItemProps, inputValue } = useCombobox({
     items: inputItems,
+    // eslint-disable-next-line @typescript-eslint/no-shadow
     onInputValueChange: ({ inputValue }) => {
-      const filteredInputItems: OrganisationUnit[] = items.filter((organisationUnit) =>
-        getFullOrganisationCode(organisationUnit)
-          .toLowerCase()
-          .startsWith((inputValue || "").toLowerCase())
-      )
-      setInputItems(filteredInputItems)
       amendFn("nextSourceOrganisation")({
         resultIndex: resultIndex,
         offenceIndex: offenceIndex,
         updatedValue: inputValue
       } as UpdatedOffenceResult)
     },
-    itemToString(item) {
-      return item ? getFullOrganisationCode(item) : ""
-    }
+    initialInputValue: value,
+    itemToString: (item) => item?.fullOrganisationCode ?? ""
   })
+
+  useEffect(() => {
+    fetchItems(inputValue)
+  }, [fetchItems, inputValue])
+
   return (
     <div>
       <Input
         {...getInputProps({
-          className: "govuk-input govuk-input--width-20",
+          className: "govuk-input",
           id: "next-hearing-location",
           name: "next-hearing-location",
           value: value
         })}
       />
-      <ListWrapper>
-        <ul {...getMenuProps()}>
-          {isOpen &&
-            inputItems.map((item, index) => (
-              <li
-                style={highlightedIndex === index ? { backgroundColor: "#bde4ff" } : {}}
-                key={`${item}${index}`}
-                {...getItemProps({ item, index })}
-              >
-                {getFullOrganisationCode(item)}
-                <span>{getFullOrganisationName(item)}</span>
-              </li>
-            ))}
-        </ul>
-      </ListWrapper>
+      {isOpen && inputItems.length > 0 && (
+        <ListWrapper>
+          <ul {...getMenuProps()}>
+            {isOpen &&
+              inputItems.map((item, index) => (
+                <li
+                  style={highlightedIndex === index ? { backgroundColor: "#bde4ff" } : {}}
+                  key={`${item}${index}`}
+                  {...getItemProps({ item, index })}
+                >
+                  {item.fullOrganisationCode}
+                  <span>{item.fullOrganisationName}</span>
+                </li>
+              ))}
+          </ul>
+        </ListWrapper>
+      )}
     </div>
   )
 }
