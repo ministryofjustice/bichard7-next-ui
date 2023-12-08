eba7ac54925863653e1d0bffd4774ad7d96389e0
csabagyorfi <csaba@madetech.com>
2023-12-08 15:43:33 +0000

    Add tests submit exception permission

--- cypress/e2e/court-cases/manuallyResolve.cy.ts
+++ cypress/e2e/court-cases/manuallyResolve.cy.ts
@@ -1,5 +1,4 @@
 import hashedPassword from "../../fixtures/hashedPassword"
-import canManuallyResolveAndSubmitTestData from "../../fixtures/canManuallyResolveAndSubmitTestData.json"
 
 describe("Manually resolve a case", () => {
   const insertUsers = (userRoles: string[]) => {
@@ -125,70 +124,6 @@ describe("Manually resolve a case", () => {
       expect(response.status).to.eq(404)
     })
   })
-
-  canManuallyResolveAndSubmitTestData.forEach(
-    ({
-      canManuallyResolveAndSubmit,
-      exceptionStatus,
-      exceptionLockedByAnotherUser,
-      loggedInAs,
-      exceptionsFeatureFlagEnabled
-    }) => {
-      it(`Should ${
-        canManuallyResolveAndSubmit ? "be able to resolve or submit" : "NOT be able to resolve or submit"
-      } when exceptions are ${exceptionStatus}, ${
-        exceptionLockedByAnotherUser ? "locked by another user" : "locked by current user"
-      } and user is a ${loggedInAs} ${
-        !exceptionsFeatureFlagEnabled ? "and exceptions feature flag is disabled" : ""
-      }`, () => {
-        if (!exceptionsFeatureFlagEnabled) {
-          cy.task("clearUsers")
-          cy.task("insertUsers", {
-            users: [
-              {
-                username: `${loggedInAs} username`,
-                visibleForces: ["01"],
-                forenames: `${loggedInAs}'s forename`,
-                surname: `${loggedInAs}surname`,
-                email: `${loggedInAs}@example.com`,
-                password: hashedPassword,
-                featureFlags: { exceptionsEnabled: false }
-              }
-            ],
-            userGroups: ["B7NewUI_grp", "B7GeneralHandler_grp"]
-          })
-        }
-        cy.task("insertCourtCasesWithFields", [
-          {
-            orgForPoliceFilter: "01",
-            errorStatus: exceptionStatus,
-            errorLockedByUsername: exceptionLockedByAnotherUser ? "Bichard03" : `${loggedInAs} username`
-          }
-        ])
-        cy.login(`${loggedInAs}@example.com`, "password")
-        cy.visit("/bichard/court-cases/0")
-
-        if (loggedInAs === "GeneralHandler") {
-          cy.get(".triggers-and-exceptions-sidebar #exceptions-tab").click()
-        }
-
-        if (canManuallyResolveAndSubmit) {
-          cy.get("button").contains("Mark as manually resolved").should("exist")
-          // TODO check submit button should exists
-        } else {
-          cy.get("button").contains("Mark as manually resolved").should("not.exist")
-          // TODO check submit button should not exists
-        }
-
-        cy.request({
-          failOnStatusCode: false,
-          url: "/bichard/court-cases/0/resolve"
-        }).then((response) => {
-          expect(response.status).to.eq(canManuallyResolveAndSubmit ? 200 : 403)
-        })
-      })
-    }
-  )
 })
 
 export {}
