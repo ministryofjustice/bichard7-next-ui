9f50e3ac5e46b26d93b34914b1640bc3bc7b8864
Joe Folkard <joe.folkard@madetech.com>
2023-12-08 16:26:14 +0000

    Added conditional rendering for asn prompt

--- src/features/CourtCaseDetails/Tabs/Panels/DefendantDetails.tsx
+++ src/features/CourtCaseDetails/Tabs/Panels/DefendantDetails.tsx
@@ -1,4 +1,5 @@
 import { HearingDefendant } from "@moj-bichard7-developers/bichard7-next-core/core/types/AnnotatedHearingOutcome"
+import { ExceptionCode } from "@moj-bichard7-developers/bichard7-next-core/core/types/ExceptionCode"
 import { GenderCode, RemandStatusCode } from "@moj-bichard7-developers/bichard7-next-data/dist/types/types"
 import { GenderCodes } from "@moj-bichard7-developers/bichard7-next-data/dist/types/GenderCode"
 import { RemandStatuses } from "@moj-bichard7-developers/bichard7-next-data/dist/types/RemandStatusCode"
@@ -7,14 +8,37 @@ import { TableRow } from "./TableRow"
 import { formatDisplayedDate } from "utils/formattedDate"
 import { AddressCell } from "./AddressCell"
 import { capitalizeString } from "utils/capitaliseString"
+import { DisplayFullCourtCase } from "types/display/CourtCases"
+import ErrorMessages from "types/ErrorMessages"
+import ExceptionFieldTableRow from "components/ExceptionFieldTableRow"
+import ErrorPromptMessage from "components/ErrorPromptMessage"
 
 interface DefendantDetailsProps {
   defendant: HearingDefendant
+  asn: string | null
+  exceptions: { code: ExceptionCode; path: (string | number)[] }[]
+  courtCase: DisplayFullCourtCase
 }
 
-export const DefendantDetails = ({ defendant }: DefendantDetailsProps) => {
+export const DefendantDetails = ({ defendant, asn, exceptions, courtCase }: DefendantDetailsProps) => {
+  // TODO: break out into a reusable function as used in OffenceDetails
+  const findUnresolvedException = (exceptionCode: ExceptionCode) =>
+    exceptions.find((exception) => exception.code === exceptionCode && courtCase.errorStatus !== "Resolved")
+  // need to add logic for HO200114, need to check if you can have both HO200113 and HO200114
+  const asnErrorPrompt = findUnresolvedException(ExceptionCode.HO200113) && ErrorMessages.AsnUneditable
   return (
     <Table>
+      {
+        <>
+          {asnErrorPrompt ? (
+            <ExceptionFieldTableRow badgeText={"SYSTEM ERROR"} value={asn} badgeColour={"purple"} label={"ASN"}>
+              <ErrorPromptMessage message={ErrorMessages.AsnUneditable} />
+            </ExceptionFieldTableRow>
+          ) : (
+            <TableRow label="ASN" value={asn} />
+          )}
+        </>
+      }
       <TableRow label="PNC Check name" value={defendant.PNCCheckname} />
       <TableRow label="Given name" value={defendant.DefendantDetail?.PersonName.GivenName?.join(", ")} />
       <TableRow label="Family name" value={defendant.DefendantDetail?.PersonName.FamilyName} />
