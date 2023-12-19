08dbf629f64ab5f1c53f1f4366c88da3fb444f9b
Ammaar Limbada <ammaar.limbada@madetech.com>
2023-12-19 16:06:58 +0000

    HO100306

--- test/helpers/createDummyExceptions.ts
+++ test/helpers/createDummyExceptions.ts
@@ -2,7 +2,7 @@ import { AnnotatedHearingOutcome } from "@moj-bichard7-developers/bichard7-next-
 import exponential from "@stdlib/random-base-exponential"
 import sampleMany from "@stdlib/random-sample"
 import { sample as sampleOne } from "lodash"
-import { HO100206, HO100212, HO100234, HO100301, HO100302, HO100304 } from "./exceptions"
+import { HO100206, HO100212, HO100234, HO100301, HO100302, HO100304, HO100306 } from "./exceptions"
 
 const possibleExceptions: Record<string, (aho: AnnotatedHearingOutcome) => AnnotatedHearingOutcome> = {
   HO100101: (aho) => aho,
@@ -14,7 +14,7 @@ const possibleExceptions: Record<string, (aho: AnnotatedHearingOutcome) => Annot
   HO100301: (aho) => HO100301(aho),
   HO100302: (aho) => HO100302(aho),
   HO100304: (aho) => HO100304(aho),
-  HO100306: (aho) => aho,
+  HO100306: (aho) => HO100306(aho),
   HO100310: (aho) => aho,
   HO100314: (aho) => aho,
   HO100321: (aho) => aho,
