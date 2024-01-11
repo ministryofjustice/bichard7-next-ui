a08acc32c1149f1f5deb3e380e93c848a0896840
Ian King <ian.antking@gmail.com>
2024-01-11 15:33:30 +0000

    remove useCustomStyle hook from ResolutionStatusBadge

--- src/features/CourtCaseList/tags/ResolutionStatusBadge.tsx
+++ src/features/CourtCaseList/tags/ResolutionStatusBadge.tsx
@@ -1,26 +1,21 @@
 import Badge from "components/Badge"
-import { useCustomStyles } from "../../../../styles/customStyles"
 import { ResolutionStatus } from "../../../types/ResolutionStatus"
 
 interface Props {
   resolutionStatus: ResolutionStatus
 }
 
-const ResolutionStatusBadge: React.FC<Props> = ({ resolutionStatus }: Props) => {
-  const classes = useCustomStyles()
-
-  if (resolutionStatus === "Unresolved") {
-    return <></>
-  }
-
-  return (
-    <Badge
-      isRendered={true}
-      label={resolutionStatus}
-      colour={resolutionStatus === "Resolved" ? "grey" : "blue"}
-      className={`govuk-!-static-margin-left-5 moj-badge-${resolutionStatus.toLowerCase()}`}
-    />
-  )
-}
+const ResolutionStatusBadge: React.FC<Props> = ({ resolutionStatus }: Props) => (
+  <>
+    {resolutionStatus !== "Unresolved" && (
+      <Badge
+        isRendered={true}
+        label={resolutionStatus}
+        colour={resolutionStatus === "Resolved" ? "grey" : "blue"}
+        className={`govuk-!-static-margin-left-5 moj-badge-${resolutionStatus.toLowerCase()}`}
+      />
+    )}
+  </>
+)
 
 export default ResolutionStatusBadge
