44898c0dd088d309cbe212b474627a1cbc79c798
Ammaar Limbada <ammaar.limbada@madetech.com>
2024-01-11 17:24:46 +0000

    HO100310

--- test/helpers/exceptions/HO100310.ts
+++ test/helpers/exceptions/HO100310.ts
@@ -2,6 +2,14 @@ import { AnnotatedHearingOutcome } from "@moj-bichard7-developers/bichard7-next-
 import { ExceptionCode } from "@moj-bichard7-developers/bichard7-next-core/core/types/ExceptionCode"
 
 export default function (aho: AnnotatedHearingOutcome): AnnotatedHearingOutcome {
+  const offences = aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence
+  offences.splice(1, 2)
+  offences.push(structuredClone(offences[0]))
+
+  offences[1].Result.push(structuredClone(offences[1].Result[0]))
+
+  offences[1].Result[1].CJSresultCode = 3052
+
   aho.Exceptions.push({
     code: ExceptionCode.HO100310,
     path: [
