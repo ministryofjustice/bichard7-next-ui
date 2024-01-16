88b0de0518724ac73e91ec7350dcd0cc1ebffb17
Ammaar Limbada <ammaar.limbada@madetech.com>
2024-01-16 15:05:30 +0000

    Reverted node version. Replaced use of structuredClone with JSON.parse/stringify implementation

--- test/helpers/exceptions/HO100310.ts
+++ test/helpers/exceptions/HO100310.ts
@@ -1,12 +1,20 @@
-import { AnnotatedHearingOutcome } from "@moj-bichard7-developers/bichard7-next-core/core/types/AnnotatedHearingOutcome"
+import {
+  AnnotatedHearingOutcome,
+  Offence,
+  Result
+} from "@moj-bichard7-developers/bichard7-next-core/core/types/AnnotatedHearingOutcome"
 import { ExceptionCode } from "@moj-bichard7-developers/bichard7-next-core/core/types/ExceptionCode"
+
+const clone = (value: Offence | Result) => {
+  return JSON.parse(JSON.stringify(value))
+}
 
 export default function (aho: AnnotatedHearingOutcome): AnnotatedHearingOutcome {
   const offences = aho.AnnotatedHearingOutcome.HearingOutcome.Case.HearingDefendant.Offence
   offences.splice(1, 2)
-  offences.push(structuredClone(offences[0]))
+  offences.push(clone(offences[0]))
 
-  offences[1].Result.push(structuredClone(offences[1].Result[0]))
+  offences[1].Result.push(clone(offences[1].Result[0]))
 
   offences[1].Result[1].CJSresultCode = 3052
 
