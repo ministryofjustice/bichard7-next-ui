22390152630331c01bbb1a303106ff9189c7cfa4
Tausif <tausifpatel.dev@gmail.com>
2024-01-17 15:51:13 +0000

    Display ASN according to exception

--- cypress/e2e/court-cases/edit-fields-with-exceptions/arrestSummonsNumber.cy.ts
+++ cypress/e2e/court-cases/edit-fields-with-exceptions/arrestSummonsNumber.cy.ts
@@ -1,130 +1,166 @@
-import hashedPassword from "../../../fixtures/hashedPassword";
-import asnExceptions from "../../../../test/test-data/AsnExceptionHo100206.json";
-import dummyAho from "../../../../test/test-data/error_list_aho.json";
-import {verifyUpdatedMessage} from "../../../support/helpers";
+import hashedPassword from "../../../fixtures/hashedPassword"
+import AsnExceptionHo100206 from "../../../../test/test-data/AsnExceptionHo100206.json"
+import AsnExceptionHo100321 from "../../../../test/test-data/AsnExceptionHo100321.json"
+import dummyAho from "../../../../test/test-data/error_list_aho.json"
+import { verifyUpdatedMessage } from "../../../support/helpers"
 
 describe("NextHearingDate", () => {
-    before(() => {
-        cy.task("clearCourtCases")
-        cy.task("clearUsers")
-        cy.task("insertUsers", {
-            users: [
-                {
-                    username: "Bichard01",
-                    visibleForces: ["001"],
-                    forenames: "Bichard Test User",
-                    surname: "01",
-                    email: "bichard01@example.com",
-                    password: hashedPassword
-                }
-            ],
-            userGroups: ["B7NewUI_grp", "B7GeneralHandler_grp"]
-        })
+  before(() => {
+    cy.task("clearCourtCases")
+    cy.task("clearUsers")
+    cy.task("insertUsers", {
+      users: [
+        {
+          username: "Bichard01",
+          visibleForces: ["001"],
+          forenames: "Bichard Test User",
+          surname: "01",
+          email: "bichard01@example.com",
+          password: hashedPassword
+        }
+      ],
+      userGroups: ["B7NewUI_grp", "B7GeneralHandler_grp"]
     })
+  })
 
-    beforeEach(() => {
-        cy.task("clearCourtCases")
-        cy.task("insertCourtCasesWithFields", [
-            {
-                orgForPoliceFilter: "01",
-                hearingOutcome: asnExceptions.hearingOutcomeXml,
-                updatedHearingOutcome: asnExceptions.updatedHearingOutcomeXml,
-                errorCount: 1
-            }
-        ])
+  beforeEach(() => {
+    cy.task("clearCourtCases")
+    cy.task("insertCourtCasesWithFields", [
+      {
+        orgForPoliceFilter: "01",
+        hearingOutcome: AsnExceptionHo100206.hearingOutcomeXml,
+        updatedHearingOutcome: AsnExceptionHo100206.updatedHearingOutcomeXml,
+        errorCount: 1
+      }
+    ])
+  })
+
+  it("Should not be able to edit ASN field when there is no exception", () => {
+    cy.task("insertCourtCasesWithFields", [
+      {
+        orgForPoliceFilter: "01",
+        hearingOutcome: dummyAho.hearingOutcomeXml,
+        errorCount: 1
+      }
+    ])
+
+    cy.login("bichard01@example.com", "password")
+    cy.visit("/bichard/court-cases/0")
+
+    cy.get(".moj-badge").contains("Editable Field").should("not.exist")
+    cy.get("#asn").should("not.exist")
+  })
+
+  it("Should not be able to edit ASN field when the case isn't in 'unresolved' state", () => {
+    const submittedCaseId = 0
+    const resolvedCaseId = 1
+    cy.task("insertCourtCasesWithFields", [
+      {
+        errorStatus: "Submitted",
+        errorId: submittedCaseId,
+        orgForPoliceFilter: "01",
+        hearingOutcome: AsnExceptionHo100206.hearingOutcomeXml,
+        updatedHearingOutcome: AsnExceptionHo100206.updatedHearingOutcomeXml,
+        errorCount: 1
+      },
+      {
+        errorStatus: "Resolved",
+        errorId: resolvedCaseId,
+        orgForPoliceFilter: "01",
+        hearingOutcome: AsnExceptionHo100206.hearingOutcomeXml,
+        updatedHearingOutcome: AsnExceptionHo100206.updatedHearingOutcomeXml,
+        errorCount: 1
+      }
+    ])
+
+    cy.login("bichard01@example.com", "password")
+    cy.visit(`/bichard/court-cases/${submittedCaseId}`)
+
+    cy.get(".moj-badge").contains("Editable Field").should("not.exist")
+    cy.get("#asn").should("not.exist")
+
+    cy.visit(`/bichard/court-cases/${resolvedCaseId}`)
+
+    cy.get(".moj-badge").contains("Editable Field").should("not.exist")
+    cy.get("#asn").should("not.exist")
+  })
+
+  it("Should be able to edit ASN field if HO100206 is raised", () => {
+    cy.login("bichard01@example.com", "password")
+    cy.visit("/bichard/court-cases/0")
+
+    cy.get(".moj-badge").contains("Editable Field").should("exist")
+    cy.get("#asn").type("1101ZD0100000448754K")
+
+    cy.get("button").contains("Submit exception(s)").click()
+
+    cy.contains(
+      "Are you sure you want to submit the amended details to the PNC and mark the exception(s) as resolved?"
+    ).should("exist")
+    cy.get("button").contains("Submit exception(s)").click()
+
+    cy.contains("Bichard01: Portal Action: Update Applied. Element: asn. New Value: 1101ZD0100000448754K")
+    cy.contains("Bichard01: Portal Action: Resubmitted Message.")
+
+    verifyUpdatedMessage({
+      expectedCourtCase: { errorId: 0, errorStatus: "Submitted" },
+      updatedMessageNotHaveContent: ["<br7:ArrestSummonsNumber>AAAAAAAAAAAAAAAAAAAA</br7:ArrestSummonsNumber>"],
+      updatedMessageHaveContent: ["<br7:ArrestSummonsNumber>1101ZD0100000448754K</br7:ArrestSummonsNumber>"]
     })
+  })
 
-    it("Should not be able to edit ASN field when there is no exception", () => {
-        cy.task("insertCourtCasesWithFields", [
-            {
-                orgForPoliceFilter: "01",
-                hearingOutcome: dummyAho.hearingOutcomeXml,
-                errorCount: 1
-            }
-        ])
+  it("Should be able to edit ASN field if HO100321 is raised", () => {
+    cy.task("insertCourtCasesWithFields", [
+      {
+        orgForPoliceFilter: "01",
+        hearingOutcome: AsnExceptionHo100321.hearingOutcomeXml,
+        updatedHearingOutcome: AsnExceptionHo100321.updatedHearingOutcomeXml,
+        errorCount: 1
+      }
+    ])
 
-        cy.login("bichard01@example.com", "password")
-        cy.visit("/bichard/court-cases/0")
-        
-        cy.get(".moj-badge").contains("Editable Field").should("not.exist")
-        cy.get("#asn").should("not.exist")
+    cy.login("bichard01@example.com", "password")
+    cy.visit("/bichard/court-cases/0")
+
+    cy.get(".moj-badge").contains("Editable Field").should("exist")
+    cy.get("#asn").type("1101ZD0100000448754K")
+
+    cy.get("button").contains("Submit exception(s)").click()
+
+    cy.contains(
+      "Are you sure you want to submit the amended details to the PNC and mark the exception(s) as resolved?"
+    ).should("exist")
+    cy.get("button").contains("Submit exception(s)").click()
+
+    cy.contains("Bichard01: Portal Action: Update Applied. Element: asn. New Value: 1101ZD0100000448754K")
+    cy.contains("Bichard01: Portal Action: Resubmitted Message.")
+
+    verifyUpdatedMessage({
+      expectedCourtCase: { errorId: 0, errorStatus: "Submitted" },
+      updatedMessageNotHaveContent: ["<br7:ArrestSummonsNumber>0800NP0100000000001H</br7:ArrestSummonsNumber>"],
+      updatedMessageHaveContent: ["<br7:ArrestSummonsNumber>1101ZD0100000448754K</br7:ArrestSummonsNumber>"]
     })
+  })
 
-    it("Should not be able to edit ASN field when the case isn't in 'unresolved' state", () => {
-        const submittedCaseId = 0
-        const resolvedCaseId = 1
-        cy.task("insertCourtCasesWithFields", [
-            {
-                errorStatus: "Submitted",
-                errorId: submittedCaseId,
-                orgForPoliceFilter: "01",
-                hearingOutcome: asnExceptions.hearingOutcomeXml,
-                updatedHearingOutcome: asnExceptions.updatedHearingOutcomeXml,
-                errorCount: 1
-            },
-            {
-                errorStatus: "Resolved",
-                errorId: resolvedCaseId,
-                orgForPoliceFilter: "01",
-                hearingOutcome: asnExceptions.hearingOutcomeXml,
-                updatedHearingOutcome: asnExceptions.updatedHearingOutcomeXml,
-                errorCount: 1
-            }
-        ])
+  it("should display the updated ASN after submission", () => {
+    cy.login("bichard01@example.com", "password")
+    cy.visit("/bichard/court-cases/0")
 
-        cy.login("bichard01@example.com", "password")
-        cy.visit(`/bichard/court-cases/${submittedCaseId}`)
+    cy.get(".Defendant-details-table").contains("AAAAAAAAAAAAAAAAAAAA")
+    cy.get("#asn").type("1101ZD0100000448754K")
 
-        cy.get(".moj-badge").contains("Editable Field").should("not.exist")
-        cy.get("#asn").should("not.exist")
+    cy.get("button").contains("Submit exception(s)").click()
+    cy.get("button").contains("Submit exception(s)").click()
 
-        cy.visit(`/bichard/court-cases/${resolvedCaseId}`)
+    cy.get(".Defendant-details-table").contains("1101ZD0100000448754K")
+  })
 
-        cy.get(".moj-badge").contains("Editable Field").should("not.exist")
-        cy.get("#asn").should("not.exist")
-    })
+  it("should display error when invalid ASN is entered", () => {
+    cy.login("bichard01@example.com", "password")
+    cy.visit("/bichard/court-cases/0")
 
-    it.only("Should be able to edit ASN field if HO100206 is raised", () => {
-        cy.login("bichard01@example.com", "password")
-        cy.visit("/bichard/court-cases/0")
+    cy.get("#asn").type("asdf")
 
-        cy.get(".moj-badge").contains("Editable Field").should("exist")
-        cy.get("#asn").type("1101ZD0100000448754K")
-
-        cy.get("button").contains("Submit exception(s)").click()
-
-        cy.contains("Are you sure you want to submit the amended details to the PNC and mark the exception(s) as resolved?").should("exist")
-        cy.get("button").contains("Submit exception(s)").click()
-
-        cy.contains("Bichard01: Portal Action: Update Applied. Element: asn. New Value: 1101ZD0100000448754K")
-        cy.contains("Bichard01: Portal Action: Resubmitted Message.")
-
-        verifyUpdatedMessage({
-            expectedCourtCase: { errorId: 0, errorStatus: "Submitted" },
-            updatedMessageNotHaveContent: ["<br7:ArrestSummonsNumber>AAAAAAAAAAAAAAAAAAAA</br7:ArrestSummonsNumber>"],
-            updatedMessageHaveContent: ["<br7:ArrestSummonsNumber>1101ZD0100000448754K</br7:ArrestSummonsNumber>"]
-        })
-    })
-
-    it("should display the updated ASN after submission", () => {
-        cy.login("bichard01@example.com", "password")
-        cy.visit("/bichard/court-cases/0")
-
-        cy.get(".Defendant-details-table").contains("AAAAAAAAAAAAAAAAAAAA")
-        cy.get("#asn").type("1101ZD0100000448754K")
-
-        cy.get("button").contains("Submit exception(s)").click()
-        cy.get("button").contains("Submit exception(s)").click()
-
-        cy.get(".Defendant-details-table").contains("1101ZD0100000448754K")
-    })
-
-    it("should display error when invalid ASN is entered", () => {
-        cy.login("bichard01@example.com", "password")
-        cy.visit("/bichard/court-cases/0")
-        
-        cy.get("#asn").type("asdf")
-        
-        cy.get(".Defendant-details-table").contains("Invalid ASN format")
-    })
+    cy.get(".Defendant-details-table").contains("Invalid ASN format")
+  })
 })
