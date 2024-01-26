7b835b5607f9ccb3c2874b553e2a6ad721529485
BushraAbdullahi <bushra1175@gmail.com>
2024-01-26 11:11:24 +0000

    WIP

---
+++ cypress/e2e/court-cases/court-case-details/generateDummyCourtcase.ts
@@ -0,0 +1,48 @@
+import nextHearingDateExceptions from "../../../../test/test-data/NextHearingDateExceptions.json"
+import dummyAho from "../../../../test/test-data/multipleHearingResultsOnOffence.json"
+import hashedPassword from "../../../fixtures/hashedPassword"
+
+describe("NextHearingDate", () => {
+  before(() => {
+    cy.task("clearCourtCases")
+    cy.task("clearUsers")
+    cy.task("insertUsers", {
+      users: [
+        {
+          username: "Bichard01",
+          visibleForces: ["001"],
+          forenames: "Bichard Test User",
+          surname: "01",
+          email: "bichard01@example.com",
+          password: hashedPassword
+        }
+      ],
+      userGroups: ["B7NewUI_grp", "B7GeneralHandler_grp"]
+    })
+  })
+
+  beforeEach(() => {
+    cy.task("clearCourtCases")
+    cy.task("insertCourtCasesWithFields", [
+      {
+        orgForPoliceFilter: "01",
+        hearingOutcome: nextHearingDateExceptions.hearingOutcomeXml,
+        updatedHearingOutcome: nextHearingDateExceptions.updatedHearingOutcomeXml,
+        errorCount: 1
+      }
+    ])
+  })
+
+  it("Should not be able to edit next hearing date field there is no exception", () => {
+    cy.task("insertCourtCasesWithFields", [
+      {
+        orgForPoliceFilter: "01",
+        hearingOutcome: dummyAho.hearingOutcomeXml,
+        errorCount: 1
+      }
+    ])
+
+    cy.login("bichard01@example.com", "password")
+    cy.visit("/bichard/court-cases/0")
+  })
+})
