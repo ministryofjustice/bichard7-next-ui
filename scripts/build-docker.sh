0eb532866939423940a97ff0a4906d41f221dcb2
Ian King <ian.antking@gmail.com>
2024-01-29 11:42:48 +0000

    remove reference to unavailable environment variables

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -44,10 +44,10 @@ function pull_and_build_from_aws() {
 if [ $(arch) = "arm64" ]
 then
     echo "Building for ARM"
-    docker build --build-arg WORKSPACE=${WORKSPACE} --build-arg "${TF_VAR_ui_deploy_tag}" --build-arg "BUILD_IMAGE=${DOCKER_IMAGE_HASH}" --platform=linux/arm64 -t ui .
+    docker build --build-arg "BUILD_IMAGE=${DOCKER_IMAGE_HASH}" --platform=linux/arm64 -t ui .
 else
     echo "Building regular image"
-    docker build --build-arg WORKSPACE=${WORKSPACE} "${TF_VAR_ui_deploy_tag}" --build-arg "BUILD_IMAGE=${DOCKER_IMAGE_HASH}" -t ui .
+    docker build --build-arg "BUILD_IMAGE=${DOCKER_IMAGE_HASH}" -t ui .
 fi
 
   if [[ -n "${CODEBUILD_RESOLVED_SOURCE_VERSION}" && -n "${CODEBUILD_START_TIME}" ]]; then
