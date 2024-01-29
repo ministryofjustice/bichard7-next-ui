801ad644948b21cf87a7374ee26bfc43985e5f30
Tausif <tausifpatel.dev@gmail.com>
2024-01-29 15:09:09 +0000

    Refactor helper method

--- src/utils/exceptions/hasNextHearingLocationException.ts
+++ src/utils/exceptions/hasNextHearingLocationException.ts
@@ -3,15 +3,13 @@ import { ExceptionCode } from "@moj-bichard7-developers/bichard7-next-core/core/
 import { DisplayFullCourtCase } from "../../types/display/CourtCases"
 import Phase from "@moj-bichard7-developers/bichard7-next-core/core/types/Phase"
 
-const isNextHearingLocationEditable = (exceptions: Exception[], courtCase: DisplayFullCourtCase): boolean => {
-  const hasNextHearingLocationException = exceptions.some(
+const hasNextHearingLocationException = (exceptions: Exception[]): boolean =>
+  exceptions.some(
     (exception) =>
       exception.path.join(".").endsWith(".NextResultSourceOrganisation.OrganisationUnitCode") &&
       (exception.code === ExceptionCode.HO100200 ||
         exception.code === ExceptionCode.HO100300 ||
         exception.code === ExceptionCode.HO100322)
   )
-  return hasNextHearingLocationException && courtCase.canUserEditExceptions && courtCase.phase === Phase.HEARING_OUTCOME
-}
 
-export default isNextHearingLocationEditable
+export default hasNextHearingLocationException
