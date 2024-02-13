5c350de60e518238ee20aaafdb03755e1501a0ae
Tausif <tausifpatel.dev@gmail.com>
2024-02-13 12:42:47 +0000

    Refactor unit tests

--- src/utils/nextHearingDateValidationError.test.ts
+++ src/utils/nextHearingDateValidationError.test.ts
@@ -1,45 +1,16 @@
-import { DisplayFullCourtCase } from "../types/display/CourtCases"
 import { AmendmentRecords } from "../types/Amendments"
 import nextHearingDateValidationError from "./nextHearingDateValidationError"
+import createDummyAho from "../../test/helpers/createDummyAho"
+import HO100102 from "../../test/helpers/exceptions/HO100102"
+import HO100323 from "../../test/helpers/exceptions/HO100323"
 
 describe("nextHearingDateValidationError", () => {
-  const exceptionHO100102 = {
-    code: "HO100102",
-    path: [
-      "AnnotatedHearingOutcome",
-      "HearingOutcome",
-      "Case",
-      "HearingDefendant",
-      "Offence",
-      0,
-      "Result",
-      0,
-      "NextHearingDate"
-    ]
-  }
-
-  const exceptionHO100323 = {
-    code: "HO100323",
-    path: [
-      "AnnotatedHearingOutcome",
-      "HearingOutcome",
-      "Case",
-      "HearingDefendant",
-      "Offence",
-      0,
-      "Result",
-      0,
-      "NextHearingDate"
-    ]
-  }
-
-  const courtCase = {
-    aho: {
-      Exceptions: [exceptionHO100102]
-    }
-  } as unknown as DisplayFullCourtCase
+  const dummyAho = createDummyAho()
 
   it("should return true if next hearing date editable field is empty", () => {
+    dummyAho.Exceptions.length = 0
+    HO100102(dummyAho)
+
     const amendments = {
       asn: "1101ZD0100000448754K",
       nextHearingDate: [
@@ -50,12 +21,15 @@ describe("nextHearingDateValidationError", () => {
         }
       ]
     } as unknown as AmendmentRecords
-    const result = nextHearingDateValidationError(courtCase.aho.Exceptions, amendments)
+    const result = nextHearingDateValidationError(dummyAho.Exceptions, amendments)
 
     expect(result).toBe(true)
   })
 
   it("should return false if a value is entered into the next hearing date editable field", () => {
+    dummyAho.Exceptions.length = 0
+    HO100102(dummyAho)
+
     const amendments = {
       asn: "1101ZD0100000448754K",
       nextHearingDate: [
@@ -67,17 +41,15 @@ describe("nextHearingDateValidationError", () => {
       ]
     } as unknown as AmendmentRecords
 
-    const result = nextHearingDateValidationError(courtCase.aho.Exceptions, amendments)
+    const result = nextHearingDateValidationError(dummyAho.Exceptions, amendments)
 
     expect(result).toBe(false)
   })
 
   it("should return true if one of the next hearing location editable fields remained empty", () => {
-    const courtCaseWithMultipleExceptions = {
-      aho: {
-        Exceptions: [exceptionHO100102, exceptionHO100323]
-      }
-    } as unknown as DisplayFullCourtCase
+    dummyAho.Exceptions.length = 0
+    HO100102(dummyAho)
+    HO100323(dummyAho)
 
     const amendments = {
       asn: "1101ZD0100000448754K",
@@ -95,17 +67,15 @@ describe("nextHearingDateValidationError", () => {
       ]
     } as unknown as AmendmentRecords
 
-    const result = nextHearingDateValidationError(courtCaseWithMultipleExceptions.aho.Exceptions, amendments)
+    const result = nextHearingDateValidationError(dummyAho.Exceptions, amendments)
 
     expect(result).toBe(true)
   })
 
   it("should return false if multiple hearing date editable fields have values", () => {
-    const courtCaseWithMultipleExceptions = {
-      aho: {
-        Exceptions: [exceptionHO100102, exceptionHO100323]
-      }
-    } as unknown as DisplayFullCourtCase
+    dummyAho.Exceptions.length = 0
+    HO100102(dummyAho)
+    HO100323(dummyAho)
 
     const amendments = {
       asn: "1101ZD0100000448754K",
@@ -123,7 +93,7 @@ describe("nextHearingDateValidationError", () => {
       ]
     } as unknown as AmendmentRecords
 
-    const result = nextHearingDateValidationError(courtCaseWithMultipleExceptions.aho.Exceptions, amendments)
+    const result = nextHearingDateValidationError(dummyAho.Exceptions, amendments)
 
     expect(result).toBe(false)
   })
