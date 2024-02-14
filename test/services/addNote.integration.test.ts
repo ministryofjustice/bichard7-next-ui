55dfc89730dbbfc7b7223429d8303014d5ad2d3b
Sen <65850511+Senny10@users.noreply.github.com>
2024-02-14 12:31:20 +0000

    Bicaws7 1497 Update Note Text Length New UI (#551)
    * Update MAX_NOTE_LENGTH constant
    
    * Increase MaxNoteLength to 2000
    
    * Update character limit for notes
    
    * Update character limit in note text field
    
    * Add a helper method
    
    * Create a function to handle add-exceptions button state
    
    * Add tests for editableFieldsValidationError function
    
    * Fix asn-editable-field cypress test
    
    * Add a helper function
    
    * Add and refactor ASN-editable-field cypress tests
    
    * Refactor tests to reflect the disable or enable behaviour of the submit exceptions button
    
    * Refactor Next-hearing-location tests to reflect the disable or enable behaviour of the submit exceptions button
    
    * Amend helper method to change the submit button's action for ASN exceptions
    
    * install and configure stylelint
    
    * run linter in fix mode
    
    * ignore linting errors related to govuk
    
    * restore .note-preview-row
    
    * Remove urgent column and filter section (#543)
    
    * remove urgent column
    
    * remove urgency filters
    
    * WIP
    
    * WIP
    
    * remove tests for sorting urgency
    
    * refactor tests
    
    * WIP
    
    * WIP
    
    * WIP
    
    * refactor tests
    
    * Add submit exceptions button cypress tests
    
    * remove extra cell from court case list entry
    
    * fix case list page test
    
    * Separate all validationError functions
    
    * Refactor unit tests
    
    * Remove unneed detail
    
    * First iteration of re-design
    
    * Linter issue
    
    * Use visiblity to help show court name
    
    * Fix media query
    
    * Remove court reference number
    
    * Refactor a unit test
    
    * Update note text length limit to 2000 characters
    
    * Update note length in addNote.integration.test.ts
    
    ---------
    
    Co-authored-by: Tausif <tausifpatel.dev@gmail.com>
    Co-authored-by: Ian King <ian.antking@gmail.com>
    Co-authored-by: BushraAbdullahi <103956365+BushraAbdullahi@users.noreply.github.com>
    Co-authored-by: Richard Race <1273965+richrace@users.noreply.github.com>

--- test/services/addNote.integration.test.ts
+++ test/services/addNote.integration.test.ts
@@ -114,18 +114,18 @@ describe("addNote", () => {
     expect(insertNotes).toHaveBeenCalledTimes(0)
   })
 
-  it("Should add multiple notes when note text length is more than the 1000 characters", async () => {
+  it("Should add multiple notes when note text length is more than the 2000 characters", async () => {
     await insertCourtCasesWithFields([
       { messageId: uuid(), ...existingCourtCasesDbObject, errorLockedByUsername: currentUsername }
     ])
 
-    const result = await addNote(dataSource, 0, currentUsername, "A".repeat(2503))
+    const result = await addNote(dataSource, 0, currentUsername, "A".repeat(4503))
     expect(result).toStrictEqual({ isSuccessful: true })
 
     expect(insertNotes).toHaveBeenCalledTimes(1)
     expect(insertNotes).toHaveBeenCalledWith(expect.anything(), [
-      { errorId: 0, noteText: "A".repeat(1000), userId: currentUsername },
-      { errorId: 0, noteText: "A".repeat(1000), userId: currentUsername },
+      { errorId: 0, noteText: "A".repeat(2000), userId: currentUsername },
+      { errorId: 0, noteText: "A".repeat(2000), userId: currentUsername },
       { errorId: 0, noteText: "A".repeat(503), userId: currentUsername }
     ])
   })
