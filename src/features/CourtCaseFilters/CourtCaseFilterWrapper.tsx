d226c8e90abd719255db26e763020b3839fb891d
BushraAbdullahi <bushra1175@gmail.com>
2024-02-16 12:08:36 +0000

    WIP

--- src/features/CourtCaseFilters/CourtCaseFilterWrapper.tsx
+++ src/features/CourtCaseFilters/CourtCaseFilterWrapper.tsx
@@ -18,36 +18,38 @@ const CourtCaseFilterWrapper: React.FC<Props> = ({
   const [areAppliedFiltersShown, setAreAppliedFiltersShown] = useState(false)
   return (
     <>
-      <div className="moj-filter-layout__filter">
-        <div className={areAppliedFiltersShown ? "moj-filter" : "moj-filter moj-hidden"}>{filter}</div>
-      </div>
-      <div className="moj-filter-layout__content">
-        <div className="moj-button-menu">
-          <div className="moj-action-bar">
-            <button
-              data-module="govuk-button"
-              id="filter-button"
-              className="govuk-button govuk-button--secondary govuk-!-margin-bottom-0"
-              type="button"
-              aria-haspopup="true"
-              aria-expanded="false"
-              onClick={() => {
-                setAreAppliedFiltersShown(!areAppliedFiltersShown)
-              }}
-            >
-              {areAppliedFiltersShown ? "Hide search panel" : "Show search panel"}
-            </button>
-            {!areAppliedFiltersShown && <div className="moj-button-menu__wrapper">{appliedFilters}</div>}
+      <div className={` moj-filter-layout`}>
+        <div className="moj-filter-layout__filter">
+          <div className={areAppliedFiltersShown ? "moj-filter" : "moj-filter moj-hidden"}>{filter}</div>
+        </div>
+        <div className="moj-filter-layout__content">
+          <div className="moj-button-menu">
+            <div className="moj-action-bar">
+              <button
+                data-module="govuk-button"
+                id="filter-button"
+                className="govuk-button govuk-button--secondary govuk-!-margin-bottom-0"
+                type="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+                onClick={() => {
+                  setAreAppliedFiltersShown(!areAppliedFiltersShown)
+                }}
+              >
+                {areAppliedFiltersShown ? "Hide search panel" : "Show search panel"}
+              </button>
+              {!areAppliedFiltersShown && <div className="moj-button-menu__wrapper">{appliedFilters}</div>}
+            </div>
           </div>
+
+          {paginationTop}
+
+          <div className="moj-scrollable-pane">
+            <div className="moj-scrollable-pane__wrapper">{courtCaseList}</div>
+          </div>
+
+          {paginationBottom}
         </div>
-
-        {paginationTop}
-
-        <div className="moj-scrollable-pane">
-          <div className="moj-scrollable-pane__wrapper">{courtCaseList}</div>
-        </div>
-
-        {paginationBottom}
       </div>
     </>
   )
