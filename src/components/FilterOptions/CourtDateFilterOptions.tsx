88b3f0e3302d0c91bfe9312801497caba21c7082
Sen <65850511+Senny10@users.noreply.github.com>
2024-02-16 17:32:13 +0000

    Update case age options and filter label

--- src/components/FilterOptions/CourtDateFilterOptions.tsx
+++ src/components/FilterOptions/CourtDateFilterOptions.tsx
@@ -33,7 +33,7 @@ const getCaseAgeWithFormattedDate = (namedCaseAge: string): string => {
 
   const dateRange = [caseAge].flat()[0]
 
-  return namedCaseAge === "Day 15 and older"
+  return namedCaseAge === "15 days ago and older"
     ? `${namedCaseAge} (up to ${formatDisplayedDate(dateRange.to)})`
     : `${namedCaseAge} (${formatDisplayedDate(dateRange.from)})`
 }
@@ -73,7 +73,7 @@ const CourtDateFilterOptions: React.FC<Props> = ({ caseAges, caseAgeCounts, disp
           id={"case-age"}
           dataAriaControls={"conditional-case-age"}
           defaultChecked={caseAges && caseAges.length > 0 ? true : false}
-          label={"Case age (SLA)"}
+          label={"Case Recieved"}
         />
         <div className="govuk-radios__conditional" id="conditional-case-age">
           <div className={classes["scrollable-case-ages"]}>
