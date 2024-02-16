9e9f42f0db08132bcb1b847212b3c27d570f0e5e
Tausif Patel <tausifpatel.dev@gmail.com>
2024-02-16 15:44:07 +0000

    Add offence matching exception error prompt

--- cypress/e2e/case-details/handle-triggers-and-exceptions/view-offence-matching-exception-prompt.cy.ts
+++ cypress/e2e/case-details/handle-triggers-and-exceptions/view-offence-matching-exception-prompt.cy.ts
@@ -1,4 +1,6 @@
 import hashedPassword from "../../../fixtures/hashedPassword"
+import ExceptionHO100304 from "../../../../test/test-data/HO100304.json"
+import NextHearingDateExceptions from "../../../../test/test-data/NextHearingDateExceptions.json"
 
 describe("View offence matching exception prompts", () => {
   before(() => {
@@ -18,5 +20,49 @@ describe("View offence matching exception prompts", () => {
       userGroups: ["B7NewUI_grp", "B7GeneralHandler_grp"]
     })
   })
-  it("Should not display an error prompt when a HO100304 is not raised", () => {})
+
+  it("Should not display an error prompt when HO100304 is not raised", () => {
+    cy.task("clearCourtCases")
+    cy.task("insertCourtCasesWithFields", [
+      {
+        orgForPoliceFilter: "01",
+        hearingOutcome: NextHearingDateExceptions.hearingOutcomeXml,
+        errorCount: 1,
+        errorLockedByUsername: "Bichard01"
+      }
+    ])
+
+    cy.login("bichard01@example.com", "password")
+    cy.visit(`/bichard/court-cases/0`)
+
+    cy.get("ul.moj-sub-navigation__list").contains("Offences").click()
+    cy.get(".govuk-link").contains("Offence with HO100102 - INCORRECTLY FORMATTED DATE EXCEPTION").click()
+
+    cy.get(".offences-table").contains("td", "Matched PNC offence").should("not.exist")
+  })
+
+  it("Should display an error prompt when HO100304 is raised", () => {
+    cy.task("clearCourtCases")
+    cy.task("insertCourtCasesWithFields", [
+      {
+        orgForPoliceFilter: "01",
+        hearingOutcome: ExceptionHO100304.hearingOutcomeXml,
+        errorCount: 1,
+        errorLockedByUsername: "Bichard01"
+      }
+    ])
+
+    cy.login("bichard01@example.com", "password")
+    cy.visit(`/bichard/court-cases/0`)
+
+    cy.get("ul.moj-sub-navigation__list").contains("Offences").click()
+    cy.get(".govuk-link").contains("Theft of pedal cycle").click()
+
+    cy.get(".offences-table")
+      .contains("td", "Matched PNC offence")
+      .should(
+        "include.text",
+        "If offences appear to match, then check if offence dates match also. After this manually result on the PNC, to deal with error."
+      )
+  })
 })
--- src/types/ErrorMessages.ts
+++ src/types/ErrorMessages.ts
@@ -8,7 +8,8 @@ enum ErrorMessages {
   HO100306ErrorPrompt = "This code could not be recognised. If this is a new offence code update the PNC and mark the case as manually resolved. Otherwise if this offence code is incorrect contact the courts to correct it.",
   HO200113 = "The PNC cannot be updated automatically because there are new remands together with sentencing. Please resolve this exception by manually updating the PNC with correct code.",
   HO200114 = "The PNC cannot be updated automatically because there are changes to existing disposals together with sentencing. Please resolve this exception by manually updating the PNC with correct code.",
-  HO100307 = "This code could not be found via look-up, report the issue to Bichard 7 team and the courts for the correct so that they can investigate this issue and advise."
+  HO100307 = "This code could not be found via look-up, report the issue to Bichard 7 team and the courts for the correct so that they can investigate this issue and advise.",
+  HO100304 = "If offences appear to match, then check if offence dates match also. After this manually result on the PNC, to deal with error."
 }
 
 const findExceptions = (
