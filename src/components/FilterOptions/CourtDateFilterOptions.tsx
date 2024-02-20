ea00b932707a49cf5c4818f1669c05cebed7dc07
Sen <65850511+Senny10@users.noreply.github.com>
2024-02-20 11:29:02 +0000

    BICAWS7 2562 change filter option title on case age to case recieved (#554)
    * Add swc dependencies for different platforms
    
    * Update case age options and filter label
    
    * Update CourtDateFilterOptions and caseAgeOptions
    
    * Refactor case age options and update case age display
    
    * Remove console.log statement in CourtDateFilterOptions.tsx
    
    * Refactor date range keys in validateCaseAges.test and getCountOfCasesByCaseAge.integration.test
    
    * Update case age filter labels e2e tests

--- src/components/FilterOptions/CourtDateFilterOptions.tsx
+++ src/components/FilterOptions/CourtDateFilterOptions.tsx
@@ -33,8 +33,8 @@ const getCaseAgeWithFormattedDate = (namedCaseAge: string): string => {
 
   const dateRange = [caseAge].flat()[0]
 
-  return namedCaseAge === "Day 15 and older"
-    ? `${namedCaseAge} (up to ${formatDisplayedDate(dateRange.to)})`
+  return namedCaseAge === "15 days ago and older"
+    ? `15 days ago and older (up to ${formatDisplayedDate(dateRange.to)})`
     : `${namedCaseAge} (${formatDisplayedDate(dateRange.from)})`
 }
 
@@ -73,7 +73,7 @@ const CourtDateFilterOptions: React.FC<Props> = ({ caseAges, caseAgeCounts, disp
           id={"case-age"}
           dataAriaControls={"conditional-case-age"}
           defaultChecked={caseAges && caseAges.length > 0 ? true : false}
-          label={"Case age (SLA)"}
+          label={"Case Recieved"}
         />
         <div className="govuk-radios__conditional" id="conditional-case-age">
           <div className={classes["scrollable-case-ages"]}>
