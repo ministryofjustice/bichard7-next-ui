b2f91df63822c73f3950cabd19a6744dc33e4b4c
Sen <65850511+Senny10@users.noreply.github.com>
2024-02-25 17:02:50 +0000

    Remove unnecessary assertion in switchingFeedback.cy.ts

--- cypress/e2e/switchingFeedback.cy.ts
+++ cypress/e2e/switchingFeedback.cy.ts
@@ -130,7 +130,6 @@ describe("Switching Bichard Version Feedback Form", () => {
     cy.contains("Back").click()
 
     cy.url().should("match", /\/court-cases\/\d+/)
-    cy.findByText("Case details").should("exist")
   })
 
   it("Should access the switching feedback form with first question visible when user clicks 'Switch to old Bichard'", () => {
