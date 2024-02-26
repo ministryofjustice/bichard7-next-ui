a7639c39942256ca94bb6410063e6af2c7634bad
Joe Folkard <joe.folkard@madetech.com>
2024-02-26 15:26:00 +0000

    Moved pinned packages to .ncurc file

--- .ncurc.js
+++ .ncurc.js
@@ -1,6 +1,6 @@
-const semver = new RegExp(
-  /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/
-) // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+// const semver = new RegExp(
+//   /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/
+// ) // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
 
 /*
   Pinned:
@@ -11,25 +11,26 @@ const semver = new RegExp(
   - next
     - 13.4.13 causes failures with fetch
 */
-const pinned = ["styled-components", "date-fns"]
+const pinned = ["styled-components", "date-fns", "@typescript-eslint/eslint-plugin", "@typescript-eslint/parser"]
 const ignored = []
 const skipped = [{ package: "next", version: "13.4.13" }]
 
 module.exports = {
-  target: (package) => {
-    if (pinned.some((pin) => pin === package)) {
+  target: (pkg) => {
+    if (pinned.some((pin) => pin === pkg)) {
       const res = "minor"
-      console.log(` ${package} is pinned to ${res} upgrades only (.ncurc.js)`)
+      console.debug(` ${pkg} is pinned to ${res} upgrades only (.ncurc.js)`)
       return res
     }
     return "latest"
   },
 
-  filterResults: (package, { upgradedVersion }) => {
-    if (ignored.some((ignore) => ignore.package === package)) {
-      return
-    } else if (skipped.some((skip) => skip.package === package && skip.version === upgradedVersion)) {
-      return
+  filterResults: (pkg, { upgradedVersion }) => {
+    if (ignored.some((ignore) => ignore.pkg === pkg)) {
+      return null
+    }
+    if (skipped.some((skip) => skip.pkg === pkg && skip.version === upgradedVersion)) {
+      return null
     }
     return true
   }
