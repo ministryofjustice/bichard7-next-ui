0c3a9e5e9377f8d835f832ce3497cbf93aac0e94
Richard Race <1273965+richrace@users.noreply.github.com>
2024-03-04 14:31:16 +0000

    Make it more readable

--- src/utils/isAsnFormatValid.ts
+++ src/utils/isAsnFormatValid.ts
@@ -1,8 +1,8 @@
 import Asn from "services/Asn"
 
 const isAsnFormatValid = (asn: string): boolean => {
-  const checkDigit = /[A-HJ-NP-RT-Z]{1}/
-  const validFormat = new RegExp(`^\d{2}[A-Z0-9]{6,7}\d{11}${checkDigit}$`).test(asn)
+  const checkDigitRegex = /[A-HJ-NP-RT-Z]{1}/
+  const validFormat = new RegExp(`^\d{2}[A-Z0-9]{6,7}\d{11}${checkDigitRegex}$`).test(asn)
   const validCheckDigit = new Asn(asn).checkCharacter() === asn.slice(-1)
   return validFormat && validCheckDigit
 }
