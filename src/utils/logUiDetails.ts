214277bd63f8c15ac923386cc951069e8e49d750
BushraAbdullahi <bushra1175@gmail.com>
2024-03-04 11:15:34 +0000

    WIP

--- src/utils/logUiDetails.ts
+++ src/utils/logUiDetails.ts
@@ -1,5 +1,5 @@
 export function logUiDetails(environment: string | null, build: string | null): void {
-  // console.clear()
+  console.clear()
   console.table({
     environment: environment ?? "local",
     build: build ?? "local"
