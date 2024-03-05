86fed943f54559f4c6a7ab219cbd0d247737181a
Joe Folkard <joe.folkard@madetech.com>
2024-03-05 16:51:42 +0000

    Unpinned eslint packages

--- .ncurc.js
+++ .ncurc.js
@@ -12,7 +12,7 @@
   - next
     - 13.4.13 causes failures with fetch
 */
-const pinned = ["styled-components", "date-fns", "@typescript-eslint/eslint-plugin", "@typescript-eslint/parser"]
+const pinned = ["styled-components", "date-fns"]
 const ignored = []
 const skipped = [{ package: "next", version: "13.4.13" }]
 
