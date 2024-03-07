58a4def4d8e41d15a254a0ad83606c33038523b7
Sen <65850511+Senny10@users.noreply.github.com>
2024-03-07 14:16:00 +0000

    WIP

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -2,7 +2,7 @@ import { Button } from "govuk-react"
 import { useRouter } from "next/router"
 import { ReactNode } from "react"
 
-interface Props extends React.ComponentProps<"button"> {
+interface LinkButtonProps extends React.ComponentProps<"button"> {
   children: ReactNode
   href: string
   buttonColour?: string
@@ -10,15 +10,24 @@ interface Props extends React.ComponentProps<"button"> {
   buttonShadowColour?: string
 }
 
-const LinkButton: React.FC<Props> = ({
+interface ReactiveLinkButtonProps extends React.ComponentProps<"button"> {
+  children: ReactNode
+  buttonColour?: string
+  buttonTextColour?: string
+  buttonShadowColour?: string
+  onClick: (event: React.MouseEvent<HTMLElement>) => void
+}
+
+const LinkButton: React.FC<LinkButtonProps> = ({
   children,
   href,
   buttonColour,
   buttonShadowColour,
   buttonTextColour,
   ...buttonProps
-}: Props) => {
+}: LinkButtonProps) => {
   const { asPath, basePath } = useRouter()
+
   return (
     <a href={href.startsWith("/") ? href : `${basePath}${asPath}/${href}`}>
       <Button
@@ -33,4 +42,32 @@ const LinkButton: React.FC<Props> = ({
   )
 }
 
+const ReactiveLinkButton: React.FC<ReactiveLinkButtonProps> = ({
+  children,
+  buttonColour,
+  buttonShadowColour,
+  buttonTextColour,
+  onClick,
+  ...buttonProps
+}: ReactiveLinkButtonProps) => {
+  const handleOnClick = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault()
+    onClick(event)
+  }
+
+  return (
+    <LinkButton
+      href={"#"}
+      onClick={handleOnClick}
+      buttonColour={buttonColour}
+      buttonTextColour={buttonTextColour}
+      buttonShadowColour={buttonShadowColour}
+      {...buttonProps}
+    >
+      {children}
+    </LinkButton>
+  )
+}
+
+export { LinkButton, ReactiveLinkButton }
 export default LinkButton
