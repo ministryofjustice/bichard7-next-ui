379162673e8d8cc06a64607e1e1d281eb27dc617
Richard Cane <richard.cane@madetech.com>
2024-03-21 15:10:24 +0000

    Only allow matching offence codes

--- src/components/OffenceMatcher.tsx
+++ src/components/OffenceMatcher.tsx
@@ -1,12 +1,19 @@
+import { Offence } from "@moj-bichard7-developers/bichard7-next-core/core/types/AnnotatedHearingOutcome"
 import { useCourtCase } from "context/CourtCaseContext"
+import getOffenceCode from "utils/getOffenceCode"
 
-export const OffenceMatcher = () => {
+interface Props {
+  offence: Offence
+}
+
+export const OffenceMatcher = ({ offence }: Props) => {
   const {
     aho: { PncQuery: pncQuery }
   } = useCourtCase()
 
+  const offenceCode = getOffenceCode(offence)
+
   // TODO: prevent matching twice
-  // TODO: match offence codes
   // TODO: match dates
   return (
     <select className="govuk-select">
@@ -14,13 +21,15 @@ export const OffenceMatcher = () => {
       {pncQuery?.courtCases?.map((c) => {
         return (
           <optgroup key={c.courtCaseReference} label={c.courtCaseReference}>
-            {c.offences.map((o) => {
-              return (
-                <option key={o.offence.cjsOffenceCode} value={o.offence.sequenceNumber}>
-                  {`${String(o.offence.sequenceNumber).padStart(3, "0")} - ${o.offence.cjsOffenceCode}`}
-                </option>
-              )
-            })}
+            {c.offences
+              .filter((o) => o.offence.cjsOffenceCode === offenceCode)
+              .map((o) => {
+                return (
+                  <option key={o.offence.cjsOffenceCode} value={o.offence.sequenceNumber}>
+                    {`${String(o.offence.sequenceNumber).padStart(3, "0")} - ${o.offence.cjsOffenceCode}`}
+                  </option>
+                )
+              })}
           </optgroup>
         )
       })}
