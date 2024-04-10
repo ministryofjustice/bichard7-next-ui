d5c1538ba4f9eaeb9c9c53736be375932b211398
Richard Race <1273965+richrace@users.noreply.github.com>
2024-04-10 14:34:09 +0100

    Fix more corrections

--- src/components/CustomDateInput/DateInput.tsx
+++ src/components/CustomDateInput/DateInput.tsx
@@ -17,18 +17,18 @@ const SmallButton = styled.button`
   font-size: 1em;
   text-decoration: underline;
   cursor: pointer;
-  &:hover: {
+  &:hover {
     color: white;
     background: ${blue};
   }
-  &:hover img: {
+  &:hover img {
     filter: invert(1);
   }
-  &:focus: {
+  &:focus {
     color: ${gdsBlack};
     background: ${yellow};
   }
-  &:focus img: {
+  &:focus img {
     filter: contrast(1);
   }
 `
--- src/features/CourtCaseFilters/FilterChipSection.tsx
+++ src/features/CourtCaseFilters/FilterChipSection.tsx
@@ -23,7 +23,7 @@ const HeaderRow = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  & div:nth-child(1): {
+  & div:nth-child(1) {
     flex: 1;
   }
 `
