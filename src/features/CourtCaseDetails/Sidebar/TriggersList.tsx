668db0ddc21a11b9ff6a4252b3e729c8c6860e8d
Richard Race <1273965+richrace@users.noreply.github.com>
2024-04-11 15:45:26 +0100

    More extraction

--- src/features/CourtCaseDetails/Sidebar/TriggersList.tsx
+++ src/features/CourtCaseDetails/Sidebar/TriggersList.tsx
@@ -9,36 +9,16 @@ import { useRouter } from "next/router"
 import { encode } from "querystring"
 import { ChangeEvent, SyntheticEvent, useState } from "react"
 
-import styled from "styled-components"
 import type NavigationHandler from "types/NavigationHandler"
 import { triggersAreLockedByAnotherUser } from "utils/caseLocks"
 import Form from "../../../components/Form"
 import LockStatusTag from "../LockStatusTag"
 import Trigger from "./Trigger"
+import { LockStatus, MarkCompleteGridCol, SelectAllTriggersGridRow } from "./TriggersList.styles"
 
 interface Props {
   onNavigate: NavigationHandler
 }
-
-const SelectAllTriggersGridRow = styled(GridRow)`
-  text-align: right;
-  padding-bottom: 20px;
-  cursor: pointer;
-  font-size: 1em;
-`
-
-const MarkCompleteGridCol = styled(GridCol)`
-  display: flex;
-  justify-content: end;
-  margin-bottom: 0;
-`
-
-const LockStatus = styled.div`
-  padding-top: 20px;
-  display: flex;
-  justify-content: flex-end;
-  margin-bottom: 0;
-`
 
 const TriggersList = ({ onNavigate }: Props) => {
   const currentUser = useCurrentUser()
