24318c4707648ab33adf65bec3bacae38a4b7077
Richard Race <1273965+richrace@users.noreply.github.com>
2024-04-11 09:15:12 +0100

    Move styled-components to their own 'styles' suffixed files

--- src/components/CustomDateInput/DateInput.tsx
+++ src/components/CustomDateInput/DateInput.tsx
@@ -1,37 +1,7 @@
 import { Dispatch } from "react"
-import styled from "styled-components"
 import { SerializedCourtDateRange } from "types/CaseListQueryParams"
 import { FilterAction } from "types/CourtCaseFilter"
-import { blue, gdsBlack, yellow } from "utils/colours"
-
-const SmallButton = styled.button`
-  display: inline-flex;
-  flex-wrap: nowrap;
-  align-items: center;
-  flex-direction: row;
-  padding: 0px;
-  border: none;
-  background-color: white;
-  gap: 11px;
-  color: ${blue};
-  font-size: 1em;
-  text-decoration: underline;
-  cursor: pointer;
-  &:hover {
-    color: white;
-    background: ${blue};
-  }
-  &:hover img {
-    filter: invert(1);
-  }
-  &:focus {
-    color: ${gdsBlack};
-    background: ${yellow};
-  }
-  &:focus img {
-    filter: contrast(1);
-  }
-`
+import { SmallButton } from "./DateInput.styles"
 
 interface Props {
   dateType: "from" | "to"
--- src/features/CourtCaseList/tags/LockedByTag/LockedByButton.tsx
+++ src/features/CourtCaseList/tags/LockedByTag/LockedByButton.tsx
@@ -1,7 +1,6 @@
 import { useCsrfToken } from "context/CsrfTokenContext"
-import styled from "styled-components"
-import { blue, gdsBlack, tagBlue, textBlue, yellow } from "utils/colours"
 import Form from "../../../../components/Form"
+import { StyledLockedByButton } from "./LockedByButton.styles"
 import LockedImage from "./LockedImage"
 
 interface UnlockConfirmationProps {
@@ -42,35 +41,6 @@ interface LockedByButtonProps {
   showUnlockConfirmation: boolean
   setShowUnlockConfirmation: (value: boolean) => void
 }
-
-const StyledLockedByButton = styled.button`
-  display: inline-flex;
-  flex-wrap: nowrap;
-  align-items: center;
-  flex-direction: row;
-  padding: 8px 18px 8px 8px;
-  border: none;
-  gap: 11px;
-  background-color: ${tagBlue};
-  color: ${textBlue};
-  font-size: 1em;
-  text-decoration: underline;
-  cursor: pointer;
-  &:hover {
-    color: white;
-    background: ${blue};
-  }
-  &:hover img {
-    filter: invert(1);
-  }
-  &:focus {
-    color: ${gdsBlack};
-    background: ${yellow};
-  }
-  &:focus img {
-    filter: contrast(1);
-  }
-`
 
 const LockedByButton = ({
   lockedBy,
