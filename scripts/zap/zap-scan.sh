a50e205751d0e9f7cb6646bf2cbaad644a803dfe
Richard Race <1273965+richrace@users.noreply.github.com>
2024-04-15 10:27:12 +0100

    Update the docker image

--- scripts/zap/zap-scan.sh
+++ scripts/zap/zap-scan.sh
@@ -2,7 +2,7 @@ docker run \
   --rm \
   --network="bichard_default" \
   -v $(pwd)/scripts/zap:/zap/wrk:rw \
-  -t owasp/zap2docker-stable \
+  -t ghcr.io/zaproxy/zaproxy:bare \
   zap-baseline.py -I -j \
     -t https://ui/bichard \
     -c /zap/wrk/zap-ignore.config \
