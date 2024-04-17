b8033f9dfead7ef3b18d34154db42ff1afb1e913
BushraAbdullahi <bushra1175@gmail.com>
2024-04-17 11:39:20 +0100

    change class name

--- src/features/CourtCaseDetails/Tabs/Panels/TableRow.tsx
+++ src/features/CourtCaseDetails/Tabs/Panels/TableRow.tsx
@@ -11,12 +11,12 @@ interface TableRowProps {
 export const TableRow = ({ label, value, hintText, className }: TableRowProps) => {
   const rowClassName = `table-row__${label.replaceAll(/ /g, "-").toLowerCase()}`
   return (
-    <StyledTableRow className={`table-row ${rowClassName} row`}>
+    <StyledTableRow className={`table-row ${rowClassName} row ${className}`}>
       <Table.Cell className="row-label">
         <b>{label}</b>
         {hintText && <StyledHintText className={"hint-text"}>{hintText}</StyledHintText>}
       </Table.Cell>
-      <Table.Cell className="row-label">{value}</Table.Cell>
+      <Table.Cell className="row-value">{value}</Table.Cell>
     </StyledTableRow>
   )
 }
