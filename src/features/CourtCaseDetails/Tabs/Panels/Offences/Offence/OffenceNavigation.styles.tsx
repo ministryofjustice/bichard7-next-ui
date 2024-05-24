3237587ad9a8f5fd5962b676794b4c0094bab900
Richard Race <1273965+richrace@users.noreply.github.com>
2024-05-24 14:44:06 +0100

    Add better buttons for navigating offences

--- src/features/CourtCaseDetails/Tabs/Panels/Offences/Offence/OffenceNavigation.styles.tsx
+++ src/features/CourtCaseDetails/Tabs/Panels/Offences/Offence/OffenceNavigation.styles.tsx
@@ -2,6 +2,9 @@ import styled from "styled-components"
 
 const PreviousButton = styled.div`
   text-align: right;
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
 `
 
 const NextButton = styled.div`
