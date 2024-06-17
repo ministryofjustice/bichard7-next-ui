90f615362a2f9d5bdb0556729eec25ef859770a7
Tausif Patel <tausifpatel.dev@gmail.com>
2024-06-17 16:35:59 +0100

    Add Checkmark and Warning icons

--- src/utils/colours.ts
+++ src/utils/colours.ts
@@ -11,6 +11,8 @@ const blue = "#1D70B8"
 const yellow = "#FFDD00"
 const gdsBlack = "#0b0c0c"
 const grey = "#b0b4b6"
+const gdsGreen = "#00703c"
+const gdsRed = "#d4351c"
 
 export {
   blue,
@@ -25,5 +27,7 @@ export {
   textBlue,
   textPrimary,
   textSecondary,
-  yellow
+  yellow,
+  gdsGreen,
+  gdsRed
 }
