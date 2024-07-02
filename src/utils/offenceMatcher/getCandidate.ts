768ba74797eff5691cf2cde0f8bcbadc1fc13631
csabagyorfi <csaba@madetech.com>
2024-07-02 17:14:20 +0100

    Add parseJsonObjectWithDates helper

--- src/utils/offenceMatcher/getCandidate.ts
+++ src/utils/offenceMatcher/getCandidate.ts
@@ -3,38 +3,28 @@ import type {
   Offence
 } from "@moj-bichard7-developers/bichard7-next-core/core/types/AnnotatedHearingOutcome"
 import generateCandidate from "@moj-bichard7-developers/bichard7-next-core/core/phase1/enrichAho/enrichFunctions/matchOffencesToPnc/generateCandidate"
-
 import type { PncOffence } from "@moj-bichard7-developers/bichard7-next-core/core/types/PncQueryResult"
 import { CaseType } from "@moj-bichard7-developers/bichard7-next-core/core/phase1/enrichAho/enrichFunctions/matchOffencesToPnc/annotatePncMatch"
+
+const dateFormatRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/
+
+const parseJsonObjectWithDates = (jsonObject: HearingOutcome | PncOffence | Offence) =>
+  JSON.parse(JSON.stringify(jsonObject), (_, value) => {
+    return typeof value === "string" && dateFormatRegex.test(value) ? new Date(value) : value
+  })
+
 const getCandidate = (
   aho: HearingOutcome,
   pncOffence: PncOffence,
   offence: Offence,
   caseReference: string
 ): boolean => {
-  if (
-    offence.ConvictionDate &&
-    aho.Hearing.DateOfHearing &&
-    offence.ActualOffenceStartDate.StartDate &&
-    pncOffence.offence.startDate &&
-    offence.ActualOffenceEndDate?.EndDate &&
-    pncOffence.offence.endDate
-  ) {
-    offence.ConvictionDate = new Date(offence.ConvictionDate)
-    aho.Hearing.DateOfHearing = new Date(aho.Hearing.DateOfHearing)
-    offence.ActualOffenceStartDate.StartDate = new Date(offence.ActualOffenceStartDate.StartDate)
-    pncOffence.offence.startDate = new Date(pncOffence.offence.startDate)
-    offence.ActualOffenceEndDate.EndDate = new Date(offence.ActualOffenceEndDate?.EndDate)
-    pncOffence.offence.endDate = new Date(pncOffence.offence.endDate)
-
-    const candidate = generateCandidate(
-      offence,
-      { pncOffence, caseType: CaseType.court, caseReference },
-      aho.Hearing.DateOfHearing
-    )
-    return Boolean(candidate)
-  }
-  return false
+  const candidate = generateCandidate(
+    parseJsonObjectWithDates(offence),
+    { pncOffence: parseJsonObjectWithDates(pncOffence), caseType: CaseType.court, caseReference },
+    parseJsonObjectWithDates(aho).Hearing.DateOfHearing
+  )
+  return Boolean(candidate)
 }
 
 export default getCandidate
