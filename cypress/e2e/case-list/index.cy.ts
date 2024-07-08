49554e31b225136b455788ab9ee0b59291db2899
Csaba <csaba@madetech.com>
2024-07-08 12:54:24 +0100

    Only display unresolved exceptions/triggers in case list (#664)
    * Organise cypress tests better
    
    * Ensure we only display unresolved exceptions in case list
    
    Co-authored-by: Tausif Patel <tausifpatel.dev@gmail.com>
    
    * Add test for not displaying resolved triggers when filtering unresolved
    
    * Display resolved reason when filtering for resolved cases
    
    * Ensure extra reason row is not rendered when exception is not displayed
    
    Co-authored-by: Tausif Patel <tausifpatel.dev@gmail.com>
    
    ---------
    
    Co-authored-by: Tausif Patel <tausifpatel.dev@gmail.com>

--- cypress/e2e/case-list/index.cy.ts
+++ cypress/e2e/case-list/index.cy.ts
@@ -1,8 +1,8 @@
 import TriggerCode from "bichard7-next-data-latest/dist/types/TriggerCode"
-import { TestTrigger } from "../../test/utils/manageTriggers"
-import a11yConfig from "../support/a11yConfig"
-import { loginAndVisit } from "../support/helpers"
-import logAccessibilityViolations from "../support/logAccessibilityViolations"
+import { TestTrigger } from "../../../test/utils/manageTriggers"
+import a11yConfig from "../../support/a11yConfig"
+import { loginAndVisit } from "../../support/helpers"
+import logAccessibilityViolations from "../../support/logAccessibilityViolations"
 
 describe("Case list", () => {
   beforeEach(() => {
@@ -317,5 +317,223 @@ describe("Case list", () => {
       cy.get("tr").not(":first").get("td:nth-child(7)").contains("TRPR0010 - Conditional bail")
       cy.get("tr").not(":first").get("td:nth-child(7)").contains("TRPR0015 - Personal details changed")
     })
+
+    it("Should only display error reason when the exceptions are not resolved (showing unresolved cases by default)", () => {
+      cy.task("insertCourtCasesWithFields", [
+        {
+          orgForPoliceFilter: "011111",
+          errorStatus: "Unresolved",
+          triggerStatus: "Unresolved",
+          errorReason: "",
+          errorReport: ""
+        },
+        {
+          orgForPoliceFilter: "011111",
+          errorStatus: "Resolved",
+          triggerStatus: "Unresolved",
+          errorReason: "",
+          errorReport: ""
+        }
+      ])
+
+      cy.task("insertException", {
+        caseId: 0,
+        exceptionCode: "HO100310",
+        errorReport: "HO100310||ds:OffenceReasonSequence",
+        errorStatus: "Unresolved"
+      })
+      cy.task("insertException", {
+        caseId: 0,
+        exceptionCode: "HO100310",
+        errorReport: "HO100310||ds:OffenceReasonSequence",
+        errorStatus: "Unresolved"
+      })
+      cy.task("insertException", {
+        caseId: 1,
+        exceptionCode: "HO100322",
+        errorReport: "HO100322||ds:OrganisationUnitCode",
+        errorStatus: "Resolved"
+      })
+
+      loginAndVisit()
+
+      cy.get("tr").not(":first").get("td:nth-child(7)").contains("HO100310 (2)")
+      cy.get("tr").not(":first").get("td:nth-child(7)").contains("HO100322").should("not.exist")
+    })
+
+    it("Should only display trigger reason when the triggers are not resolved", () => {
+      cy.task("insertCourtCasesWithFields", [
+        {
+          orgForPoliceFilter: "011111",
+          errorStatus: "Unresolved",
+          exceptionStatus: "Unresolved",
+          errorReason: "",
+          errorReport: ""
+        },
+        {
+          orgForPoliceFilter: "011111",
+          errorStatus: "Resolved",
+          exceptionStatus: "Unresolved",
+          errorReason: "",
+          errorReport: ""
+        }
+      ])
+
+      cy.task("insertTriggers", {
+        caseId: 0,
+        triggers: [
+          {
+            triggerId: 0,
+            triggerCode: TriggerCode.TRPR0001,
+            status: "Unresolved",
+            createdAt: new Date("2022-07-09T10:22:34.000Z")
+          },
+          {
+            triggerId: 1,
+            triggerCode: TriggerCode.TRPR0002,
+            status: "Resolved",
+            createdAt: new Date("2022-07-09T10:22:34.000Z")
+          }
+        ]
+      })
+      cy.task("insertTriggers", {
+        caseId: 1,
+        triggers: [
+          {
+            triggerId: 2,
+            triggerCode: TriggerCode.TRPR0003,
+            status: "Unresolved",
+            createdAt: new Date("2022-07-09T10:22:34.000Z")
+          },
+          {
+            triggerId: 3,
+            triggerCode: TriggerCode.TRPR0004,
+            status: "Resolved",
+            createdAt: new Date("2022-07-09T10:22:34.000Z")
+          }
+        ]
+      })
+
+      loginAndVisit()
+
+      cy.get("tr").not(":first").get("td:nth-child(7)").contains("TRPR0001")
+      cy.get("tr").not(":first").get("td:nth-child(7)").contains("TRPR0002").should("not.exist")
+      cy.get("tr").not(":first").get("td:nth-child(7)").contains("TRPR0003")
+      cy.get("tr").not(":first").get("td:nth-child(7)").contains("TRPR0004").should("not.exist")
+    })
+
+    it("Should display resolved reason when the exceptions are resolved and filtering resolved cases", () => {
+      cy.task("insertCourtCasesWithFields", [
+        {
+          orgForPoliceFilter: "011111",
+          errorStatus: "Resolved",
+          triggerStatus: "Resolved",
+          errorReason: "",
+          errorReport: "",
+          errorResolvedBy: "GeneralHandler",
+          triggerResolvedBy: "GeneralHandler"
+        },
+        {
+          orgForPoliceFilter: "011111",
+          errorStatus: "Resolved",
+          triggerStatus: "Resolved",
+          errorReason: "",
+          errorReport: "",
+          errorResolvedBy: "GeneralHandler",
+          triggerResolvedBy: "GeneralHandler"
+        }
+      ])
+
+      cy.task("insertException", {
+        caseId: 0,
+        exceptionCode: "HO100310",
+        errorReport: "HO100310||ds:OffenceReasonSequence",
+        errorStatus: "Resolved"
+      })
+      cy.task("insertException", {
+        caseId: 0,
+        exceptionCode: "HO100310",
+        errorReport: "HO100310||ds:OffenceReasonSequence",
+        errorStatus: "Resolved"
+      })
+      cy.task("insertException", {
+        caseId: 1,
+        exceptionCode: "HO100322",
+        errorReport: "HO100322||ds:OrganisationUnitCode",
+        errorStatus: "Resolved"
+      })
+
+      cy.task("insertTriggers", {
+        caseId: 0,
+        triggers: [
+          {
+            triggerId: 0,
+            triggerCode: TriggerCode.TRPR0001,
+            status: "Resolved",
+            createdAt: new Date("2022-07-09T10:22:34.000Z")
+          }
+        ]
+      })
+      cy.task("insertTriggers", {
+        caseId: 1,
+        triggers: [
+          {
+            triggerId: 1,
+            triggerCode: TriggerCode.TRPR0002,
+            status: "Resolved",
+            createdAt: new Date("2022-07-09T10:22:34.000Z")
+          }
+        ]
+      })
+
+      loginAndVisit()
+      cy.get(`label[for="resolved"]`).click()
+      cy.get("button[id=search]").click()
+
+      cy.get("tr").not(":first").get("td:nth-child(7)").contains("HO100310 (2)")
+      cy.get("tr").not(":first").get("td:nth-child(7)").contains("HO100322")
+      cy.get("tr").not(":first").get("td:nth-child(7)").contains("TRPR0001")
+      cy.get("tr").not(":first").get("td:nth-child(7)").contains("TRPR0002")
+    })
+  })
+
+  it("Should display triggers only once when resolved exceptions are not displayed", () => {
+    cy.task("insertCourtCasesWithFields", [
+      {
+        orgForPoliceFilter: "011111",
+        errorStatus: "Resolved",
+        triggerStatus: "Unresolved",
+        errorReason: "",
+        errorReport: "",
+        errorResolvedBy: "GeneralHandler",
+        triggerResolvedBy: "GeneralHandler"
+      }
+    ])
+    cy.task("insertTriggers", {
+      caseId: 0,
+      triggers: [
+        {
+          triggerId: 0,
+          triggerCode: TriggerCode.TRPR0001,
+          status: "Unresolved",
+          createdAt: new Date("2022-07-09T10:22:34.000Z")
+        }
+      ]
+    })
+    cy.task("insertTriggers", {
+      caseId: 0,
+      triggers: [
+        {
+          triggerId: 1,
+          triggerCode: TriggerCode.TRPR0002,
+          status: "Unresolved",
+          createdAt: new Date("2022-07-09T10:22:34.000Z")
+        }
+      ]
+    })
+
+    loginAndVisit()
+
+    cy.get("tr").not(":first").get("td:nth-child(7)").find(".trigger-description").should("have.length", 2)
   })
 })
