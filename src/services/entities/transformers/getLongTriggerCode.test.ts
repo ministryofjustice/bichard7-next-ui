8eb93ce65cfc337524d6bd5fda7bb8ad5dbc10d0
Ian King <ian.antking@gmail.com>
2024-07-26 11:43:14 +0100

    add getLongTriggerCode test case of PS codes

--- src/services/entities/transformers/getLongTriggerCode.test.ts
+++ src/services/entities/transformers/getLongTriggerCode.test.ts
@@ -1,31 +1,37 @@
 import getLongTriggerCode from "./getLongTriggerCode"
 
 describe("getLongTriggerCode", () => {
-  it("Should return the short version of the trigger code", () => {
+  it("returns the short version of the trigger code", () => {
     const result = getLongTriggerCode("PR01")
 
     expect(result).toBe("TRPR0001")
   })
 
-  it("should return passed value when it is not a short trigger code", () => {
+  it("handles 'PS' trigger codes", () => {
+    const result = getLongTriggerCode("PS08")
+
+    expect(result).toBe("TRPS0008")
+  })
+
+  it("returns passed value when it is not a short trigger code", () => {
     const result = getLongTriggerCode("H0100302")
 
     expect(result).toBe("H0100302")
   })
 
-  it("Should return the passed value when trigger code is not a number", () => {
+  it("returns the passed value when trigger code is not a number", () => {
     const result = getLongTriggerCode("INVALID")
 
     expect(result).toBe("INVALID")
   })
 
-  it("Should return null when value is null", () => {
+  it("returns null when value is null", () => {
     const result = getLongTriggerCode(null)
 
     expect(result).toBe(null)
   })
 
-  it("Should return null when value is undefined", () => {
+  it("returns null when value is undefined", () => {
     const result = getLongTriggerCode()
 
     expect(result).toBe(null)
