a60854b7476bd2db90288ccb318baa335d45f596
Richard Race <1273965+richrace@users.noreply.github.com>
2024-07-30 17:15:36 +0100

    Remove unneeded CSRF cookies

--- src/middleware/withCsrf/generateCsrfToken.ts
+++ src/middleware/withCsrf/generateCsrfToken.ts
@@ -11,14 +11,21 @@ interface GenerateCsrfTokenResult {
 
 const tokens = new Tokens()
 
-export default (request: IncomingMessage): GenerateCsrfTokenResult => {
-  const { tokenName, cookieSecret, formSecret, maximumTokenAgeInSeconds } = CSRF
+const generateCookieName = (urlPath: string): string => {
+  const { tokenName } = CSRF
+  return encodeURIComponent(`${tokenName}${urlPath}`)
+}
+
+const generateCsrfToken = (request: IncomingMessage): GenerateCsrfTokenResult => {
+  const { cookieSecret, formSecret, maximumTokenAgeInSeconds } = CSRF
   const tokenExpiryDate = new Date()
   tokenExpiryDate.setSeconds(tokenExpiryDate.getSeconds() + maximumTokenAgeInSeconds)
   const token = tokens.create(`${cookieSecret}${formSecret}`)
   const cookieToken = sign(token, cookieSecret)
-  const cookieName = encodeURIComponent(`${tokenName}${request.url}`)
+  const cookieName = generateCookieName(request.url ?? "/")
   const formToken = sign(`${cookieName}=${tokenExpiryDate.getTime()}.${token}`, formSecret)
 
   return { formToken, cookieToken, cookieName }
 }
+
+export { generateCookieName, generateCsrfToken }
--- src/middleware/withCsrf/withCsrf.ts
+++ src/middleware/withCsrf/withCsrf.ts
@@ -2,9 +2,9 @@ import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult
 import { ParsedUrlQuery } from "querystring"
 import CsrfServerSidePropsContext from "types/CsrfServerSidePropsContext"
 import setCookie from "utils/setCookie"
-import generateCsrfToken from "./generateCsrfToken"
-import verifyCsrfToken from "./verifyCsrfToken"
 import { CSRF } from "../../config"
+import { generateCookieName, generateCsrfToken } from "./generateCsrfToken"
+import verifyCsrfToken from "./verifyCsrfToken"
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default <Props extends { [key: string]: any }>(
@@ -24,8 +24,23 @@ export default <Props extends { [key: string]: any }>(
       return { props: {} } as unknown as GetServerSidePropsResult<Props>
     }
 
+    if (req.headers.referer) {
+      const queryString = req.headers.referer.replace(/.*\/bichard/, "")
+
+      let cookieName: string
+      if (queryString.startsWith("/")) {
+        cookieName = generateCookieName(queryString)
+      } else {
+        const queryParams = new URLSearchParams(queryString)
+        cookieName = generateCookieName("/?" + queryParams.toString())
+      }
+
+      setCookie(res, cookieName, "", { expires: new Date(0), path: "/bichard", httpOnly: true })
+    }
+
     const { maximumTokenAgeInSeconds } = CSRF
     const { formToken, cookieToken, cookieName } = generateCsrfToken(req)
+
     setCookie(res, cookieName, cookieToken, { maxAge: maximumTokenAgeInSeconds, path: "/bichard", httpOnly: true })
 
     return getServerSidePropsFunction({ ...context, formData, csrfToken: formToken } as CsrfServerSidePropsContext)
