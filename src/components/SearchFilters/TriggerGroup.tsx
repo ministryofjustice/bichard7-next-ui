e4b3c2e0157520743a44f46a332597b3f578f872
Richard Race <1273965+richrace@users.noreply.github.com>
2024-07-30 12:16:03 +0100

    Refactor func allTriggersSelected and add tests

--- src/components/SearchFilters/TriggerGroup.tsx
+++ src/components/SearchFilters/TriggerGroup.tsx
@@ -4,7 +4,7 @@ import IndeterminateCheckbox from "components/IndeterminateCheckbox"
 import { Dispatch } from "react"
 import getShortTriggerCode from "services/entities/transformers/getShortTriggerCode"
 import { FilterAction, ReasonCode } from "types/CourtCaseFilter"
-import { allSelected, noneSelected } from "utils/triggerGroups/handleSelected"
+import allTriggersSelected from "utils/triggerGroups/allTriggersSelected"
 import selectedTrigger from "utils/triggerGroups/selectedTrigger"
 import someTriggersSelected from "utils/triggerGroups/someTriggersSelected"
 import TriggerCheckbox from "./TriggerCheckbox"
@@ -19,19 +19,20 @@ interface TriggerGroupProps {
 
 const TriggerGroup = ({ name, allGroupTriggers, filteredReasonCodes, dispatch }: TriggerGroupProps): JSX.Element => {
   const someSelected = someTriggersSelected(allGroupTriggers, filteredReasonCodes)
+  const allSelected = allTriggersSelected(allGroupTriggers, filteredReasonCodes)
 
   return (
     <fieldset className="govuk-fieldset">
       <IndeterminateCheckbox
         id={name.toLowerCase()}
-        checkedValue={allSelected(allGroupTriggers, filteredReasonCodes)}
+        checkedValue={allSelected}
         labelText={name}
         indeterminate={someSelected}
         value={allGroupTriggers.map((trigger) => getShortTriggerCode(trigger) ?? "")}
         dispatch={dispatch}
       />
 
-      <ConditionalRender isRendered={!noneSelected(allGroupTriggers, filteredReasonCodes) || someSelected}>
+      <ConditionalRender isRendered={allSelected || someSelected}>
         <TriggerGroupList>
           {allGroupTriggers.map((triggerCode) => (
             <TriggerCheckbox
