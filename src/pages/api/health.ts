c773c7e4a08634074ea57ceaf216e6ad0e0529a5
Richard Race <1273965+richrace@users.noreply.github.com>
2024-07-31 11:43:37 +0100

    Remove the CSRF Cookie secret

--- src/pages/api/health.ts
+++ src/pages/api/health.ts
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from "next"
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
   const { NODE_ENV } = process.env
 
-  if (NODE_ENV === "production" && (CSRF.cookieSecret === "OliverTwist1" || CSRF.formSecret === "OliverTwist2")) {
+  if (NODE_ENV === "production" && CSRF.formSecret === "OliverTwist2") {
     res.status(500).end()
   } else {
     res.status(200).end()
