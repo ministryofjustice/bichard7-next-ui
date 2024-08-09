af3f32c0ea247e3403392434c1755b1a07f7bc0f
Richard Race <1273965+richrace@users.noreply.github.com>
2024-08-09 16:09:27 +0100

    Remove redundant eslint ignores

--- src/features/CourtCaseFilters/ColumnOrderIcons.tsx
+++ src/features/CourtCaseFilters/ColumnOrderIcons.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable jsx-a11y/no-static-element-interactions */
 import { ReactNode } from "react"
 import { blue } from "../../utils/colours"
 import { Content, Icon, IconContainer } from "./ColumnOrderIcons.styles"
