f35269261802d9e171b6687e7ecdd13f1f438304
Richard Race <1273965+richrace@users.noreply.github.com>
2024-09-06 15:55:16 +0100

    Refactor conditional renders into simple ternaries

--- src/features/CourtCaseDetails/Tabs/Panels/Offences/Offence/Matcher/OffenceMatching.tsx
+++ src/features/CourtCaseDetails/Tabs/Panels/Offences/Offence/Matcher/OffenceMatching.tsx
@@ -1,5 +1,4 @@
 import { Offence } from "@moj-bichard7-developers/bichard7-next-core/core/types/AnnotatedHearingOutcome"
-import ConditionalRender from "components/ConditionalRender"
 import { useCourtCase } from "context/CourtCaseContext"
 import { useCurrentUser } from "context/CurrentUserContext"
 import { Exception } from "types/exceptions"
@@ -7,11 +6,8 @@ import getExceptionMessage from "utils/offenceMatcher/getExceptionMessage"
 import getOffenceMatchingException from "utils/offenceMatcher/getOffenceMatchingException"
 import isEnabled from "utils/offenceMatcher/isEnabled"
 import offenceMatchingExceptions from "utils/offenceMatcher/offenceMatchingExceptions"
-
-import LegacySequencingBadgeTableRow from "./TableRows/LegacySequencingBadgeTableRow"
-import LegacySequencingMessageTableRow from "./TableRows/LegacySequencingMessageTableRow"
-import OffenceMatcherTableRow from "./TableRows/OffenceMatcherTableRow"
-import OffenceMatchingBadgeTableRow from "./TableRows/OffenceMatchingBadgeTableRow"
+import LegacySequencingRenderer from "./LegacySequencingRenderer"
+import OffenceMatcherRenderer from "./OffenceMatcherRenderer"
 
 type OffenceMatchingProps = {
   offenceIndex: number
@@ -31,42 +27,29 @@ export const OffenceMatching = ({
   const { courtCase } = useCourtCase()
   const currentUser = useCurrentUser()
 
+  const offenceReasonSequence = offence.CriminalProsecutionReference.OffenceReasonSequence
   const offenceMatchingException = isCaseUnresolved && getOffenceMatchingException(exceptions, offenceIndex)
   const offenceMatchingExceptionMessage = getExceptionMessage(courtCase, offenceIndex)
 
   const displayOffenceMatcher =
     isEnabled(currentUser) && exceptions.some((e) => offenceMatchingExceptions.offenceNotMatched.includes(e.code))
-  const userCanMatchOffence =
-    courtCase.errorLockedByUsername === currentUser?.username && courtCase.errorStatus === "Unresolved"
 
   return (
     <>
-      {/* If we don't display the exception matcher,
-      we should display the PNC sequence number input box below. */}
-      <ConditionalRender isRendered={displayOffenceMatcher}>
-        {offenceMatchingException && userCanMatchOffence ? (
-          <OffenceMatcherTableRow offenceIndex={offenceIndex} isCaseLockedToCurrentUser={isCaseLockedToCurrentUser} />
-        ) : (
-          <OffenceMatchingBadgeTableRow
-            offenceIndex={offenceIndex}
-            offenceReasonSequence={offence.CriminalProsecutionReference.OffenceReasonSequence}
-          />
-        )}
-      </ConditionalRender>
-
-      {/* PNC sequence number */}
-      <ConditionalRender isRendered={!displayOffenceMatcher}>
-        {offenceMatchingException ? (
-          <LegacySequencingMessageTableRow
-            exception={offenceMatchingException}
-            message={offenceMatchingExceptionMessage}
-          />
-        ) : (
-          <LegacySequencingBadgeTableRow
-            offenceReasonSequence={offence.CriminalProsecutionReference.OffenceReasonSequence}
-          />
-        )}
-      </ConditionalRender>
+      {displayOffenceMatcher ? (
+        <OffenceMatcherRenderer
+          offenceMatchingException={!!offenceMatchingException}
+          offenceIndex={offenceIndex}
+          isCaseLockedToCurrentUser={isCaseLockedToCurrentUser}
+          offenceReasonSequence={offenceReasonSequence}
+        />
+      ) : (
+        <LegacySequencingRenderer
+          offenceMatchingException={offenceMatchingException}
+          offenceMatchingExceptionMessage={offenceMatchingExceptionMessage}
+          offenceReasonSequence={offenceReasonSequence}
+        />
+      )}
     </>
   )
 }
