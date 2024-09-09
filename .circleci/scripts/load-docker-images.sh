ed94822461c8a642e82caaac652d5e83dec802da
Richard Race <1273965+richrace@users.noreply.github.com>
2024-09-09 17:15:32 +0100

    Add more docker images

--- .circleci/scripts/load-docker-images.sh
+++ .circleci/scripts/load-docker-images.sh
@@ -12,4 +12,13 @@ docker image load < "images/user-service"
 echo "Loading ngnix-auth-proxy"
 docker image load < "images/nginx-auth-proxy"
 
+echo "Loading message-forwarder"
+docker image load < "images/message-forwarder"
+
+echo "Loading audit-log-api"
+docker image save < "images/audit-log-api"
+
+echo "Loading event-handler"
+docker image save < "images/event-handler"
+
 echo "Finished loading"
--- .circleci/scripts/save-docker-images.sh
+++ .circleci/scripts/save-docker-images.sh
@@ -9,4 +9,13 @@ docker image save -o "images/user-service" "user-service"
 echo "Saving nginx-auth-proxy"
 docker image save -o "images/nginx-auth-proxy" "nginx-auth-proxy"
 
+echo "Saving message-forwarder"
+docker image save -o "images/message-forwarder" "message-forwarder"
+
+echo "Saving audit-log-api"
+docker image save -o "images/audit-log-api" "audit-log-api"
+
+echo "Saving event-handler"
+docker image save -o "images/event-handler" "event-handler"
+
 echo "Finished saving"
