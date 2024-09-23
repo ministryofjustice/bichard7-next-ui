06654fddcfce75b5e52bbe544e9146bdee27464d
Ian King <ian.antking@gmail.com>
2024-09-23 13:06:26 +0100

    change endpoint to /reports

--- cypress/e2e/api/reports/getReports.cy.ts
+++ cypress/e2e/api/reports/getReports.cy.ts
@@ -1,10 +1,10 @@
-describe("court cases API endpoint", () => {
-  describe("GET /court-cases", () => {
+describe("reports API endpoint", () => {
+  describe("GET /reports", () => {
     describe("without authentication", () => {
       it("returns a 401 status code", () => {
         cy.request({
           method: "GET",
-          url: `/bichard/api/court-cases`,
+          url: `/bichard/api/reports`,
           failOnStatusCode: false
         }).then((response) => {
           expect(response.status).to.equal(401)
@@ -20,7 +20,7 @@ describe("court cases API endpoint", () => {
       it("returns a 200 status code", () => {
         cy.request({
           method: "GET",
-          url: `/bichard/api/court-cases`
+          url: `/bichard/api/reports`
         }).then((response) => {
           expect(response.status).to.equal(200)
         })
